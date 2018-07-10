72e25a4bbe35fb31b6b1f16ea46bc63b0140c7ed
Emma Beynon <emma.beynon@digital.cabinet-office.gov.uk>
2018-07-10 10:36:01 +0100

    Add healthcheck endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
+  get "/healthcheck", to: proc { [200, {}, ["OK"]] }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
