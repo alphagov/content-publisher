796b05ac39989e38dbd2465f747f38eec3ffbc92
Emma Beynon <emma.beynon@digital.cabinet-office.gov.uk>
2018-07-10 12:19:37 +0100

    Remove Active Storage
    This application doesn't need to use Active Storage so we remove all code
    related to it.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,8 +27,6 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
-  # Store uploaded files on the local file system (see config/storage.yml for options)
-  config.active_storage.service = :local
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
