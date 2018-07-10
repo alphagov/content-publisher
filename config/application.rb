f409d70eb96383ebef369df5c7f6db8bd5f75b3d
Emma Beynon <emma.beynon@digital.cabinet-office.gov.uk>
2018-07-10 12:10:21 +0100

    Specify required Rails frameworks
    We have removed some of the Rails frameworks we don't need, so we specify which
    ones this application needs.

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,10 @@
 require_relative 'boot'
 
-require 'rails/all'
+require 'active_record/railtie'
+require 'action_controller/railtie'
+require 'action_view/railtie'
+require 'active_job/railtie'
+require 'sprockets/railtie'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
