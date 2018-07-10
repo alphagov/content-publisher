2ca3622eb8db711bbdd14610321f4aa3c2c8eac2
Emma Beynon <emma.beynon@digital.cabinet-office.gov.uk>
2018-07-10 12:07:39 +0100

    Remove Action Mailer
    This application does not need to use Action Mailer so we remove all usage of
    it.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,12 +57,6 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "content-publisher_#{Rails.env}"
 
-  config.action_mailer.perform_caching = false
-
-  # Ignore bad email addresses and do not raise email delivery errors.
-  # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
-
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
