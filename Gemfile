2698f14e62069f830db50d119b5d5cfb029aa8e7
Ben Thorner <ben.thorner@digital.cabinet-office.gov.uk>
2018-07-10 13:50:09 +0100

    Remove gems until we need them, simplify specs

--- Gemfile
+++ Gemfile
@@ -2,30 +2,24 @@ ruby File.read(".ruby-version").strip
 
 source "https://rubygems.org"
 
-gem "addressable"
-gem "bootsnap"
-gem "gds-api-adapters"
-gem "gds-sso"
-gem "govuk_app_config"
-gem "govuk_sidekiq"
-gem "listen"
-gem "pg"
-gem "plek"
-gem "rails", "5.2.0"
-gem "sass-rails"
-gem "uglifier"
+gem "rails", "~> 5.2"
+
+gem "bootsnap", "~> 1"
+gem "gds-sso", "~> 13"
+gem "govuk_app_config", "~> 1"
+gem "pg", "~> 1"
+gem "plek", "~> 2"
+
+group :development do
+  gem "byebug"
+  gem "govuk-lint"
+  gem "listen"
+end
+
+group :test do
+  gem "simplecov", require: false
+end
 
 group :development, :test do
-  gem "byebug" # Comes standard with Rails
-  gem "capybara"
-  gem "database_cleaner"
-  gem "factory_bot_rails"
-  gem "govuk-content-schema-test-helpers"
-  gem "govuk-lint"
-  gem "poltergeist"
-  gem "pry"
   gem "rspec-rails"
-  gem "simplecov", require: false
-  gem "timecop"
-  gem "webmock", require: false
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,43 +1,14 @@
-require "pry"
 require "byebug"
-require "webmock"
-require "timecop"
 require "simplecov"
 
+ENV["RAILS_ENV"] ||= "test"
+require File.expand_path("../../config/environment", __FILE__)
+require "rspec/rails"
+
+Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 SimpleCov.start
 
 RSpec.configure do |config|
-  # rspec-expectations config goes here. You can use an alternate
-  # assertion/expectation library such as wrong or the stdlib/minitest
-  # assertions if you prefer.
-  config.expect_with :rspec do |expectations|
-    # This option will default to `true` in RSpec 4. It makes the `description`
-    # and `failure_message` of custom matchers include text for helper methods
-    # defined using `chain`, e.g.:
-    #     be_bigger_than(2).and_smaller_than(4).description
-    #     # => "be bigger than 2 and smaller than 4"
-    # ...rather than:
-    #     # => "be bigger than 2"
-    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
-  end
-
-  # rspec-mocks config goes here. You can use an alternate test double
-  # library (such as bogus or mocha) by changing the `mock_with` option here.
-  config.mock_with :rspec do |mocks|
-    # Prevents you from mocking or stubbing a method that does not exist on
-    # a real object. This is generally recommended, and will default to
-    # `true` in RSpec 4.
-    mocks.verify_partial_doubles = true
-  end
-
-  config.disable_monkey_patching!
   config.expose_dsl_globally = false
-
-  config.before(:suite) do
-    WebMock.disable_net_connect!(allow_localhost: true)
-  end
-
-  config.after do
-    Timecop.return
-  end
+  config.infer_spec_type_from_file_location!
 end
