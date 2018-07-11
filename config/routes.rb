d326cda1ebf4d542f10069b6b64c11728f8738d3
Ben Thorner <ben.thorner@digital.cabinet-office.gov.uk>
2018-07-11 11:07:47 +0100

    More cleanup for the skeleton rails app
      * Remove app/jobs, vendor/, bin/update, package.json, config/puma.rb
      * Upgrade to ruby 2.5.1
      * Enable rubocop check for FrozenStringLiteralComment
      * Version all gems (including dev/test)
      * Add designated port to Procfile

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   get "/healthcheck", to: proc { [200, {}, ["OK"]] }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
