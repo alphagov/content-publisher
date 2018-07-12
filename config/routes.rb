3a289b32800811d5b71b83841c0fe406169c0a83
Tijmen Brommet <tijmen@gmail.com>
2018-07-12 18:06:20 +0100

    Remove TmpController
    This serves the same purpose as the DevelopmentController.

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,4 @@ Rails.application.routes.draw do
   root to: 'development#index'
 
   get "/healthcheck", to: proc { [200, {}, ["OK"]] }
-
-  root 'tmp#index'
-
-  resources :tmp, only: [:index]
 end
