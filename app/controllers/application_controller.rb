8a9252bfec40ae1b02ee53bc7b9de7051c675846
Tijmen Brommet <tijmen@gmail.com>
2018-07-13 15:03:13 +0100

    WAAAAT

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@
 class ApplicationController < ActionController::Base
   include GDS::SSO::ControllerMethods
 
-  if ENV["REQUIRE_BASIC_AUTH"]
+  if ENV["BASIC_AUTH_USERNAME"]
     http_basic_authenticate_with(
       name: ENV.fetch("BASIC_AUTH_USERNAME"),
       password: ENV.fetch("BASIC_AUTH_PASSWORD")
