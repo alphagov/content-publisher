4c6c7e097a20c552036d7b5191b80798b0ca560b
Tijmen Brommet <tijmen@gmail.com>
2018-07-13 14:54:46 +0100

    Use basic auth on Heroku
    This prevents confusion for users and prevents search engines from
    indexing the pages.
    
    https://trello.com/c/CPWJzL9y

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,12 @@
 class ApplicationController < ActionController::Base
   include GDS::SSO::ControllerMethods
 
+  if ENV["REQUIRE_BASIC_AUTH"]
+    http_basic_authenticate_with(
+      name: ENV.fetch("BASIC_AUTH_USERNAME"),
+      password: ENV.fetch("BASIC_AUTH_PASSWORD")
+    )
+  end
+
   before_action :authenticate_user!
 end
