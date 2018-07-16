d65a4621613d3d57370ee9adbe720becf02db7a4
Tijmen Brommet <tijmen@gmail.com>
2018-07-16 18:15:17 +0100

    Create the smallest working document workflow
    This commit is the least I can do to get a working workflow where a
    user chooses a format (out of a choice of 1), updates the title and
    saves the document.
    
    Nothing in this commit is intended to be set in stone - it's likely
    that most of it will change in the coming months. It's here to get
    something started and we can start on more stories to build out parts
    of the interface.
    
    - There's just 1 format to choose from now
    - Note that the routes are a mess, and should probably use `resources`,
    but I wanted to keep an open mind on how RESTful the application will
    be.
    - We'll probably want to use better form helpers to create our forms.
    - We'll extract creating and updating of models into service objects -
    doing it like this is recipe for a mess

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  root to: 'development#index'
+  root to: redirect('/content')
+  get '/dev' => 'development#index'
+
+  get '/content' => 'content#index'
+  get '/content/new' => 'content#choose_format', as: :new_document
+  post '/content/create' => 'content#create', as: :create_content
+  get '/content/:id/edit' => 'content#edit', as: :edit_document
+  patch '/content/:id' => 'content#update', as: :document
 
   get "/healthcheck", to: proc { [200, {}, ["OK"]] }
 end
---
+++ spec/integration/create_a_document_spec.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+RSpec.describe "Create a document", type: :feature do
+  scenario "User creates a document" do
+    when_i_click_on_create_a_document
+    and_i_choose_news_article
+    and_i_fill_in_a_title
+    then_the_document_exists
+  end
+
+  def when_i_click_on_create_a_document
+    visit "/"
+    click_on "New document"
+  end
+
+  def and_i_choose_news_article
+    click_on "Create news article"
+  end
+
+  def and_i_fill_in_a_title
+    fill_in "document[title]", with: "A great title"
+    click_on "Save"
+  end
+
+  def then_the_document_exists
+    expect(Document.last.title).to eql("A great title")
+    visit "/"
+    expect(page).to have_content "A great title"
+  end
+end
