5c365e37d009845669007e99ee2574bb7fc7eb4e
Tijmen Brommet <tijmen@gmail.com>
2018-07-16 18:04:16 +0100

    Create a test user before the test suite runs
    The integration tests will need this user to sign in.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,4 +13,8 @@ SimpleCov.start
 RSpec.configure do |config|
   config.expose_dsl_globally = false
   config.infer_spec_type_from_file_location!
+
+  config.before(:suite) do
+    User.create!(permissions: ["signin"])
+  end
 end
