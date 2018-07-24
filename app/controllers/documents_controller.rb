5a2a8a2a604756001b7095a81a04286520579978
Tijmen Brommet <tijmen@gmail.com>
2018-07-24 10:13:51 +0100

    Add MVP for format-specific content editing
    This PR adds the minimum to create a system of format-dependent content
    editing. It is the first step to allow us to specify the formats in
    this app in a YAML.
    
    It works like this:
    
    - In the the document types YAML we define `fields` for a type
    - Fields have a `id`, `label` and `type`
    - Using these fields, we build a form with the correct inputs. In this
    PR that's just the "govspeak" input, but I imagine we'll have inputs
    like dates, dropdowns, booleans and more.
    - The input of the form is then used in the controller (with an
    additional param sanitisation step)
    - The extracted input is then persisted into the `contents` JSON
    column, which the forms can access
    
    For future PRs:
    
    - Add more input types
    - Add more fields for formats
    - Use the `contents` in the payload to the publishing-api
    (https://trello.com/c/mVJa1YlK)
    - Use `DocumentTypeSchema` to refactor the controllers
    
    This majority of this work comes from a spike into the system
    @benthorner and I did last week.
    
    https://trello.com/c/PxtaaObC

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -11,13 +11,9 @@ class DocumentsController < ApplicationController
 
   def update
     document = Document.find(params[:id])
+    allowed_field_names_in_contents = document.document_type_schema.fields.map(&:id)
+    document_update_params = params.require(:document).permit(:title, contents: allowed_field_names_in_contents)
     document.update_attributes(document_update_params)
     redirect_to edit_document_path(document)
-  end
-
-private
-
-  def document_update_params
-    params.require(:document).permit(:title)
   end
 end
