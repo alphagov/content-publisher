48f6f06829dbb9bd5ec24365217973086e757682
Ben Thorner <ben.thorner@digital.cabinet-office.gov.uk>
2018-07-25 11:32:52 +0100

    Avoid using non-view instances in controllers

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -14,19 +14,19 @@ class DocumentsController < ApplicationController
   end
 
   def update
-    @document = Document.find(params[:id])
-    @document.update_attributes(document_update_params)
-    DocumentPublishingService.new.publish_draft(@document)
-    redirect_to @document, notice: "Preview creation successful"
+    document = Document.find(params[:id])
+    document.update_attributes(document_update_params(document))
+    DocumentPublishingService.new.publish_draft(document)
+    redirect_to document, notice: "Preview creation successful"
   rescue GdsApi::HTTPErrorResponse, SocketError => e
     Rails.logger.error(e)
-    redirect_to @document, alert: "Error creating preview"
+    redirect_to document, alert: "Error creating preview"
   end
 
 private
 
-  def document_update_params
-    contents_params = @document.document_type_schema.fields.map(&:id)
+  def document_update_params(document)
+    contents_params = document.document_type_schema.fields.map(&:id)
     params.require(:document).permit(:title, :base_path, contents: contents_params)
   end
 end
