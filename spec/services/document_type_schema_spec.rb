dc7d23a7845ac3b01e2c1acab1434f823a4b165f
Kevin Dew <kevindew@me.com>
2018-07-25 10:04:27 +0100

    Raise an exception when we don't know the document_type
    Otherwise this gets lost in a rather cryptic no method for nil error
    when the output is processed.

--- spec/services/document_type_schema_spec.rb
+++ spec/services/document_type_schema_spec.rb
@@ -3,6 +3,17 @@
 require 'spec_helper'
 
 RSpec.describe DocumentTypeSchema do
+  describe ".find" do
+    it "returns a DocumentTypeSchema when it's a known document_type" do
+      expect(DocumentTypeSchema.find("press_release")).to be_a(DocumentTypeSchema)
+    end
+
+    it "raises a RuntimeError when we don't know the document_type" do
+      expect { DocumentTypeSchema.find("unknown_document_type") }
+        .to raise_error(RuntimeError, "Document type unknown_document_type not found")
+    end
+  end
+
   describe '#fields' do
     it 'is an empty array by default' do
       schema = DocumentTypeSchema.new
