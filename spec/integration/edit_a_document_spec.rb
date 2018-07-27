0640d2fe8b47c3ab25b577c19895d41931e0cc08
Tijmen Brommet <tijmen@gmail.com>
2018-07-27 12:16:30 +0100

    Rewrite integration test for document editing

--- spec/integration/edit_a_document_spec.rb
+++ spec/integration/edit_a_document_spec.rb
@@ -3,42 +3,52 @@
 require "spec_helper"
 
 RSpec.describe "Edit a document", type: :feature do
-  scenario "User edits a document" do
-    given_there_is_a_document
-    when_i_go_to_edit_the_document
-    and_i_fill_in_the_fields
-    then_i_see_the_document_is_saved
-    and_the_preview_creation_succeeded
-  end
+  DocumentTypeSchema.all.each do |schema|
+    next if schema.managed_elsewhere?
 
-  def given_there_is_a_document
-    create :document, document_type: "press_release"
-  end
+    scenario "User edits #{schema.name}" do
+      @schema = schema
 
-  def when_i_go_to_edit_the_document
-    visit document_path(Document.last)
-    click_on "Edit document"
-    @request = stub_publishing_api_put_content(Document.last.content_id, {})
-  end
+      given_there_is_a_document
+      when_i_go_to_edit_the_document
+      and_i_fill_in_the_fields
+      then_i_see_the_document_is_saved
+      and_the_preview_creation_succeeded
+    end
 
-  def and_i_fill_in_the_fields
-    fill_in "document[contents][body]", with: "The document body"
-    fill_in "document[summary]", with: "A summary of the release."
-    click_on "Save"
-  end
+    def given_there_is_a_document
+      create :document, document_type: @schema.document_type
+    end
 
-  def then_i_see_the_document_is_saved
-    expect(Document.last.summary).to eql("A summary of the release.")
-    expect(page).to have_content "The document body"
-  end
+    def when_i_go_to_edit_the_document
+      visit document_path(Document.last)
+      click_on "Edit document"
+      @request = stub_publishing_api_put_content(Document.last.content_id, {})
+    end
 
-  def and_the_preview_creation_succeeded
-    expect(@request).to have_been_requested
-    expect(page).to have_content "Preview creation successful"
+    def and_i_fill_in_the_fields
+      @schema.fields do |field|
+        # TODO: this currently works because the only field type (body) accepts
+        # a string. Once there are other field types we'll need to expand on this.
+        fill_in "document[contents][#{field.id}]", with: SecureRandom.hex
+      end
 
-    expect(a_request(:put, /content/).with { |req|
-      expect(req.body).to be_valid_against_schema("news_article")
-      expect(JSON.parse(req.body)["details"]["body"]).to eq "The document body"
-    }).to have_been_requested
+      fill_in "document[summary]", with: "A summary of the release."
+      click_on "Save"
+    end
+
+    def then_i_see_the_document_is_saved
+      expect(page).to have_content "A summary of the release."
+    end
+
+    def and_the_preview_creation_succeeded
+      expect(@request).to have_been_requested
+      expect(page).to have_content "Preview creation successful"
+
+      expect(a_request(:put, /content/).with { |req|
+        expect(req.body).to be_valid_against_schema(@schema.schema_name)
+        expect(JSON.parse(req.body)["description"]).to eq "A summary of the release."
+      }).to have_been_requested
+    end
   end
 end
