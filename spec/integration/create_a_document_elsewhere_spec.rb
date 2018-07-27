0061b81500a320a66c7ae3ec01447b568dfc81a1
Tijmen Brommet <tijmen@gmail.com>
2018-07-27 12:16:46 +0100

    Rewrite integration test for document creation redirect

--- spec/integration/create_a_document_elsewhere_spec.rb
+++ spec/integration/create_a_document_elsewhere_spec.rb
@@ -3,27 +3,31 @@
 require "spec_helper"
 
 RSpec.describe "Create a document that is managed elsewhere", type: :feature do
-  scenario "User creates a document" do
-    when_i_click_on_create_a_document
-    and_i_choose_a_document_that_is_not_managed_in_this_app
-    then_i_am_redirected_to_the_application_that_manages_the_content
-  end
+  DocumentTypeSchema.all.select(&:managed_elsewhere?).each do |schema|
+    scenario "User creates a #{schema.name}" do
+      @schema = schema
 
-  def when_i_click_on_create_a_document
-    visit "/"
-    click_on "New document"
-  end
+      when_i_click_on_create_a_document
+      and_i_choose_a_document_that_is_not_managed_in_this_app
+      then_i_am_redirected_to_the_application_that_manages_the_content
+    end
 
-  def and_i_choose_a_document_that_is_not_managed_in_this_app
-    choose "Consultations"
-    click_on "Continue"
+    def when_i_click_on_create_a_document
+      visit "/"
+      click_on "New document"
+    end
 
-    choose "Consultation (managed elsewhere)"
-    click_on "Continue"
-  end
+    def and_i_choose_a_document_that_is_not_managed_in_this_app
+      choose SupertypeSchema.find(@schema.supertype).label
+      click_on "Continue"
 
-  def then_i_am_redirected_to_the_application_that_manages_the_content
-    expect(page.current_path).to eql "/government/admin/consultations/new"
-    expect(page).to have_content "You've been redirected"
+      choose @schema.name
+      click_on "Continue"
+    end
+
+    def then_i_am_redirected_to_the_application_that_manages_the_content
+      expect(page.current_path).to eql @schema.managed_elsewhere.fetch("path")
+      expect(page).to have_content "You've been redirected"
+    end
   end
 end
