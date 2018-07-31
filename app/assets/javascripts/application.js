8ab9ef4eb2355d0aba78b16a07a23a7f65e32909
alex-ju <alex-ju@users.noreply.github.com>
2018-07-31 11:52:25 +0100

    Update application.js to follow JavaScript conventions

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,29 +1,29 @@
-'use strict';
+'use strict'
 
 document.onreadystatechange = function () {
-  var csrf = document.querySelector('meta[name="csrf-token"]').content;
-  if (document.readyState === "interactive") {
-    var document_title = document.getElementById("document-title-id");
-    var pathArray = document.location.pathname.split('/');
-    var documentId = pathArray[2];
-    var url = '/documents/' + documentId + '/generate-path';
-    document_title.onblur = function () {
-      fetch(url, {
-        method: "POST",
-        credentials: "include",
+  var csrf = document.querySelector('meta[name="csrf-token"]').content
+  if (document.readyState === 'interactive') {
+    var documentTitle = document.getElementById('document-title-id')
+    var pathArray = document.location.pathname.split('/')
+    var documentId = pathArray[2]
+    var url = '/documents/' + documentId + '/generate-path'
+    documentTitle.onblur = function () {
+      window.fetch(url, {
+        method: 'POST',
+        credentials: 'include',
         headers: {
-          "Content-Type": "application/json; charset=utf-8",
-          "X-CSRF-Token": csrf,
+          'Content-Type': 'application/json; charset=utf-8',
+          'X-CSRF-Token': csrf
         },
-        body: JSON.stringify({ title: document_title.value })
-      }).then(response => response.json().then(function(result) {
-        var base_path = document.getElementById("base-path-id");
+        body: JSON.stringify({ title: documentTitle.value })
+      }).then(response => response.json().then(function (result) {
+        var basePath = document.getElementById('base-path-id')
         if (result.reserved) {
-          base_path.innerHTML = "www.gov.uk" + result.base_path;
+          basePath.innerHTML = 'www.gov.uk' + result.basePath
         } else {
-          base_path.innerHTML = "Path is taken, please edit the title.";
+          basePath.innerHTML = 'Path is taken, please edit the title.'
         }
-      }));
-    };
+      }))
+    }
   }
-};
+}
