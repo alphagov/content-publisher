f15bab79c5c1d42a6e83210fbe42c8f80acb8912
Emma Beynon <emma.beynon@digital.cabinet-office.gov.uk>
2018-08-01 14:25:08 +0000

    Add test for when a base path is already in use
    Currently set to pending while @alex-ju works on fixing some Javascript
    problems with Capybara.

---
+++ spec/integration/create_a_document_base_path_taken_spec.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+RSpec.describe "Create a document", type: :feature do
+  DocumentTypeSchema.all.reject(&:managed_elsewhere?).each do |schema|
+    scenario "User creates #{schema.name}" do
+      pending("Alex to fix JS problems")
+      @schema = schema
+
+      when_i_click_on_create_a_document
+      and_i_choose_a_supertype
+      and_i_choose_a_document_type
+      and_i_fill_in_the_title_but_the_base_path_has_already_been_used
+      then_i_see_a_path_taken_warning_message
+    end
+
+    def when_i_click_on_create_a_document
+      visit "/"
+      click_on "New document"
+    end
+
+    def and_i_choose_a_supertype
+      choose SupertypeSchema.find(@schema.supertype).label
+      click_on "Continue"
+    end
+
+    def and_i_choose_a_document_type
+      choose @schema.name
+      click_on "Continue"
+    end
+
+    def and_i_fill_in_the_title_but_the_base_path_has_already_been_used
+      @request = publishing_api_has_lookups("#{@schema.path_prefix}/a-great-title" => "a-content-id")
+      fill_in("document[title]", with: "A great title")
+    end
+
+    def then_i_see_a_path_taken_warning_message
+      expect(@request).to have_been_requested
+      expect(page).to have_content "Path is taken, please edit the title."
+    end
+  end
+end
