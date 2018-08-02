fbaca5d5fbe22f08d154ae75a7cd82ef600814f4
David King <david.king@digital.cabinet-office.gov.uk>
2018-08-02 18:23:18 +0100

    WIP test for editing an already published doc

--- spec/integration/previews_a_url_when_editing_title_spec.rb
+++ spec/integration/previews_a_url_when_editing_title_spec.rb
@@ -3,16 +3,25 @@
 require "spec_helper"
 
 RSpec.describe "Shows a preview of the URL", type: :feature, js: true do
-  scenario "when a user edits a document" do
+  # TODO: Flaky tests yayyyy (some randomly generated paths create errors)
+  scenario "when a user edits the title of a document" do
     given_there_is_a_document
     when_i_go_to_edit_the_document
     and_i_fill_in_the_title
     then_i_see_a_preview_of_the_url_on_govuk
   end
 
+  scenario "when the user does not edit the title" do
+    given_there_is_a_document
+    when_i_go_to_edit_the_document
+    and_i_interact_with_the_title_but_leave_it_unedited
+    then_i_see_the_path_preview_unchanged
+  end
+
   def given_there_is_a_document
     @document = create(:document)
     @document_path_prefix = @document.document_type_schema.path_prefix
+    @document_base_path = @document.base_path
   end
 
   def when_i_go_to_edit_the_document
@@ -20,14 +29,29 @@ RSpec.describe "Shows a preview of the URL", type: :feature, js: true do
     click_on "Edit document"
   end
 
+  def and_i_interact_with_the_title_but_leave_it_unedited
+    @publishing_api_call_with_content = publishing_api_has_lookups(
+      "#{@document_base_path}": @document.content_id,
+      "#{@document_base_path}-1": nil,
+    )
+    page.find("#document-title-id").click
+    page.find("body").click
+  end
+
   def and_i_fill_in_the_title
-    @request = publishing_api_has_lookups("#{@document_path_prefix}/a-great-title": nil)
+    @publishing_api_call_without_content = publishing_api_has_lookups("#{@document_path_prefix}/a-great-title": nil)
     fill_in("document[title]", with: "A great title")
     page.find("body").click
   end
 
   def then_i_see_a_preview_of_the_url_on_govuk
     expect(page).to have_content "www.gov.uk#{@document_path_prefix}/a-great-title"
-    expect(@request).to have_been_requested
+    expect(@publishing_api_call_without_content).to have_been_requested.times(2)
+  end
+
+  def then_i_see_the_path_preview_unchanged
+    expect(page).to have_content "www.gov.uk#{@document_base_path}"
+    expect(page).to_not have_content "www.gov.uk#{@document_base_path}-1"
+    expect(@publishing_api_call_with_content).to have_been_requested.times(4)
   end
 end
