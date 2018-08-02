e9b9f3748ffd27f1170fd135eaa5dddd31198a29
Ben Thorner <ben.thorner@digital.cabinet-office.gov.uk>
2018-08-02 11:52:16 +0100

    Replace formatted_params method with permit

--- app/controllers/document_associations_controller.rb
+++ app/controllers/document_associations_controller.rb
@@ -8,7 +8,7 @@ class DocumentAssociationsController < ApplicationController
   def update
     @document = Document.find(params[:id])
     # @TODO there might not actually be any changes to save here
-    @document.update_attribute(:associations, formatted_assocations)
+    @document.update_attribute(:associations, update_params)
     DocumentPublishingService.new.publish_draft(@document)
     redirect_to @document, notice: "Preview creation successful"
   rescue GdsApi::HTTPErrorResponse, SocketError => e
@@ -19,16 +19,6 @@ class DocumentAssociationsController < ApplicationController
 private
 
   def update_params
-    # As the fields might be arrays we can't just pass field names in as args
-    params.require(:associations).permit!
-  end
-
-  def formatted_assocations
-    @document.document_type_schema.associations.each_with_object({}) do |field, memo|
-      next unless update_params[field.id]
-      # @TODO there should probably be some sanity checks and storing of the
-      # linkable name here
-      memo[field.id] = update_params[field.id]
-    end
+    params.require(:associations).permit(topical_event: [])
   end
 end
--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -15,7 +15,7 @@ class DocumentsController < ApplicationController
 
   def update
     document = Document.find(params[:id])
-    document.update(document_update_params(document))
+    document.update_attributes(update_params(document))
     DocumentPublishingService.new.publish_draft(document)
     redirect_to document, notice: "Preview creation successful"
   rescue GdsApi::HTTPErrorResponse, SocketError => e
@@ -33,7 +33,7 @@ class DocumentsController < ApplicationController
 
 private
 
-  def document_update_params(document)
+  def update_params(document)
     contents_params = document.document_type_schema.contents.map(&:id)
     base_path = PathGeneratorService.new.path(document, params[:document][:title])
 
