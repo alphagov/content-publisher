8016bcc2a85ea67de7c6b9e9fa40fade68172664
Ben Thorner <ben.thorner@digital.cabinet-office.gov.uk>
2018-08-02 10:50:19 +0100

    Replace supertype thing as a first-class supertype
    Previously a static not-sure supertype was defined in the new documents
    controller, which required a proxy SupertypeThing class to handle the
    generation and processing of options. My original intention was simply
    to move this class into /services to highlight its existance, but
    instead this PR is an attempt to remove it altogether by adding a 'path'
    option to supertypes, behaving similarly to managed_elsewhere for docs.

--- app/services/supertype_schema.rb
+++ app/services/supertype_schema.rb
@@ -1,12 +1,13 @@
 # frozen_string_literal: true
 
 class SupertypeSchema
-  attr_reader :id, :label, :description
+  attr_reader :id, :label, :description, :managed_elsewhere
 
   def initialize(params = {})
     @id = params["id"]
     @label = params["label"]
     @description = params["description"]
+    @managed_elsewhere = params["managed_elsewhere"]
   end
 
   def self.all
@@ -19,6 +20,10 @@ class SupertypeSchema
   def self.find(schema_id)
     item = all.find { |schema| schema.id == schema_id }
     item || (raise RuntimeError, "Supertype #{schema_id} not found")
+  end
+
+  def managed_elsewhere_url
+    managed_elsewhere["path"]
   end
 
   def document_types
