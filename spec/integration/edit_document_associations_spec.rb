626760618315d740a3bddb989cd16383408a71e7
Ben Thorner <ben.thorner@digital.cabinet-office.gov.uk>
2018-08-03 11:46:45 +0100

    Loop over all document types to test all associations

--- spec/integration/edit_document_associations_spec.rb
+++ spec/integration/edit_document_associations_spec.rb
@@ -3,73 +3,79 @@
 require "spec_helper"
 
 RSpec.describe "Edit document associations", type: :feature do
-  scenario "User edits associations to a document" do
-    given_there_is_a_document_with_associations
-    when_i_visit_the_document_page
-    and_i_click_on_edit_associations
-    and_i_edit_the_associations
-    then_i_can_view_the_associations
-    and_the_preview_creation_succeeded
-  end
+  DocumentTypeSchema.all do |schema|
+    next if schema.migrated_elsewhere
 
-  def given_there_is_a_document_with_associations
-    publishing_api_has_linkables(linkables, document_type: "topical_event")
+    scenario "User edits associations to a document" do
+      @schema = schema
 
-    @document = create(:document, :with_associations_in_schema,
-                       associations: { topical_events: [linkables[2]["content_id"]] })
-
-    @associations = @document.document_type_schema.associations
-  end
-
-  def when_i_visit_the_document_page
-    visit document_path(@document)
-  end
-
-  def and_i_click_on_edit_associations
-    click_on "Edit associations"
-  end
-
-  def and_i_edit_the_associations
-    stub_any_publishing_api_put_content
-    @request = stub_publishing_api_put_content(Document.last.content_id, {})
-
-    @associations.each do |association|
-      select linkables[0]["title"], from: "associations[#{association.id}][]"
-      select linkables[1]["title"], from: "associations[#{association.id}][]"
-      unselect linkables[2]["title"], from: "associations[#{association.id}][]"
+      given_there_is_a_document_with_associations
+      when_i_visit_the_document_page
+      and_i_click_on_edit_associations
+      and_i_edit_the_associations
+      then_i_can_view_the_associations
+      and_the_preview_creation_succeeded
     end
 
-    click_on "Save"
-  end
+    def given_there_is_a_document_with_associations
+      publishing_api_has_linkables(linkables, document_type: "topical_event")
 
-  def then_i_can_view_the_associations
-    @associations.each do |association|
-      within("##{association.id}") do
-        expect(page).to have_content linkables[0]["title"]
-        expect(page).to have_content linkables[1]["title"]
-        expect(page).not_to have_content linkables[2]["title"]
+      @document = create(:document, document_type: @schema.id,
+                         associations: { topical_events: [linkables[2]["content_id"]] })
+
+      @associations = @document.document_type_schema.associations
+    end
+
+    def when_i_visit_the_document_page
+      visit document_path(@document)
+    end
+
+    def and_i_click_on_edit_associations
+      click_on "Edit associations"
+    end
+
+    def and_i_edit_the_associations
+      stub_any_publishing_api_put_content
+      @request = stub_publishing_api_put_content(Document.last.content_id, {})
+
+      @associations.each do |association|
+        select linkables[0]["title"], from: "associations[#{association.id}][]"
+        select linkables[1]["title"], from: "associations[#{association.id}][]"
+        unselect linkables[2]["title"], from: "associations[#{association.id}][]"
+      end
+
+      click_on "Save"
+    end
+
+    def then_i_can_view_the_associations
+      @associations.each do |association|
+        within("##{association.id}") do
+          expect(page).to have_content linkables[0]["title"]
+          expect(page).to have_content linkables[1]["title"]
+          expect(page).not_to have_content linkables[2]["title"]
+        end
       end
     end
-  end
 
-  def and_the_preview_creation_succeeded
-    expect(@request).to have_been_requested
-    expect(page).to have_content "Preview creation successful"
+    def and_the_preview_creation_succeeded
+      expect(@request).to have_been_requested
+      expect(page).to have_content "Preview creation successful"
 
-    expect(a_request(:put, /content/).with { |req|
-      expect(JSON.parse(req.body)["links"]).to eq(edition_links)
-    }).to have_been_requested
-  end
+      expect(a_request(:put, /content/).with { |req|
+        expect(JSON.parse(req.body)["links"]).to eq(edition_links)
+      }).to have_been_requested
+    end
 
-  def edition_links
-    Hash[@associations.map { |association|
-      [association.id, [linkables[0]["content_id"], linkables[1]["content_id"]]]
-    }]
-  end
+    def edition_links
+      Hash[@associations.map { |association|
+        [association.id, [linkables[0]["content_id"], linkables[1]["content_id"]]]
+      }]
+    end
 
-  def linkables
-    @linkables ||= 3.times.map do |i|
-      { "content_id" => SecureRandom.uuid, "title" => "Linkable #{i}" }
+    def linkables
+      @linkables ||= 3.times.map do |i|
+        { "content_id" => SecureRandom.uuid, "title" => "Linkable #{i}" }
+      end
     end
   end
 end
