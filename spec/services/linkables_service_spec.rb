62d26761c0ad619dc74d39885eee4b858bdfbcc4
Ben Thorner <ben.thorner@digital.cabinet-office.gov.uk>
2018-08-03 11:51:23 +0100

    Add missing unit spec for linkables service

--- spec/services/linkables_service_spec.rb
+++ spec/services/linkables_service_spec.rb
@@ -5,21 +5,20 @@ require 'spec_helper'
 RSpec.describe LinkablesService do
   describe "#select_options" do
     it "returns an array of titles with content_ids" do
-      linkables = [
-        {
-          "content_id" => "f5461d92-d36f-4077-81d2-1d5e5b57d585",
-          "title" => "UK Pavilion at Astana Expo 2017",
-          "publication_state" => "published",
-          "base_path" => "/government/topical-events/uk-pavilion-at-astana-expo-2017",
-          "internal_name" => "UK Pavilion at Astana Expo 2017 ",
-        }
-      ]
-      publishing_api_has_linkables(linkables, document_type: "topical_event")
-
+      linkable = { "content_id" => SecureRandom.uuid, "title" => "Linkable 1" }
+      publishing_api_has_linkables([linkable], document_type: "topical_event")
       service = LinkablesService.new("topical_event")
-      expect(service.select_options).to match([
-        ["UK Pavilion at Astana Expo 2017", "f5461d92-d36f-4077-81d2-1d5e5b57d585"],
-      ])
+      expect(service.select_options).to eq([[linkable["title"], linkable["content_id"]]])
+    end
+  end
+
+  describe "#by_content_id" do
+    it "returns the linkable with the matching content_id" do
+      linkable = { "content_id" => SecureRandom.uuid, "title" => "Linkable 1" }
+      publishing_api_has_linkables([linkable], document_type: "topical_event")
+      service = LinkablesService.new("topical_event")
+      expect(service.by_content_id(linkable["content_id"])).to eq linkable
+      expect(service.by_content_id("something-else")).to be_nil
     end
   end
 end
