b01c54b6fd48daff5a4c5ce7555699fb3bc66fac
Ben Thorner <ben.thorner@digital.cabinet-office.gov.uk>
2018-08-07 10:50:18 +0100

    Refactor our javascript to make it more modular
    We should avoid having all our Javascript in one file. A simple
    heuristic to split the code is to follow the layout of the views/
    directory, such that most of the code in application.js is now found in
    documents/edit.js.
    
    We should continue to restrict our Javascript so that it only executes
    on the (parts of) pages where it's actually needed. To help iterate this
    pattern, we've pulled out a 'restrict' function, which uses the
    data-module technique from govuk_publishing_components to scope the JS.

---
+++ app/assets/javascripts/documents/edit.js
@@ -0,0 +1,53 @@
+'use strict'
+
+restrict('edit-document-form', function (editDocumentForm) {
+  var urlPreview = document.getElementById('url-preview-id')
+  var basePath = document.getElementById('base-path-id')
+  var documentTitle = document.getElementById('document-title-id')
+  var noTitle = document.getElementById('no-title-id')
+  var errorGeneratingPath = document.getElementById('error-generating-path-id')
+
+  var showErrorMessage = function () {
+    urlPreview.setAttribute('class', 'app-hidden')
+    noTitle.setAttribute('class', 'app-hidden')
+    errorGeneratingPath.removeAttribute('class')
+  }
+
+  var showNoTitleMessage = function () {
+    noTitle.removeAttribute('class')
+    urlPreview.setAttribute('class', 'app-hidden')
+    errorGeneratingPath.setAttribute('class', 'app-hidden')
+  }
+
+  var showPathPreview = function (path) {
+    noTitle.setAttribute('class', 'app-hidden')
+    errorGeneratingPath.setAttribute('class', 'app-hidden')
+    urlPreview.removeAttribute('class')
+    basePath.innerHTML = path
+  }
+
+  documentTitle.onblur = function () {
+    var path = editDocumentForm.getAttribute('data-generate-path-path')
+    var url = new URL(document.location.origin + path)
+
+    if (!documentTitle.value) {
+      showNoTitleMessage()
+      return
+    } else {
+      url.searchParams.append('title', documentTitle.value)
+    }
+
+    window.fetch(url, {
+      credentials: 'include'
+    }).then(function (response) {
+      if (!response.ok) {
+        throw Error('Unable to generate response.')
+      }
+      response.json().then(function (result) {
+        showPathPreview(result['base_path'])
+      })
+    }).catch(showErrorMessage)
+  }
+
+  documentTitle.onblur()
+})
---
+++ app/assets/javascripts/restrict.js
@@ -0,0 +1,7 @@
+'use strict'
+
+function restrict (module, callback) {
+  var query = '[data-module="' + module + '"]'
+  var elements = document.querySelectorAll(query)
+  elements.forEach(callback)
+}
