6a13a776343bf052eb48f1f0dd7d59254faa5c1d
Tijmen Brommet <tijmen@gmail.com>
2018-08-14 17:46:49 +0100

    Move publication states into Document

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,7 +1,18 @@
 # frozen_string_literal: true
 
 class Document < ApplicationRecord
-  validates_inclusion_of :publication_state, in: PublicationState::STATES
+  PUBLICATION_STATES = %w[
+    newly_created
+    changes_not_sent_to_draft
+    sending_to_draft
+    sent_to_draft
+    error_sending_to_draft
+    publishing
+    live_on_govuk
+    error_in_publishing
+  ].freeze
+
+  validates_inclusion_of :publication_state, in: PUBLICATION_STATES
 
   def document_type_schema
     DocumentTypeSchema.find(document_type)
