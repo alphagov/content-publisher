d2daa513f181354c9c17faf67c8a1b17771aee06
Ben Thorner <ben.thorner@digital.cabinet-office.gov.uk>
2018-08-15 11:34:29 +0100

    Inject translations so we can test them all
    This means we can check that the all translations work correctly,
    providing every page containing a translations is visited in a test.
    Enabling this uncovered a key error for govspeak guidance.

--- spec/factories/document_type_schema_factory.rb
+++ spec/factories/document_type_schema_factory.rb
@@ -5,6 +5,37 @@ FactoryBot.define do
     supertype { "not-sure" }
     id { SecureRandom.hex(4) }
     document_type { SecureRandom.alphanumeric(8) }
-    initialize_with { DocumentTypeSchema.create(attributes.stringify_keys) }
+    contents { [] }
+
+    transient do
+      translations do
+        {
+          label: id,
+          guidance: {
+            title: {
+              title: SecureRandom.alphanumeric(8),
+              body: SecureRandom.alphanumeric(8)
+            },
+            summary: {
+              title: SecureRandom.alphanumeric(8),
+              body: SecureRandom.alphanumeric(8)
+            }
+          }.merge(Hash[contents.map { |schema|
+            [schema["id"], {
+              title: SecureRandom.alphanumeric(8),
+              body: SecureRandom.alphanumeric(8)
+            }]
+          }])
+        }
+      end
+    end
+
+    initialize_with do
+      I18n.backend.store_translations :en, formats: {
+        document_types: { id => translations }
+      }
+
+      DocumentTypeSchema.create(attributes.stringify_keys)
+    end
   end
 end
