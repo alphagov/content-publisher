90e00f6ce8e938918c74d29eef2cfa9ec78fa1e7
Ben Thorner <ben.thorner@digital.cabinet-office.gov.uk>
2018-08-15 11:49:42 +0100

    Move other labels out of document types config

--- spec/factories/document_type_schema_factory.rb
+++ spec/factories/document_type_schema_factory.rb
@@ -6,35 +6,44 @@ FactoryBot.define do
     id { SecureRandom.hex(4) }
     document_type { SecureRandom.alphanumeric(8) }
     contents { [] }
+    associations { [] }
 
     transient do
       translations do
         {
-          label: id,
-          guidance: {
-            title: {
-              title: SecureRandom.alphanumeric(8),
-              body: SecureRandom.alphanumeric(8)
-            },
-            summary: {
-              title: SecureRandom.alphanumeric(8),
-              body: SecureRandom.alphanumeric(8)
+          formats: {
+            associations: Hash[associations.map { |schema|
+              [schema["id"], {
+                label: SecureRandom.alphanumeric(8)
+              }]
+            }],
+            document_types: {
+              id => {
+                label: id,
+                guidance: {
+                  title: {
+                    title: SecureRandom.alphanumeric(8),
+                    body: SecureRandom.alphanumeric(8)
+                  },
+                  summary: {
+                    title: SecureRandom.alphanumeric(8),
+                    body: SecureRandom.alphanumeric(8)
+                  }
+                }.merge(Hash[contents.map { |schema|
+                  [schema["id"], {
+                    title: SecureRandom.alphanumeric(8),
+                    body: SecureRandom.alphanumeric(8)
+                  }]
+                }])
+              }
             }
-          }.merge(Hash[contents.map { |schema|
-            [schema["id"], {
-              title: SecureRandom.alphanumeric(8),
-              body: SecureRandom.alphanumeric(8)
-            }]
-          }])
+          }
         }
       end
     end
 
     initialize_with do
-      I18n.backend.store_translations :en, formats: {
-        document_types: { id => translations }
-      }
-
+      I18n.backend.store_translations :en, translations
       DocumentTypeSchema.create(attributes.stringify_keys)
     end
   end
