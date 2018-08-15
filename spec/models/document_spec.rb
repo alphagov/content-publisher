7a38e94d8ec85cf244207ef5c7a5c93802490731
Ben Thorner <ben.thorner@digital.cabinet-office.gov.uk>
2018-08-15 15:09:30 +0100

    Merge publication states translations from rebase
    This meant the documents.yml translation file grew quite large,
    suggesting we should split files by page instead.

--- spec/models/document_spec.rb
+++ spec/models/document_spec.rb
@@ -6,8 +6,8 @@ RSpec.describe Document do
   describe "PUBLICATION_STATES" do
     it "has correct translations for each state" do
       Document::PUBLICATION_STATES.each do |state|
-        I18n.t!("publication_state.#{state}.name")
-        I18n.t!("publication_state.#{state}.description")
+        I18n.t!("documents.show.publication_state.#{state}.name")
+        I18n.t!("documents.show.publication_state.#{state}.description")
       end
     end
   end
