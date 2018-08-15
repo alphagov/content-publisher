4c9e3025e2bbd571fa5f366c73e8a44530740082
Tijmen Brommet <tijmen@gmail.com>
2018-08-15 12:22:44 +0100

    Start validating the publication state
    We'll soon have a lot more states and the risk of typos will increase.

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class Document < ApplicationRecord
+  validates_inclusion_of :publication_state, in: PublicationState::STATES
+
   def document_type_schema
     DocumentTypeSchema.find(document_type)
   end
---
+++ app/services/publication_state.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+class PublicationState
+  STATES = %w[
+    newly_created
+  ].freeze
+end
