cc209030025e4035694bf8a8b2c6dec7c96454dd
Tijmen Brommet <tijmen@gmail.com>
2018-09-03 12:06:59 +0100

    Link to the separate preview page
    This changes the action link to go to the new separate preview page, so
    the user can see the preview on mobile, desktop and Google.
    
    https://trello.com/c/i2Fi5qNQ

--- spec/features/preview_document_spec.rb
+++ spec/features/preview_document_spec.rb
@@ -10,11 +10,12 @@ RSpec.feature "Preview a document" do
   end
 
   def given_there_is_a_document_in_draft
-    @document = create(:document, base_path: "/foo/foo", content_id: "d2547c42-8ed3-49f5-baeb-6112f98c2bf9")
+    @document = create(:document, publication_state: "sent_to_draft", base_path: "/foo/foo", content_id: "d2547c42-8ed3-49f5-baeb-6112f98c2bf9")
   end
 
   def when_i_visit_the_preview_page
-    visit preview_document_path(@document)
+    visit document_path(@document)
+    click_on I18n.t("documents.show.actions.view_draft")
   end
 
   def then_i_see_the_previews
