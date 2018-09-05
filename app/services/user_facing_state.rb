f9628794700fa75a6b1e3669f978c30e2bc3e325
Tijmen Brommet <tijmen@gmail.com>
2018-09-05 09:52:56 +0100

    Clean up user facing state a bit

--- app/services/user_facing_state.rb
+++ app/services/user_facing_state.rb
@@ -2,19 +2,20 @@
 
 class UserFacingState
   attr_reader :document
+  delegate :review_state, :publication_state, to: :document
 
   def initialize(document)
     @document = document
   end
 
   def to_s
-    if document.review_state == "submitted_for_review"
+    if review_state == "submitted_for_review"
       "submitted_for_review"
-    elsif document.publication_state.in?(%w[changes_not_sent_to_draft sent_to_draft sending_to_draft error_sending_to_draft sending_to_live])
+    elsif publication_state.in?(%w[changes_not_sent_to_draft sent_to_draft sending_to_draft error_sending_to_draft sending_to_live])
       "draft"
-    elsif document.review_state == "published_without_review"
+    elsif review_state == "published_without_review"
       "published_but_needs_2i"
-    elsif document.publication_state == "sent_to_live"
+    elsif publication_state == "sent_to_live"
       "published"
     else
       raise "Encountered an unknown user facing state. review_state: #{review_state}, publication_state: #{publication_state}"
