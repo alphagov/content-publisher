b0be206856d48b2d694d7301517024bedf181376
Tijmen Brommet <tijmen@gmail.com>
2018-09-06 08:29:09 +0100

    Consistently handle transitional states
    This makes sure that we handle the transitional states
    `sending_to_draft` and `sending_to_live` consistently. As discussed in
    https://trello.com/c/VkVrVGeB, users don't need to see these states.
    We'll consider something `sending_to_draft` to be the same as
    `sent_to_draft`, and `sending_to_live` the same as `sent_to_live`.
    
    https://trello.com/c/VkVrVGeB

--- app/services/user_facing_state.rb
+++ app/services/user_facing_state.rb
@@ -11,11 +11,11 @@ class UserFacingState
   def to_s
     if review_state == "submitted_for_review"
       "submitted_for_review"
-    elsif publication_state.in?(%w[changes_not_sent_to_draft sent_to_draft sending_to_draft error_sending_to_draft sending_to_live])
+    elsif publication_state.in?(%w[changes_not_sent_to_draft sent_to_draft sending_to_draft error_sending_to_draft])
       "draft"
     elsif review_state == "published_without_review"
       "published_but_needs_2i"
-    elsif publication_state.in?(%w[sent_to_live error_sending_to_live])
+    elsif publication_state.in?(%w[sent_to_live sending_to_live error_sending_to_live])
       "published"
     else
       raise "Encountered an unknown user facing state. review_state: #{review_state}, publication_state: #{publication_state}"
