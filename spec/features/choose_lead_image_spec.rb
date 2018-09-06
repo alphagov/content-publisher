d7500656607c8b0d051349c965f12fc5c1016951
Emma Beynon <emma.beynon@digital.cabinet-office.gov.uk>
2018-09-06 13:12:10 +0000

    Display if image is lead image

--- spec/features/choose_lead_image_spec.rb
+++ spec/features/choose_lead_image_spec.rb
@@ -6,13 +6,17 @@ RSpec.feature "Choose a lead image" do
     when_i_visit_the_summary_page
     and_i_visit_the_lead_images_page
     then_i_should_be_able_to_see_a_list_of_existing_images
+    when_i_select_an_image_to_be_the_lead_image
   end
 
   def given_there_is_a_document_with_existing_images
     document_type_schema = build(:document_type_schema, lead_image: true)
     document = create(:document, document_type: document_type_schema.id)
-    create(:image, document: document, filename: "image-1.jpg")
+    @image1 = create(:image, document: document, filename: "image-1.jpg",
+                              alt_text: "image 1 alt text", caption: "image 1 caption",
+                              credit: "image 1 credit")
     create(:image, document: document, filename: "image-2.jpg")
+    document.update(lead_image: @image1)
   end
 
   def when_i_visit_the_summary_page
@@ -25,8 +29,17 @@ RSpec.feature "Choose a lead image" do
 
   def then_i_should_be_able_to_see_a_list_of_existing_images
     expect(find("#image-0")["src"]).to include("image-1.jpg")
-    expect(page).to have_content("image-1.jpg")
+    within("td#image-0-metadata") do
+      expect(page).to have_content(@image1.alt_text)
+      expect(page).to have_content(@image1.caption)
+      expect(page).to have_content(@image1.credit)
+      expect(page).to have_content("Lead image")
+    end
+
     expect(find("#image-1")["src"]).to include("image-2.jpg")
-    expect(page).to have_content("image-2.jpg")
+  end
+
+  def when_i_select_an_image_to_be_the_lead_image
+    click_on()
   end
 end
