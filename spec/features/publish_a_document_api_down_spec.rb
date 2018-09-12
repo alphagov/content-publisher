ceb07c5d0bbeafcd4b9aaffd5e1cfce3894dda06
Ben Thorner <ben.thorner@digital.cabinet-office.gov.uk>
2018-09-12 10:39:53 +0100

    Fix failing tests due to bad translation key

--- spec/features/publish_a_document_api_down_spec.rb
+++ spec/features/publish_a_document_api_down_spec.rb
@@ -41,6 +41,6 @@ RSpec.feature "Publishing a document when the API is down" do
 
   def then_i_see_the_publish_succeeded
     expect(@request).to have_been_requested.twice
-    expect(page).to have_content(I18n.t("publish_document.published_document.reviewed.title"))
+    expect(page).to have_content(I18n.t("publish_document.published.reviewed.title"))
   end
 end
