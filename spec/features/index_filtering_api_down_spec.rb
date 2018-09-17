6eea9140c152b17d2d5f9ddba4e54639ec36e0b1
Ben Thorner <ben.thorner@digital.cabinet-office.gov.uk>
2018-09-17 14:40:39 +0100

    Handle no Publishing API for organisation filters

---
+++ spec/features/index_filtering_api_down_spec.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+RSpec.feature "User filters documents without Publishing API" do
+  scenario "User filters documents without Publishing API" do
+    given_there_are_some_documents
+    and_the_publishing_api_is_down
+    when_i_visit_the_index_page
+    then_i_cannot_filter_by_organisation
+  end
+
+  def given_there_are_some_documents
+    create(:document)
+  end
+
+  def and_the_publishing_api_is_down
+    publishing_api_isnt_available
+  end
+
+  def when_i_visit_the_index_page
+    visit documents_path
+  end
+
+  def then_i_cannot_filter_by_organisation
+    expect(all("#organisation option").count).to eq 1
+  end
+end
