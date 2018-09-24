d28bd381f7b60c4618f23eff57b5c0a0aef9aebf
Emma Beynon <emma.beynon@digital.cabinet-office.gov.uk>
2018-09-24 14:28:35 +0000

    Display last edited by user on index page
    In the event that a document has not yet been edited, it makes sense to show
    the name of the user that created it as that was the latest interaction with
    the document.
    
    In the event that a document has no creator, for example if the document has
    been imported and the user is not present in this application, then we display
    the same 'unknown' value as the 'Creator' field.

--- spec/features/workflow/viewing_a_document_with_no_creator_or_editor_spec.rb
+++ spec/features/workflow/viewing_a_document_with_no_creator_or_editor_spec.rb
@@ -1,13 +1,14 @@
 # frozen_string_literal: true
 
-RSpec.feature "Viewing a document with no creator" do
-  scenario "User finds an unedited document without a creator and views it" do
+RSpec.feature "Viewing a document with no creator or editor" do
+  scenario "User finds a document without a creator or editor and views it" do
     given_there_is_a_document_with_no_creator
     when_i_visit_the_index_page
     then_i_see_we_dont_know_who_created_the_document
+    and_i_see_the_document_has_no_last_editor
     when_i_visit_the_document_page
     then_i_again_see_we_dont_know_who_created_the_document
-    then_i_see_the_document_has_no_last_editor
+    and_i_again_see_the_document_has_no_last_editor
   end
 
   def given_there_is_a_document_with_no_creator
@@ -25,6 +26,13 @@ RSpec.feature "Viewing a document with no creator" do
     )
   end
 
+  def and_i_see_the_document_has_no_last_editor
+    expect(page).to have_content(
+      I18n.t("documents.index.search_results.last_edited_by",
+        user: I18n.t("documents.index.search_results.unknown_creator")),
+    )
+  end
+
   def when_i_visit_the_document_page
     click_on @document.title
   end
@@ -36,7 +44,7 @@ RSpec.feature "Viewing a document with no creator" do
     )
   end
 
-  def then_i_see_the_document_has_no_last_editor
+  def and_i_again_see_the_document_has_no_last_editor
     expect(page).to have_content(
       I18n.t("documents.show.metadata.last_edited_by") + ": " +
       I18n.t("documents.show.metadata.unknown_creator"),
