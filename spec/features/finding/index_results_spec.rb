e3792f23ed6238a0be1ec103c7e136c34687945e
Emma Beynon <emma.beynon@digital.cabinet-office.gov.uk>
2018-09-25 13:56:43 +0000

    Add index page results test
    This adds more comprehensive test coverage for the data that appears in the
    results on the index page.

---
+++ spec/features/finding/index_results_spec.rb
@@ -0,0 +1,49 @@
+# frozen_string_literal: true
+
+RSpec.feature "Users views document results on index page" do
+  scenario "User views document information on index page" do
+    given_there_is_a_document
+    when_i_visit_the_index_page
+    then_i_can_see_the_document_title
+    and_i_can_see_the_document_type
+    and_i_can_see_the_document_creator
+    and_i_can_see_the_document_state
+    and_i_can_see_the_document_last_editor
+  end
+
+  def given_there_is_a_document
+    creator = create(:user)
+    editor = create(:user)
+    create(:document, creator: creator, last_editor: editor)
+  end
+
+  def when_i_visit_the_index_page
+    visit documents_path
+  end
+
+  def then_i_can_see_the_document_title
+    expect(page).to have_content(Document.last.title)
+  end
+
+  def and_i_can_see_the_document_type
+    expect(page).to have_content(Document.last.document_type_schema.label)
+  end
+
+  def and_i_can_see_the_document_creator
+    expect(page).to have_content(
+      I18n.t("documents.index.search_results.creator",
+        creator: Document.last.creator.name),
+    )
+  end
+
+  def and_i_can_see_the_document_state
+    expect(page).to have_content("Draft")
+  end
+
+  def and_i_can_see_the_document_last_editor
+    expect(page).to have_content(
+      I18n.t("documents.index.search_results.last_edited_by",
+        user: Document.last.last_editor.name),
+    )
+  end
+end
