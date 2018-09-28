1fc62d62a0fa434ec238f2bc8d3389eb0094b0d2
Ben Thorner <ben.thorner@digital.cabinet-office.gov.uk>
2018-09-28 15:38:47 +0100

    Shift where upload spec checks for Pub API down
    The previous spec that covered uploading an image with the Publishing
    API down is now covered by the new crop spec. But the next stage in the
    upload ('Save and choose') also required the Publishing API but was not
    tested, so I've changed this spec to target this endpoint instead.

--- spec/features/editing_images/upload_lead_image_publishing_api_down_spec.rb
+++ spec/features/editing_images/upload_lead_image_publishing_api_down_spec.rb
@@ -3,11 +3,15 @@
 RSpec.feature "Upload a lead image when Publishing API is down" do
   scenario do
     given_there_is_a_document
+    when_i_visit_the_summary_page
+    then_i_see_there_is_no_lead_image
     when_i_visit_the_lead_images_page
+    and_i_upload_a_new_image
+    and_i_crop_the_image
     and_the_publishing_api_is_down
-    when_i_upload_an_image
-    then_i_see_the_image_is_the_lead_image
-    and_i_see_the_preview_creation_failed
+    and_i_fill_in_the_metadata
+    then_i_see_the_new_lead_image
+    and_the_preview_creation_failed
   end
 
   def given_there_is_a_document
@@ -15,36 +19,48 @@ RSpec.feature "Upload a lead image when Publishing API is down" do
     create(:document, document_type: document_type_schema.id)
   end
 
+  def when_i_visit_the_summary_page
+    visit document_path(Document.last)
+  end
+
+  def then_i_see_there_is_no_lead_image
+    expect(page).to have_content(I18n.t("documents.show.lead_image.no_lead_image"))
+  end
+
   def when_i_visit_the_lead_images_page
-    visit document_lead_image_path(Document.last)
+    click_on "Change Lead image"
+  end
+
+  def and_i_upload_a_new_image
+    @asset_id = SecureRandom.uuid
+    @asset_url = "https://asset-manager.test.gov.uk/media/#{@asset_id}/1000x1000.jpg"
+    asset_manager_receives_an_asset(@asset_url)
+    find('form input[type="file"]').set(Rails.root.join(file_fixture("1000x1000.jpg")))
+    click_on "Upload"
+  end
+
+  def and_i_crop_the_image
+    asset_manager_delete_asset(@asset_id)
+    stub_publishing_api_put_content(Document.last.content_id, {})
+    click_on "Crop image"
+    WebMock.reset!
   end
 
   def and_the_publishing_api_is_down
     publishing_api_isnt_available
   end
 
-  def when_i_upload_an_image
-    @asset_id = SecureRandom.uuid
-    asset_url = "https://asset-manager.test.gov.uk/media/#{@asset_id}/960x640.jpg"
-    asset_manager_receives_an_asset(asset_url)
-
-    find('form input[type="file"]').set(file_fixture("960x640.jpg"))
-    click_on "Upload"
-
-    asset_manager_delete_asset(@asset_id)
-    click_on "Crop image"
-
-    fill_in "alt_text", with: "Some alt text"
+  def and_i_fill_in_the_metadata
     click_on "Save and choose"
   end
 
-  def then_i_see_the_image_is_the_lead_image
+  def then_i_see_the_new_lead_image
     within("#image-#{Image.last.id}") do
       expect(page).to have_content(I18n.t("document_lead_image.index.lead_image"))
     end
   end
 
-  def and_i_see_the_preview_creation_failed
+  def and_the_preview_creation_failed
     expect(page).to have_content(I18n.t("document_lead_image.index.flashes.api_error.title"))
   end
 end
