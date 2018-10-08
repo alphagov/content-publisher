35b422efce887cd02133ed7e9d22a59408990570
Ben Thorner <ben.thorner@digital.cabinet-office.gov.uk>
2018-10-08 12:16:20 +0100

    Make sure we do a 404 when we can't find a doc

--- app/models/document.rb
+++ app/models/document.rb
@@ -45,7 +45,7 @@ class Document < ApplicationRecord
 
   def self.find_by_param(content_id_and_locale)
     content_id, locale = content_id_and_locale.split(":")
-    Document.find_by(content_id: content_id, locale: locale)
+    Document.find_by!(content_id: content_id, locale: locale)
   end
 
   def user_facing_state
