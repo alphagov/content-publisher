8770bae3bf3efc5fe190ea9ae581a025e4426e1d
Ben Thorner <ben.thorner@digital.cabinet-office.gov.uk>
2018-10-19 12:02:08 +0100

    Show topics error page when the API is down

--- app/controllers/document_topics_controller.rb
+++ app/controllers/document_topics_controller.rb
@@ -1,6 +1,11 @@
 # frozen_string_literal: true
 
 class DocumentTopicsController < ApplicationController
+  rescue_from GdsApi::BaseError do |e|
+    Rails.logger.error(e)
+    render "#{action_name}_api_down", status: :service_unavailable
+  end
+
   def edit
     @document = Document.find_by_param(params[:document_id])
     @tree = TopicsService.new.tree
