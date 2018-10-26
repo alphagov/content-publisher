9d0f4ea14beb248a12dc12ade74e52dfb0cd8b46
Murilo Dal Ri <murilo.dalri@digital.cabinet-office.gov.uk>
2018-10-26 13:49:14 +0100

    Show topic breadcrumbs on document summary page
    In order to show the full lineage of a topic, this adds a method to the
    TopicsService to make use of the additional 'parent' attribute of each
    topic in the index, which is now set as part of unrolling the taxonomy.

--- app/services/topics_service.rb
+++ app/services/topics_service.rb
@@ -5,31 +5,46 @@ require "gds_api/publishing_api_v2"
 class TopicsService
   GOVUK_HOMEPAGE_CONTENT_ID = "f3bbdec2-0e62-4520-a7fd-6ffd5d36e03a"
   CACHE_OPTIONS = { expires_in: 5.minutes, race_condition_ttl: 10.seconds }.freeze
-  TOPICS_TIMEOUT = 4.seconds
+  TOPIC_INDEX_TIMEOUT = 4.seconds
 
-  def topics
+  def topic_index
     Rails.cache.fetch("topics", CACHE_OPTIONS) do
-      all_topics = {}
+      index = {}
 
-      all_topics[GOVUK_HOMEPAGE_CONTENT_ID] = {
+      index[GOVUK_HOMEPAGE_CONTENT_ID] = {
         title: "GOV.UK Homepage",
         children: level_one_topics.map { |level_one_topic| level_one_topic["content_id"] },
       }
 
-      unroll(all_topics, level_one_topics)
-      all_topics
+      unroll(index, level_one_topics, GOVUK_HOMEPAGE_CONTENT_ID)
+      index
     end
+  end
+
+  def topics_for_document(content_id)
+    publishing_api.get_links(content_id)
+      .dig("links", "taxons").to_a
+      .map { |topic_content_id| topic_index[topic_content_id] }
+  end
+
+  def topic_breadcrumb(topic)
+    parent = topic_index[topic[:parent]]
+    parent ? [topic] + topic_breadcrumb(parent) : [topic]
   end
 
 private
 
-  def unroll(all_topics, topics)
+  def unroll(index, topics, parent_topic)
     topics.each do |topic|
       child_topics = topic.dig("links", "child_taxons").to_a
       child_topic_content_ids = child_topics.map { |child_topic| child_topic["content_id"] }
 
-      all_topics[topic["content_id"]] = { title: topic["title"], children: child_topic_content_ids }
-      unroll(all_topics, child_topics)
+      index[topic["content_id"]] = {
+        title: topic["title"],
+        children: child_topic_content_ids,
+        parent: parent_topic["content_id"],
+      }
+      unroll(index, child_topics, topic)
     end
   end
 
@@ -40,7 +55,7 @@ private
       .dig("expanded_links", "level_one_taxons")
 
     @level_one_topics.each do |level_one_topic|
-      raise GdsApi::TimedOutException.new if Time.zone.now - start_time > TOPICS_TIMEOUT
+      raise GdsApi::TimedOutException.new if Time.zone.now - start_time > TOPIC_INDEX_TIMEOUT
       level_one_topic_content_id = level_one_topic["content_id"]
 
       level_one_topic_links = publishing_api.get_expanded_links(level_one_topic_content_id)
--- spec/features/editing_topics/show_topics_spec.rb
+++ spec/features/editing_topics/show_topics_spec.rb
@@ -1,22 +1,44 @@
 # frozen_string_literal: true
 
-RSpec.feature "Show all the topics" do
+RSpec.feature "Show the topics for a document" do
   scenario do
     given_there_is_a_document
-    when_i_visit_the_document_page
-    and_i_try_to_change_the_topics
-    then_i_see_all_of_the_topics
+    when_the_document_has_no_topics
+    and_i_visit_the_document_page
+    then_i_see_there_are_no_topics
+
+    when_the_document_has_a_topic
+    and_i_visit_the_document_page
+    then_i_see_the_topic_breadcrumb
   end
 
   def given_there_is_a_document
-    create :document
+    @document = create :document
   end
 
-  def when_i_visit_the_document_page
+  def when_the_document_has_no_topics
+    publishing_api_has_links(
+      "content_id" => @document.content_id,
+      "links" => {},
+    )
+  end
+
+  def and_i_visit_the_document_page
     visit document_path(Document.last)
   end
 
-  def and_i_try_to_change_the_topics
+  def then_i_see_there_are_no_topics
+    expect(page).to have_content(I18n.t("documents.show.topics.no_topics"))
+  end
+
+  def when_the_document_has_a_topic
+    publishing_api_has_links(
+      "content_id" => @document.content_id,
+      "links" => {
+        "taxons" => %w(level_three_topic),
+      },
+    )
+
     # GOV.UK homepage
     publishing_api_has_expanded_links(
       "content_id" => "f3bbdec2-0e62-4520-a7fd-6ffd5d36e03a",
@@ -50,13 +72,13 @@ RSpec.feature "Show all the topics" do
         ],
       },
     )
-
-    click_on "Change Topics"
   end
 
-  def then_i_see_all_of_the_topics
-    expect(page).to have_content("Level One Topic")
-    expect(page).to have_content("Level Two Topic")
-    expect(page).to have_content("Level Three Topic")
+  def then_i_see_the_topic_breadcrumb
+    within(".topic-breadcrumb") do
+      expect(page).to have_content("Level One Topic")
+      expect(page).to have_content("Level Two Topic")
+      expect(page).to have_content("Level Three Topic")
+    end
   end
 end
