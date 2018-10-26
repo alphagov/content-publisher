a7c9cbae07091f609dcd1e9920289dd15de9fdf7
Kevin Dew <kevindew@me.com>
2018-10-26 10:43:11 +0100

    Restrict access to topics without permission
    A user requires the pre_release_features permission to access topics as
    this functionality is not complete yet, this is intended to start a
    pattern of how to approach adding new functionality that is not publicly
    available.

--- app/controllers/document_topics_controller.rb
+++ app/controllers/document_topics_controller.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 class DocumentTopicsController < ApplicationController
+  include GDS::SSO::ControllerMethods
+  before_action { authorise_user!(User::PRE_RELEASE_FEATURES_PERMISSION) }
+
   rescue_from GdsApi::BaseError do |e|
     Rails.logger.error(e)
     render "#{action_name}_api_down", status: :service_unavailable
