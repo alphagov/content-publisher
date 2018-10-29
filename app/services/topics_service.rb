7b13a888c1633b944f2a7a9784171b3a30d4a7bc
Ben Thorner <ben.thorner@digital.cabinet-office.gov.uk>
2018-10-29 13:17:16 +0000

    Make sure we timeout early when getting all topics
    Creating the topics index requires looping over all of the L1 topics,
    each of which can potentially take a long time to yield a response. If
    the Publishing API is running slow, we accumulate a very long response
    time at the application level, causing Nginx to time out.
    
    This adds some superficial logic to monitor the total time it's taken to
    accumulate the L1 topics (and the L0 topic) and abort if it's taking too
    long. This means we can show an in-app error to the user, rather than a
    Gateway Timeout at the Nginx level.

--- app/services/topics_service.rb
+++ app/services/topics_service.rb
@@ -5,6 +5,7 @@ require "gds_api/publishing_api_v2"
 class TopicsService
   GOVUK_HOMEPAGE_CONTENT_ID = "f3bbdec2-0e62-4520-a7fd-6ffd5d36e03a"
   CACHE_OPTIONS = { expires_in: 5.minutes, race_condition_ttl: 10.seconds }.freeze
+  TOPICS_TIMEOUT = 4.seconds
 
   def topics
     Rails.cache.fetch("topics", CACHE_OPTIONS) do
@@ -33,11 +34,15 @@ private
   end
 
   def level_one_topics
+    start_time = Time.zone.now
+
     @level_one_topics ||= publishing_api.get_expanded_links(GOVUK_HOMEPAGE_CONTENT_ID)
       .dig("expanded_links", "level_one_taxons")
 
     @level_one_topics.each do |level_one_topic|
+      raise GdsApi::TimedOutException.new if Time.zone.now - start_time > TOPICS_TIMEOUT
       level_one_topic_content_id = level_one_topic["content_id"]
+
       level_one_topic_links = publishing_api.get_expanded_links(level_one_topic_content_id)
       level_one_topic["links"] = level_one_topic_links["expanded_links"]
     end
@@ -46,10 +51,6 @@ private
   end
 
   def publishing_api
-    GdsApi::PublishingApiV2.new(
-      Plek.new.find("publishing-api"),
-      disable_cache: true,
-      bearer_token: ENV["PUBLISHING_API_BEARER_TOKEN"] || "example",
-    )
+    GdsApi.publishing_api_v2(timeout: 1)
   end
 end
