cbaa4d992cb23223ff0a633827264902db7e0ae3
Ben Thorner <ben.thorner@digital.cabinet-office.gov.uk>
2018-11-06 13:06:10 +0000

    Stop sending update_type on publish
    Turns out this is deprecated.
    
    https://github.com/alphagov/publishing-api/blob/45b3b427c8f4330fae45367cd79ade3961ff75ee/app/commands/v2/publish.rb#L69

--- app/services/document_publishing_service.rb
+++ app/services/document_publishing_service.rb
@@ -14,7 +14,9 @@ class DocumentPublishingService
   def publish(document, review_state)
     document.update!(publication_state: "sending_to_live", review_state: review_state)
     publish_assets(document.images)
-    GdsApi.publishing_api_v2.publish(document.content_id, document.update_type, locale: document.locale)
+
+    # Sending update_type in .publish is deprecated (should be in payload instead)
+    GdsApi.publishing_api_v2.publish(document.content_id, nil, locale: document.locale)
     document.update!(publication_state: "sent_to_live", has_live_version_on_govuk: true)
   rescue GdsApi::BaseError => e
     GovukError.notify(e)
