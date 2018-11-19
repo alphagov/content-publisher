8ee9fdd58f7d8f954f4133cea5b967a913d95607
Leena Gupte <leena.gupte@digital.cabinet-office.gov.uk>
2018-11-19 13:03:06 +0000

    Add a service to retire a document
    Build the backend service that withdraws ("retires" in the Content
    Publisher terminology) from publishing-api.
    
    Eventually this service will form the foundation for the full feature
    that will allow users to retire content themselves, but for now it will
    be used by a rake task that will be added in a later commit to allow
    content to be retired manually.

---
+++ app/services/document_unpublishing_service.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+class DocumentUnpublishingService
+  def retire(document, explanatory_note)
+    GdsApi.publishing_api_v2.unpublish(document.content_id, type: "withdrawal", explanation: explanatory_note)
+  end
+end
---
+++ spec/services/document_unpublishing_service_spec.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+RSpec.describe DocumentUnpublishingService do
+  describe "#retire" do
+    it "withdraws a document in publishing-api with an explanatory note" do
+      document = create(:document)
+      explanatory_note = "The document is out of date"
+
+      stub_publishing_api_unpublish(document.content_id, body: { type: "withdrawal", explanation: explanatory_note })
+      DocumentUnpublishingService.new.retire(document, explanatory_note)
+
+      assert_publishing_api_unpublish(document.content_id, type: "withdrawal", explanation: explanatory_note)
+    end
+  end
+end
