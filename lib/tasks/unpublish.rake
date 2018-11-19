9ad08718e58f6668151ec04af62635748ee018c3
Leena Gupte <leena.gupte@digital.cabinet-office.gov.uk>
2018-11-19 18:09:48 +0000

    Add a rake task to remove documents manually

--- lib/tasks/unpublish.rake
+++ lib/tasks/unpublish.rake
@@ -10,4 +10,13 @@ namespace :unpublish do
 
     DocumentUnpublishingService.new.retire(document, args.explanatory_note) if document.present?
   end
+
+  desc "Remove a document"
+  task :remove_document, [:base_path] => :environment do |_, args|
+    raise "Missing base_path parameter" unless args.base_path
+
+    document = Document.find_by(base_path: args.base_path)
+
+    DocumentUnpublishingService.new.remove(document) if document.present?
+  end
 end
--- spec/features/workflow/unpublishing_spec.rb
+++ spec/features/workflow/unpublishing_spec.rb
@@ -26,4 +26,23 @@ RSpec.feature "Unpublish documents rake tasks" do
       expect { Rake::Task["unpublish:retire_document"].invoke("/a-base-path") }.to raise_error("Missing explanatory_note parameter")
     end
   end
+
+  describe "unpublish:remove_document" do
+    before do
+      Rake::Task["unpublish:remove_document"].reenable
+    end
+
+    it "runs the rake task to remove a document" do
+      document = create(:document, base_path: "/a-base-path")
+      stub_publishing_api_unpublish(document.content_id, body: { type: "gone" })
+
+      expect_any_instance_of(DocumentUnpublishingService).to receive(:remove).with(document)
+
+      Rake::Task["unpublish:remove_document"].invoke("/a-base-path")
+    end
+
+    it "raises an error if a base_path is not present" do
+      expect { Rake::Task["unpublish:remove_document"].invoke }.to raise_error("Missing base_path parameter")
+    end
+  end
 end
