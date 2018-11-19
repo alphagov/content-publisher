5b55d9a308d20ba5d6bb053353e64c04584dce14
Leena Gupte <leena.gupte@digital.cabinet-office.gov.uk>
2018-11-19 14:09:21 +0000

    Allow removed documents to be redirected
    Update the service to allow documents to be redirected to another page
    on GOV.UK when they are removed.

--- app/services/document_unpublishing_service.rb
+++ app/services/document_unpublishing_service.rb
@@ -5,7 +5,8 @@ class DocumentUnpublishingService
     GdsApi.publishing_api_v2.unpublish(document.content_id, type: "withdrawal", explanation: explanatory_note)
   end
 
-  def remove(document)
+  def remove(document, redirect_path: nil)
+    return GdsApi.publishing_api_v2.unpublish(document.content_id, type: "redirect", alternative_path: redirect_path) if redirect_path.present?
     GdsApi.publishing_api_v2.unpublish(document.content_id, type: "gone")
   end
 end
--- spec/services/document_unpublishing_service_spec.rb
+++ spec/services/document_unpublishing_service_spec.rb
@@ -14,13 +14,22 @@ RSpec.describe DocumentUnpublishingService do
   end
 
   describe "#remove" do
-    it "removes a document" do
-      document = create(:document)
+    let(:document) { create(:document) }
 
+    it "removes a document" do
       stub_publishing_api_unpublish(document.content_id, body: { type: "gone" })
       DocumentUnpublishingService.new.remove(document)
 
       assert_publishing_api_unpublish(document.content_id, type: "gone")
+    end
+
+    it "allows removed documents to be redirected" do
+      redirect_path = "/redirect-path"
+
+      stub_publishing_api_unpublish(document.content_id, body: { type: "redirect", alternative_path: redirect_path })
+      DocumentUnpublishingService.new.remove(document, redirect_path: redirect_path)
+
+      assert_publishing_api_unpublish(document.content_id, type: "redirect", alternative_path: redirect_path)
     end
   end
 end
