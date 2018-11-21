492bb5b351cdf0898fa17f7ef11b8bb1c44765fb
Ben Thorner <ben.thorner@digital.cabinet-office.gov.uk>
2018-11-21 16:38:02 +0000

    Prevent manual preview if requirements not met

--- app/controllers/preview_controller.rb
+++ app/controllers/preview_controller.rb
@@ -4,6 +4,11 @@ class PreviewController < ApplicationController
   def create
     document = Document.find_by_param(params[:id])
 
+    if Requirements::DocumentChecker.new(document).pre_preview_issues.any?
+      redirect_to document_path(document), tried_to_preview: true
+      return
+    end
+
     DocumentDraftingService.update!(
       document: document,
       user: current_user,
