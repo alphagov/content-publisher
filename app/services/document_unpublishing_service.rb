e59b720fdd2ce9841e9420e0c8086139a6a7fcc3
Leena Gupte <leena.gupte@digital.cabinet-office.gov.uk>
2018-11-21 16:21:23 +0000

    Specify the locale of the document being retired
    At the moment Content Publisher does not handle translations. Other
    applications such as Whitehall only differentiate between translations
    by the locale, so multiple documents could have the same content_id and
    base_path.
    
    Add locale as an optional parameter to enable us to handle translations
    in the future.

--- app/services/document_unpublishing_service.rb
+++ app/services/document_unpublishing_service.rb
@@ -2,6 +2,11 @@
 
 class DocumentUnpublishingService
   def retire(document, explanatory_note)
-    GdsApi.publishing_api_v2.unpublish(document.content_id, type: "withdrawal", explanation: explanatory_note)
+    GdsApi.publishing_api_v2.unpublish(
+      document.content_id,
+      type: "withdrawal",
+      explanation: explanatory_note,
+      locale: document.locale,
+    )
   end
 end
--- spec/services/document_unpublishing_service_spec.rb
+++ spec/services/document_unpublishing_service_spec.rb
@@ -2,14 +2,24 @@
 
 RSpec.describe DocumentUnpublishingService do
   describe "#retire" do
+    let(:explanatory_note) { "The document is out of date" }
+
     it "withdraws a document in publishing-api with an explanatory note" do
       document = create(:document)
-      explanatory_note = "The document is out of date"
 
-      stub_publishing_api_unpublish(document.content_id, body: { type: "withdrawal", explanation: explanatory_note })
+      stub_publishing_api_unpublish(document.content_id, body: { type: "withdrawal", explanation: explanatory_note, locale: document.locale })
       DocumentUnpublishingService.new.retire(document, explanatory_note)
 
-      assert_publishing_api_unpublish(document.content_id, type: "withdrawal", explanation: explanatory_note)
+      assert_publishing_api_unpublish(document.content_id, type: "withdrawal", explanation: explanatory_note, locale: document.locale)
+    end
+
+    it "sets the locale of the document if specified" do
+      french_document = create(:document, locale: "fr")
+
+      stub_publishing_api_unpublish(french_document.content_id, body: { type: "withdrawal", explanation: explanatory_note, locale: french_document.locale })
+      DocumentUnpublishingService.new.retire(french_document, explanatory_note)
+
+      assert_publishing_api_unpublish(french_document.content_id, type: "withdrawal", explanation: explanatory_note, locale: french_document.locale)
     end
   end
 end
