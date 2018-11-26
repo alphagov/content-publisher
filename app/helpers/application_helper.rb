d47b78802161fa09179190cf050151edccf17870
Murilo Dal Ri <murilo.dalri@digital.cabinet-office.gov.uk>
2018-11-26 15:22:59 +0000

    Change back link on published page
    We want to show a different back link text in a specific page so this
    allows injecting that content in the component.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,8 @@ module ApplicationHelper
   def govspeak_to_html(govspeak)
     raw(Govspeak::Document.new(govspeak).to_html) # rubocop:disable Rails/OutputSafety
   end
+
+  def render_back_link(options)
+    render("govuk_publishing_components/components/back_link", options)
+  end
 end
