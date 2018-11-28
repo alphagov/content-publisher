32da1e39ed0bb47038452d857d805c70307793ee
Leena Gupte <leena.gupte@digital.cabinet-office.gov.uk>
2018-11-28 12:25:58 +0000

    Wrap call to redirect documents in a transaction
    This is so that if the call to publishing-api fails, the state of the document won’t be updated.

--- app/services/document_unpublishing_service.rb
+++ app/services/document_unpublishing_service.rb
@@ -33,17 +33,20 @@ class DocumentUnpublishingService
   end
 
   def remove_and_redirect(document, redirect_path, explanatory_note: nil)
-    GdsApi.publishing_api_v2.unpublish(
-      document.content_id,
-      type: "redirect",
-      explanation: explanatory_note,
-      alternative_path: redirect_path,
-      locale: document.locale,
-    )
+    Document.transaction do
+      document.update!(live_state: "removed")
+      TimelineEntry.create!(document: document, entry_type: "removed")
+
+      GdsApi.publishing_api_v2.unpublish(
+        document.content_id,
+        type: "redirect",
+        explanation: explanatory_note,
+        alternative_path: redirect_path,
+        locale: document.locale,
+      )
+    end
 
     delete_assets(document.images)
-    document.update!(live_state: "removed")
-    TimelineEntry.create!(document: document, entry_type: "removed")
   end
 
 private
