f8f96ad0c6e0353ad52b3cbc423aff82436410e2
Leena Gupte <leena.gupte@digital.cabinet-office.gov.uk>
2018-12-03 14:01:54 +0000

    Add entry type validation to Removal model
    Only allows Removal records to be created for “removed” Timeline entries

--- app/models/removal.rb
+++ app/models/removal.rb
@@ -2,4 +2,9 @@
 
 class Removal < ApplicationRecord
   belongs_to :timeline_entry, class_name: "TimelineEntry", foreign_key: :timeline_entries_id, inverse_of: :removal
+  validates_inclusion_of :entry_type, in: %w[removed]
+
+  def entry_type
+    self.timeline_entry.entry_type
+  end
 end
