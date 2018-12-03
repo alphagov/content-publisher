7cb1337d54871fdb869deb8838a9e99629190ff9
Leena Gupte <leena.gupte@digital.cabinet-office.gov.uk>
2018-12-03 13:43:17 +0000

    Add entry type validation to Retire model
    Only allows Retirement records to be created for “retired” Timeline entries

--- app/models/retirement.rb
+++ app/models/retirement.rb
@@ -2,4 +2,9 @@
 
 class Retirement < ApplicationRecord
   belongs_to :timeline_entry, class_name: "TimelineEntry", foreign_key: :timeline_entries_id, inverse_of: :retirement
+  validates_inclusion_of :entry_type, in: %w[retired]
+
+  def entry_type
+    self.timeline_entry.entry_type
+  end
 end
