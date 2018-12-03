1faed5114b678a8590e5da7970e1232c7b227d09
Leena Gupte <leena.gupte@digital.cabinet-office.gov.uk>
2018-12-03 13:49:52 +0000

    Create a Removal model
    This will be used to stored details of why a document was removed
    
    Created by running:
    `rails generate model Removal explanatory_note:string alternative_path:string redirect:boolean timeline_entries:references`
    
    Removal is a child of TimelineEntry. As the plural of TimelineEntry
    doesn’t follow the rails convention of just appending an `s` we also
    have to specify the foreign key id.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_03_123128) do
+ActiveRecord::Schema.define(version: 2018_12_03_134354) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -84,6 +84,16 @@ ActiveRecord::Schema.define(version: 2018_12_03_123128) do
     t.index ["document_id"], name: "index_images_on_document_id"
   end
 
+  create_table "removals", force: :cascade do |t|
+    t.string "explanatory_note"
+    t.string "alternative_path"
+    t.boolean "redirect", default: false
+    t.bigint "timeline_entries_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["timeline_entries_id"], name: "index_removals_on_timeline_entries_id"
+  end
+
   create_table "retirements", force: :cascade do |t|
     t.string "explanatory_note"
     t.bigint "timeline_entries_id", null: false
@@ -133,6 +143,7 @@ ActiveRecord::Schema.define(version: 2018_12_03_123128) do
   add_foreign_key "images", "active_storage_blobs", column: "blob_id", on_delete: :cascade
   add_foreign_key "images", "documents", on_delete: :cascade
   add_foreign_key "retirements", "timeline_entries", column: "timeline_entries_id", on_delete: :cascade
+  add_foreign_key "removals", "timeline_entries", column: "timeline_entries_id", on_delete: :cascade
   add_foreign_key "timeline_entries", "documents", on_delete: :cascade
   add_foreign_key "timeline_entries", "users", on_delete: :restrict
 end
