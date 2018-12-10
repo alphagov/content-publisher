63c57810ee3b5f7e31cafb73611c5e5ea256067f
Erin Raj-Staniland <erin.raj-staniland@digital.cabinet-office.gov.uk>
2018-12-10 14:47:04 +0000

    Use kebab case for new-document

--- spec/javascripts/modules/gtm-form-listener-spec.js
+++ spec/javascripts/modules/gtm-form-listener-spec.js
@@ -5,7 +5,7 @@ describe('GTM dataLayer messages for radio button submissions', function () {
 
   beforeEach(function () {
     var rawForm = `
-      <form id="myForm" data-gtm="new_document" onsubmit="return false;">
+      <form id="myForm" data-gtm="new-document" onsubmit="return false;">
         <input type="radio" name="supertype" id="radio-news" value="news">
         <input type="radio" name="supertype" id="radio-guidance" value="guidance">
       </form>
@@ -24,6 +24,6 @@ describe('GTM dataLayer messages for radio button submissions', function () {
     var submitEvent = new Event('submit', {'bubbles': true, 'cancelable': true})
     document.getElementById('myForm').dispatchEvent(submitEvent)
 
-    expect(dataLayer).toEqual([{new_document: { supertype: 'guidance' }}])
+    expect(dataLayer).toEqual([{'new-document': { supertype: 'guidance' }}])
   })
 })
