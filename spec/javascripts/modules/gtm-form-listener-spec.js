8cfc33a092bf9321f17a35c744e9120106ec3965
Erin Raj-Staniland <erin.raj-staniland@digital.cabinet-office.gov.uk>
2018-12-10 14:42:18 +0000

    Remove jQuery from spec

--- spec/javascripts/modules/gtm-form-listener-spec.js
+++ spec/javascripts/modules/gtm-form-listener-spec.js
@@ -1,33 +1,29 @@
-var $ = window.jQuery
-
 describe('GTM dataLayer messages for radio button submissions', function () {
   'use strict'
 
-  var form
   var dataLayer
-  var gtmFormListener
 
   beforeEach(function () {
-    form = $(`
+    var rawForm = `
       <form id="myForm" data-gtm="new_document" onsubmit="return false;">
         <input type="radio" name="supertype" id="radio-news" value="news">
         <input type="radio" name="supertype" id="radio-guidance" value="guidance">
       </form>
-    `)
-    $(document.body).append(form)
+      `
+    document.body.insertAdjacentHTML('beforeend', rawForm)
     dataLayer = []
     GTMFormListener.init(dataLayer)
   })
 
   afterEach(function () {
-    form.remove()
-    form = undefined
+    document.getElementById('myForm').remove()
   })
 
   it('should append the corrent message on submit', function () {
-    $('#radio-guidance').click()
+    document.getElementById('radio-guidance').click()
     var submitEvent = new Event('submit', {'bubbles': true, 'cancelable': true})
-    !document.getElementById('myForm').dispatchEvent(submitEvent)
+    document.getElementById('myForm').dispatchEvent(submitEvent)
+
     expect(dataLayer).toEqual([{new_document: { supertype: 'guidance' }}])
   })
 })
