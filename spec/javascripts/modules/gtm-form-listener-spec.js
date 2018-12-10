569a19254b15d163e49cc45c2b0ad2e0087c89ec
Erin Raj-Staniland <erin.raj-staniland@digital.cabinet-office.gov.uk>
2018-12-10 15:01:31 +0000

    Add spec for checkboxes

--- spec/javascripts/modules/gtm-form-listener-spec.js
+++ spec/javascripts/modules/gtm-form-listener-spec.js
@@ -2,12 +2,17 @@ describe('GTM dataLayer messages for radio button submissions', function () {
   'use strict'
 
   var dataLayer
+  var submitFormEvent = new Event('submit', {'bubbles': true, 'cancelable': true})
 
   beforeEach(function () {
     var rawForm = `
-      <form id="myForm" data-gtm="new-document" onsubmit="return false;">
+      <form id="testForm" data-gtm="new-document" onsubmit="return false;">
         <input type="radio" name="supertype" id="radio-news" value="news">
         <input type="radio" name="supertype" id="radio-guidance" value="guidance">
+
+        <input type="checkbox" name="caprinae" id="checkbox-goat" value="goat">
+        <input type="checkbox" name="caprinae" id="checkbox-sheep" value="sheep">
+        <input type="checkbox" name="caprinae" id="checkbox-ibex" value="ibex">
       </form>
       `
     document.body.insertAdjacentHTML('beforeend', rawForm)
@@ -16,14 +21,24 @@ describe('GTM dataLayer messages for radio button submissions', function () {
   })
 
   afterEach(function () {
-    document.getElementById('myForm').remove()
+    document.getElementById('testForm').remove()
   })
 
-  it('should append the corrent message on submit', function () {
+  it('should append the corrent message on submit for radios', function () {
     document.getElementById('radio-guidance').click()
-    var submitEvent = new Event('submit', {'bubbles': true, 'cancelable': true})
-    document.getElementById('myForm').dispatchEvent(submitEvent)
+    document.getElementById('testForm').dispatchEvent(submitFormEvent)
 
     expect(dataLayer).toEqual([{'new-document': { supertype: 'guidance' }}])
+  })
+
+  it('should append the corrent message on submit for checkboxes', function () {
+    document.getElementById('checkbox-sheep').click()
+    document.getElementById('checkbox-goat').click()
+    document.getElementById('testForm').dispatchEvent(submitFormEvent)
+
+    expect(dataLayer).toEqual([
+      {'new-document': { caprinae: 'goat' }},
+      {'new-document': { caprinae: 'sheep' }}
+    ])
   })
 })
