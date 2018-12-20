2d09b3bbe8fac5087cb9ea9fae6b55bdaa48589d
Emma Beynon <emma.beynon@digital.cabinet-office.gov.uk>
2018-12-20 15:32:00 +0000

    Display public explanation requirements
    This implements the Withdrawal Checker and displays an error if the public
    explanation does not meet requirements.

--- app/controllers/withdraw_controller.rb
+++ app/controllers/withdraw_controller.rb
@@ -10,13 +10,23 @@ class WithdrawController < ApplicationController
 
   def create
     Document.transaction do
-      document = Document.with_current_edition.lock!.find_by_param(params[:id])
+      @document = Document.with_current_edition.lock!.find_by_param(params[:id])
       public_explanation = params[:public_explanation]
+      issues = Requirements::WithdrawalChecker.new(public_explanation).pre_withdrawal_issues
 
-      #FIXME We should check that the edition is withdrawable before passing
-      # it to the UnpublishService
-      UnpublishService.new.withdraw(document.current_edition, public_explanation)
-      redirect_to document
+      if issues.any?
+        flash["alert_with_items"] = {
+          "title" => I18n.t!("withdraw.new.flashes.requirements"),
+          "items" => issues.items,
+        }
+
+        render :new
+      else
+        #FIXME We should check that the edition is withdrawable before passing
+        # it to the UnpublishService
+        UnpublishService.new.withdraw(@document.current_edition, public_explanation)
+        redirect_to @document
+      end
     end
   end
 end
