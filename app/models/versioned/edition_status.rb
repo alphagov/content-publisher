f9f5cd45789bc7387d62367794f1cb9af3c151c3
Kevin Dew <kevindew@me.com>
2018-12-28 13:19:28 +0000

    Change Versioning to Versioned namespace
    Rather than having a confusing mix of Versioning and Versioned it's
    probably better to just settle on Versioned.

--- app/models/versioned/edition_status.rb
+++ app/models/versioned/edition_status.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-module Versioning
+module Versioned
   class EditionStatus < ApplicationRecord
     self.table_name = "versioned_edition_statuses"
 
@@ -13,18 +13,18 @@ module Versioning
                foreign_key: :created_by_id
 
     belongs_to :revision_at_creation,
-               class_name: "Versioning::Revision",
+               class_name: "Versioned::Revision",
                foreign_key: :revision_at_creation_id
     # rubocop:enable Rails/InverseOf
 
     belongs_to :edition,
-               class_name: "Versioning::Edition",
+               class_name: "Versioned::Edition",
                foreign_key: :edition_id,
                optional: true,
                inverse_of: :statuses
 
     has_one :status_of,
-            class_name: "Versioning::Edition",
+            class_name: "Versioned::Edition",
             foreign_key: :status_id,
             inverse_of: :status,
             dependent: :restrict_with_exception
