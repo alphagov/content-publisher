7f6112b15c99f60ded7b7844c182c135ffcd0dfb
Kevin Dew <kevindew@me.com>
2018-12-31 11:54:49 +0000

    Versioned ports of requirements services
    These are ports of the requirements services that are needed for
    the documents controller.

---
+++ app/services/versioned/requirements/edition_checker.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+module Versioned
+  module Requirements
+    class EditionChecker
+      attr_reader :edition, :revision
+
+      def initialize(edition, revision = nil)
+        @edition = edition
+        @revision = revision || edition.revision
+      end
+
+      def pre_preview_issues
+        issues = []
+
+        revision.images.each do |image|
+          issues += ::Requirements::ImageChecker.new(image).pre_preview_issues.to_a
+        end
+
+        issues += ContentChecker.new(edition, revision).pre_preview_issues.to_a
+        ::Requirements::CheckerIssues.new(issues)
+      end
+
+      def pre_publish_issues(params = {})
+        issues = []
+        issues += ContentChecker.new(edition, revision).pre_publish_issues.to_a
+        issues += ::Requirements::TopicChecker.new(edition.document).pre_publish_issues(params).to_a
+        ::Requirements::CheckerIssues.new(issues)
+      end
+    end
+  end
+end
