3c10fbd3dc2e4b103ed7fd038934dfd7a9d7354d
Kevin Dew <kevindew@me.com>
2019-01-02 12:02:23 +0000

    Change Image model to ImageRevision model
    This renames the Image model to be ImageRevision to reflect that this
    refers to a revision of an image rather than a continuous item. It
    creates a Image model to act as a continuous item - without this only
    the association to a blob was a way of knowing that two image revisions
    were related.
    
    This change allows us to use the image_id as a user facing value so that
    the id of the revision can be concealed. E.g. if we have image_id of 1
    and image_revision_id of 2. This could have a URL of /images/1 for
    image_id or /images/2 for image_revision_id. If we then created a new
    revision /images/2 would no longer being a URL that works, whereas if we
    use /images/1 that can work no matter what revision.

--- app/models/versioned/image.rb
+++ app/models/versioned/image.rb
@@ -4,44 +4,21 @@ module Versioned
   class Image < ApplicationRecord
     self.table_name = "versioned_images"
 
-    WIDTH = 960
-    HEIGHT = 640
-    THUMBNAIL_WIDTH = 300
-    THUMBNAIL_HEIGHT = 200
+    # rubocop:disable Rails/InverseOf
+    belongs_to :created_by,
+               class_name: "User",
+               optional: true,
+               foreign_key: :created_by_id
+    # rubocop:enable Rails/InverseOf
 
-    belongs_to :blob, class_name: "ActiveStorage::Blob"
-
-    has_and_belongs_to_many :revisions,
-                            class_name: "Versioned::Revision",
-                            join_table: "versioned_revision_images"
+    has_many :image_revisions,
+             class_name: "Versioned::ImageRevision",
+             foreign_key: :image_id,
+             inverse_of: :image,
+             dependent: :restrict_with_exception
 
     def readonly?
       !new_record?
-    end
-
-    def thumbnail
-      crop_variant("#{THUMBNAIL_WIDTH}x#{THUMBNAIL_HEIGHT}")
-    end
-
-    def crop_variant(resize = "#{WIDTH}x#{HEIGHT}")
-      crop = "#{crop_width}x#{crop_height}+#{crop_x}+#{crop_y}"
-      blob.variant(
-        crop: crop,
-        resize: resize,
-      )
-    end
-
-    def content_type
-      blob.content_type
-    end
-
-    def cropped_bytes
-      processed_image = thumbnail.processed
-      processed_image.service.download(processed_image.key)
-    end
-
-    def at_exact_dimensions?
-      width == WIDTH && height == HEIGHT
     end
   end
 end
