246aeb08531ab824c8cd05592b440a2a76cb8d60
Kevin Dew <kevindew@me.com>
2019-01-02 11:58:44 +0000

    Fix brakeman errors for block rescuing
    Brakeman was raising an error due to these being rescues within a block.
    These could be switched to be method rescues but that feels a bit wrong
    since they rely on a variable created in the method (document) which
    doesn't feel like it'd be in scope.

--- app/controllers/versioned/topics_controller.rb
+++ app/controllers/versioned/topics_controller.rb
@@ -20,15 +20,17 @@ module Versioned
                                       .lock
                                       .find_by_param(params[:document_id])
 
-        document.document_topics.patch(params.fetch(:topics, []), params[:version].to_i)
+        begin
+          document.document_topics.patch(params.fetch(:topics, []), params[:version].to_i)
 
-        redirect_to versioned_document_path(document), notice: t("documents.show.flashes.topics_updated")
-      rescue GdsApi::HTTPConflict
-        Rails.logger.warn("Conflict updating topics for #{document.content_id} at version #{params[:version].to_i}")
-        redirect_to versioned_topics_path(document), alert_with_description: t("topics.edit.flashes.topic_update_conflict")
-      rescue GdsApi::BaseError => e
-        GovukError.notify(e)
-        redirect_to document, alert_with_description: t("documents.show.flashes.topic_update_error")
+          redirect_to versioned_document_path(document), notice: t("documents.show.flashes.topics_updated")
+        rescue GdsApi::HTTPConflict
+          Rails.logger.warn("Conflict updating topics for #{document.content_id} at version #{params[:version].to_i}")
+          redirect_to versioned_topics_path(document), alert_with_description: t("topics.edit.flashes.topic_update_conflict")
+        rescue GdsApi::BaseError => e
+          GovukError.notify(e)
+          redirect_to document, alert_with_description: t("documents.show.flashes.topic_update_error")
+        end
       end
     end
   end
