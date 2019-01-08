ab2cc01ff59c94f30f619e735f721fdf768f772e
Kevin Dew <kevindew@me.com>
2019-01-08 09:44:47 +0000

    Port versioned remove/retire

--- config/routes.rb
+++ config/routes.rb
@@ -82,8 +82,8 @@ Rails.application.routes.draw do
     get "/documents/:id/preview" => "preview#show", as: :preview_document
     post "/documents/:id/create-preview" => "preview#create", as: :create_preview
 
-    # get "/documents/:id/retire" => "unpublish#retire", as: :retire
-    # get "/documents/:id/remove" => "unpublish#remove", as: :remove
+    get "/documents/:id/retire" => "unpublish#retire", as: :retire
+    get "/documents/:id/remove" => "unpublish#remove", as: :remove
 
     get "/documents/:document_id/images" => "images#index", as: :images
     post "/documents/:document_id/images" => "images#create", as: :create_image
