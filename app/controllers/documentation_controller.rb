91e00148a705e4c650566dded60a5b5a913e620c
Ben Thorner <ben.thorner@digital.cabinet-office.gov.uk>
2019-01-22 10:42:51 +0000

    Remove redundant include in controller

--- app/controllers/documentation_controller.rb
+++ app/controllers/documentation_controller.rb
@@ -1,6 +1,5 @@
 # frozen_string_literal: true
 
 class DocumentationController < ApplicationController
-  include GDS::SSO::ControllerMethods
   before_action { authorise_user!(User::DEBUG_PERMISSION) }
 end
