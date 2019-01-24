98b7fef3ff5de5f17a6604d1cbeeb4aaff26ce1e
Emma Beynon <emma.beynon@digital.cabinet-office.gov.uk>
2019-01-24 11:07:04 +0000

    Apply Pre Release feature flag to withdrawal workflow
    Users without this flag will see a page directing them to raise a support
    request.

--- app/controllers/withdraw_controller.rb
+++ app/controllers/withdraw_controller.rb
@@ -7,6 +7,11 @@ class WithdrawController < ApplicationController
     @public_explanation =
       edition.withdrawn? ? edition.status.details.public_explanation : nil
 
+    if !current_user.has_permission?(User::PRE_RELEASE_FEATURES_PERMISSION)
+      render :withdraw
+      return
+    end
+
     if current_user.has_permission?(User::MANAGING_EDITOR_PERMISSION)
       render :new
     else
@@ -15,6 +20,12 @@ class WithdrawController < ApplicationController
   end
 
   def create
+    unless user_has_permissions?
+      # FIXME: this shouldn't be an exception but we've not worked out the
+      # right response - maybe bad request or a redirect with flash?
+      raise "Can't withdraw an edition without permissions"
+    end
+
     Document.transaction do
       @document = Document.with_current_edition.lock!.find_by_param(params[:id])
       public_explanation = params[:public_explanation]
@@ -41,5 +52,12 @@ class WithdrawController < ApplicationController
         end
       end
     end
+  end
+
+private
+
+  def user_has_permissions?
+    current_user.has_all_permissions?([User::MANAGING_EDITOR_PERMISSION,
+                                       User::PRE_RELEASE_FEATURES_PERMISSION])
   end
 end
