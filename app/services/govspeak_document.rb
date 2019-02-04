0b9492656411ca55683789a1b8d7774941b01fd9
Kevin Garwood <kevin.garwood@digital.cabinet-office.gov.uk>
2019-02-04 15:19:57 +0000

    Support Images for Govspeak Documents
    The focus of this commit is on supporting the different needs that Preview and
    payload contexts have for accepting HTML code containing images.  to_html has
    been turned into separate methods in_app_html and payload_html, which differ only
    in the kind of value that is passed as a URL attribute for when Govspeak generates
    an HTML image snippet.
    
    Payload uses a URL for an image that comes from asset manager, whereas the preview
    feature uses the path for a cropped variant of the image. Initially,
    image_revision.crop_variant.service_url provided the URL attribute for previews.
    However service_url requires that a host is specified, and this is not provided
    within the service tests.  Using the rails_representation_path helper method avoids
    this problem.
    
    Code used to support in_app_html and payload_html showed a lot of similarities, so
    some of the changes have helped minimise repetitive code.  Private methods in_app_images
    and payload_images are meant to help make it obvious that the orchestration methods
    in_app_html and payload_html do different things.
    
    Splitting to_html into in_app_html and payload_html in govspeak_document warrants
    changes to be made to the tests in govspeak_document_spec. Separate describe blocks appear
    for each method.  They currently exhibit a lot of repetition that may be remedied in a
    subsequent commit.

--- app/services/govspeak_document.rb
+++ app/services/govspeak_document.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class GovspeakDocument
+  include Rails.application.routes.url_helpers
+
   attr_reader :text
 
   def initialize(text, edition)
@@ -9,14 +11,40 @@ class GovspeakDocument
   end
 
   def in_app_html
-    Govspeak::Document.new(text, contacts: contacts).to_html
+    Govspeak::Document.new(text, contacts: contacts, images: in_app_images).to_html
   end
 
   def payload_html
-    Govspeak::Document.new(text, contacts: contacts).to_html
+    Govspeak::Document.new(text, contacts: contacts, images: payload_images).to_html
   end
 
 private
+
+  def in_app_images
+    @edition.revision.image_revisions.map do |image_revision|
+      image_attributes(image_revision).merge(
+        # This is the same as url_for(image_revision.crop_variant) in the view, or
+        # image_revision.crop_variant.service_url, except that these don't work outside
+        # of a request context which normally includes the request host.
+        url: rails_representation_path(image_revision.crop_variant, only_path: true)
+      )
+    end
+  end
+
+  def payload_images
+    @edition.revision.image_revisions.map do |image_revision|
+      image_attributes(image_revision).merge(url: image_revision.asset_url("960"))
+    end
+  end
+
+  def image_attributes(image_revision)
+    {
+      alt_text: image_revision.alt_text,
+      caption: image_revision.caption,
+      credit: image_revision.credit,
+      id: image_revision.filename
+    }
+  end
 
   def contacts
     @contacts ||= begin
@@ -27,7 +55,6 @@ private
                     contacts.compact
                   end
   end
-
 
 
 end
--- spec/services/govspeak_document_spec.rb
+++ spec/services/govspeak_document_spec.rb
@@ -1,13 +1,13 @@
 # frozen_string_literal: true
 
 RSpec.describe GovspeakDocument do
-  describe "#in_app_html" do
-    before(:each) do
-      @edition = create :edition
-    end
+  include ActiveSupport::Testing::TimeHelpers
 
+  describe "#in_app_html" do
     it "converts the provided text to HTML" do
-      expect(GovspeakDocument.new("## Hullo", @edition).in_app_html)
+      edition = create(:edition)
+
+      expect(GovspeakDocument.new("## Hullo", edition).in_app_html)
         .to match(%{<h2 id="hullo">Hullo</h2>})
     end
 
@@ -15,8 +15,10 @@ RSpec.describe GovspeakDocument do
       it "renders an empty string" do
         content_id = SecureRandom.uuid
         govspeak = "[Contact:#{content_id}]"
+        edition = create(:edition)
         stub_publishing_api_get_editions([], ContactsService::EDITION_PARAMS)
-        expect(GovspeakDocument.new(govspeak, @edition).in_app_html).to eql("\n")
+
+        expect(GovspeakDocument.new(govspeak, edition).in_app_html).to eql("\n")
       end
     end
 
@@ -39,21 +41,41 @@ RSpec.describe GovspeakDocument do
           ],
           ContactsService::EDITION_PARAMS,
         )
-
         govspeak = "[Contact:#{content_id}]"
-        expect(GovspeakDocument.new(govspeak, @edition).in_app_html)
+        edition = create(:edition)
+
+        expect(GovspeakDocument.new(govspeak, edition).in_app_html)
           .to match(%{<a href="mailto:clark@dailyplanet.com" class="email">Mail Clark</a>})
+      end
+    end
+
+    context "when an image is present in the markdown" do
+      it "renders the uncropped image" do
+        image_revision = build(:image_revision,
+                               :on_asset_manager,
+                               alt_text: 'Some alt text',
+                               caption: 'An optional caption',
+                               credit: 'An optional credit',
+                               filename: 'filename.png')
+        document_type = build(:document_type, lead_image: true)
+        edition = build(:edition,
+                        image_revisions: [image_revision],
+                        document_type_id: document_type.id)
+
+        html_output = GovspeakDocument.new("[Image: filename.png]", edition).in_app_html
+        expect(html_output).to match("Some alt text")
+        expect(html_output).to match("An optional caption")
+        expect(html_output).to match("An optional credit")
+        expect(html_output).to match("filename.png")
       end
     end
   end
 
   describe "#payload_html" do
-    before(:each) do
-      @edition = create :edition
-    end
-
     it "converts the provided text to HTML" do
-      expect(GovspeakDocument.new("## Hullo", @edition).payload_html)
+      edition = create(:edition)
+
+      expect(GovspeakDocument.new("## Hullo", edition).payload_html)
         .to match(%{<h2 id="hullo">Hullo</h2>})
     end
 
@@ -61,8 +83,10 @@ RSpec.describe GovspeakDocument do
       it "renders an empty string" do
         content_id = SecureRandom.uuid
         govspeak = "[Contact:#{content_id}]"
+        edition = create(:edition)
         stub_publishing_api_get_editions([], ContactsService::EDITION_PARAMS)
-        expect(GovspeakDocument.new(govspeak, @edition).payload_html).to eql("\n")
+
+        expect(GovspeakDocument.new(govspeak, edition).payload_html).to eql("\n")
       end
     end
 
@@ -85,12 +109,34 @@ RSpec.describe GovspeakDocument do
           ],
           ContactsService::EDITION_PARAMS,
         )
-
         govspeak = "[Contact:#{content_id}]"
-        expect(GovspeakDocument.new(govspeak, @edition).payload_html)
+        edition = create(:edition)
+
+        expect(GovspeakDocument.new(govspeak, edition).in_app_html)
           .to match(%{<a href="mailto:clark@dailyplanet.com" class="email">Mail Clark</a>})
       end
     end
-  end
 
+    context "when an image is present in the markdown" do
+      it "renders the uncropped image" do
+        image_revision = build(:image_revision,
+                               :on_asset_manager,
+                               alt_text: 'Some alt text',
+                               caption: 'An optional caption',
+                               credit: 'An optional credit',
+                               filename: 'filename.png')
+
+        document_type = build(:document_type, lead_image: true)
+        edition = build(:edition,
+                        image_revisions: [image_revision],
+                        document_type_id: document_type.id)
+
+        html_output = GovspeakDocument.new("[Image: filename.png]", edition).payload_html
+        expect(html_output).to match("Some alt text")
+        expect(html_output).to match("An optional caption")
+        expect(html_output).to match("An optional credit")
+        expect(html_output).to match("filename.png")
+      end
+    end
+  end
 end
