b508e6206a4d3322a2cb64bd9c1e158ca7febe5e
Ben Thorner <ben.thorner@digital.cabinet-office.gov.uk>
2019-02-06 10:53:30 +0000

    Merge inline image spec into upload spec
    As image upload is no independent of choosing a lead image, it makes
    sense to simply check for the inline image snippet here instead.

--- spec/features/editing_images/upload_image_spec.rb
+++ spec/features/editing_images/upload_image_spec.rb
@@ -47,6 +47,8 @@ RSpec.feature "Upload an image" do
     within("#image-#{Image.first.id}") do
       expect(page).to have_content("A caption")
       expect(page).to have_content("A credit")
+      expect(page).to have_content(I18n.t("images.index.meta.inline_code.value", filename: "1000x1000.jpg"))
+
       expect(find("img")["src"]).to include("1000x1000.jpg")
       expect(find("img")["alt"]).to eq("Some alt text")
     end
