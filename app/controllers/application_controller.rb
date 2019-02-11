8dbe6c69bbe6ee23fe3ed641b5c1927758010490
Ben Thorner <ben.thorner@digital.cabinet-office.gov.uk>
2019-02-11 15:31:52 +0000

    Add user UID to context for Sentry errors
    Previously we've seen errors arising with multiple users interacting
    with the same document. Having the user UID associated with the error
    will help us to better debug these issues.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
   include GDS::SSO::ControllerMethods
 
   before_action :authenticate_user!
+  before_action { Raven.user_context(id: current_user&.uid) }
 
   add_flash_types :alert_with_description, :alert_with_items, :confirmation, :tried_to_publish, :tried_to_preview
 end
