de233723e763dc19d19570a95202a2d4e16c76ac
Emma Beynon <emma.beynon@digital.cabinet-office.gov.uk>
2019-02-14 15:35:58 +0000

    Add Time Options Helper
    This returns a list of time options that can be selected by users when choosing
    a scheduled publishing time.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  include TimeOptionsHelper
+
   def govspeak_to_html(govspeak)
     raw(Govspeak::Document.new(govspeak).to_html) # rubocop:disable Rails/OutputSafety
   end
---
+++ app/helpers/time_options_helper.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+module TimeOptionsHelper
+  def time_options(selected = nil)
+    options = times.map { |time| { text: time, value: time } }
+    options.each do |option|
+      if option[:text] == selected
+        option[:selected] = true
+        break
+      end
+    end
+    options
+  end
+
+private
+
+  def times
+    hours = [12] + (1..11).to_a
+    incremented_hours = hours.map do |hour|
+      [hour.to_s + ":00", hour.to_s + ":30"]
+    end
+
+    meridiem_hours = incremented_hours.flatten!.map { |hour| hour + "am" }
+    incremented_hours.each { |hour| meridiem_hours << hour + "pm" }
+    meridiem_hours[0] = "12:01am"
+    meridiem_hours
+  end
+end
