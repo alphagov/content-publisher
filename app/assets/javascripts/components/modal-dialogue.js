d55edec4abfd0a1a2b19ef0bd03cc8f6fed5b406
Ben Thorner <ben.thorner@digital.cabinet-office.gov.uk>
2019-02-21 15:40:30 +0000

    Rewrite modal-dialogue component to use modules

--- app/assets/javascripts/components/modal-dialogue.js
+++ app/assets/javascripts/components/modal-dialogue.js
@@ -1,91 +1,92 @@
-function ModalDialogue ($module) {
-  this.$module = $module
-  this.$dialogBox = $module.querySelector('.app-c-modal-dialogue__box')
-  this.$closeButton = $module.querySelector('.app-c-modal-dialogue__close-button')
-  this.$body = document.querySelector('body')
-}
+window.GOVUK = window.GOVUK || {}
+window.GOVUK.Modules = window.GOVUK.Modules || {};
 
-ModalDialogue.prototype.init = function () {
-  if (!this.$module) {
-    return
+(function (Modules) {
+  function ModalDialogue () { }
+
+  ModalDialogue.prototype.start = function ($module) {
+    this.$module = $module[0]
+    this.$dialogBox = this.$module.querySelector('.app-c-modal-dialogue__box')
+    this.$closeButton = this.$module.querySelector('.app-c-modal-dialogue__close-button')
+    this.$body = document.querySelector('body')
+
+    this.$module.open = this.handleOpen.bind(this)
+    this.$module.close = this.handleClose.bind(this)
+    this.$module.focusDialog = this.handleFocusDialog.bind(this)
+    this.$module.boundKeyDown = this.handleKeyDown.bind(this)
+
+    var $triggerElement = document.querySelector(
+      '[data-toggle="modal"][data-target="' + this.$module.id + '"]'
+    )
+
+    if ($triggerElement) {
+      $triggerElement.addEventListener('click', this.$module.open)
+    }
+
+    if (this.$closeButton) {
+      this.$closeButton.addEventListener('click', this.$module.close)
+    }
   }
 
-  this.$module.open = this.handleOpen.bind(this)
-  this.$module.close = this.handleClose.bind(this)
-  this.$module.focusDialog = this.handleFocusDialog.bind(this)
-  this.$module.boundKeyDown = this.handleKeyDown.bind(this)
+  ModalDialogue.prototype.handleOpen = function (event) {
+    if (event) {
+      event.preventDefault()
+    }
 
-  var $triggerElement = document.querySelector('[data-toggle="modal"][data-target="' + this.$module.id + '"]')
-  if ($triggerElement) {
-    $triggerElement.addEventListener('click', this.$module.open)
+    this.$body.classList.add('app-o-template__body--modal')
+    this.$body.classList.add('app-o-template__body--blur')
+    this.$focusedElementBeforeOpen = document.activeElement
+    this.$module.style.display = 'block'
+    this.$dialogBox.focus()
+
+    document.addEventListener('keydown', this.$module.boundKeyDown, true)
   }
 
-  if (this.$closeButton) {
-    this.$closeButton.addEventListener('click', this.$module.close)
+  ModalDialogue.prototype.handleClose = function (event) {
+    if (event) {
+      event.preventDefault()
+    }
+
+    this.$body.classList.remove('app-o-template__body--modal')
+    this.$body.classList.remove('app-o-template__body--blur')
+    this.$module.style.display = 'none'
+    this.$focusedElementBeforeOpen.focus()
+
+    document.removeEventListener('keydown', this.$module.boundKeyDown, true)
   }
-}
 
-ModalDialogue.prototype.handleOpen = function (event) {
-  if (event) {
-    event.preventDefault()
+  ModalDialogue.prototype.handleFocusDialog = function () {
+    this.$dialogBox.focus()
   }
-  this.$body.classList.add('app-o-template__body--modal')
-  this.$body.classList.add('app-o-template__body--blur')
-  this.$focusedElementBeforeOpen = document.activeElement
-  this.$module.style.display = 'block'
-  this.$dialogBox.focus()
 
-  document.addEventListener('keydown', this.$module.boundKeyDown, true)
-}
+  // while open, prevent tabbing to outside the dialogue
+  // and listen for ESC key to close the dialogue
+  ModalDialogue.prototype.handleKeyDown = function (event) {
+    var KEY_TAB = 9
+    var KEY_ESC = 27
 
-ModalDialogue.prototype.handleClose = function (event) {
-  if (event) {
-    event.preventDefault()
-  }
-  this.$body.classList.remove('app-o-template__body--modal')
-  this.$body.classList.remove('app-o-template__body--blur')
-  this.$module.style.display = 'none'
-  this.$focusedElementBeforeOpen.focus()
-
-  document.removeEventListener('keydown', this.$module.boundKeyDown, true)
-}
-
-ModalDialogue.prototype.handleFocusDialog = function () {
-  this.$dialogBox.focus()
-}
-
-// while open, prevent tabbing to outside the dialogue
-// and listen for ESC key to close the dialogue
-ModalDialogue.prototype.handleKeyDown = function (event) {
-  var KEY_TAB = 9
-  var KEY_ESC = 27
-
-  switch (event.keyCode) {
-    case KEY_TAB:
-      if (event.shiftKey) {
-        if (document.activeElement === this.$dialogBox) {
-          event.preventDefault()
-          this.$closeButton.focus()
+    switch (event.keyCode) {
+      case KEY_TAB:
+        if (event.shiftKey) {
+          if (document.activeElement === this.$dialogBox) {
+            event.preventDefault()
+            this.$closeButton.focus()
+          }
+        } else {
+          if (document.activeElement === this.$closeButton) {
+            event.preventDefault()
+            this.$dialogBox.focus()
+          }
         }
-      } else {
-        if (document.activeElement === this.$closeButton) {
-          event.preventDefault()
-          this.$dialogBox.focus()
-        }
-      }
 
-      break
-    case KEY_ESC:
-      this.$module.close()
-      break
-    default:
-      break
+        break
+      case KEY_ESC:
+        this.$module.close()
+        break
+      default:
+        break
+    }
   }
-}
 
-// Initialise modals
-var $modalDialogues = document.querySelectorAll('[data-module="modal-dialogue"]')
-$modalDialogues.forEach(function ($el) {
-  var $modalDialogue = new ModalDialogue($el)
-  $modalDialogue.init()
-})
+  Modules.ModalDialogue = ModalDialogue
+})(window.GOVUK.Modules)
