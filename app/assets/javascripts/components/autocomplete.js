34e4d1448c7bd78cc5895ba1a3ed2c49123152bb
Ben Thorner <ben.thorner@digital.cabinet-office.gov.uk>
2019-02-21 15:01:08 +0000

    Rewrite autocomplete component to use modules
    Previously the autocomplete component used to switch it's init behaviour
    based on data-module, which could also be autocomplete-with-hint-on...
    This rewrites the component to use an alternative data-autocomplete-type
    attribute, so that it is compatible with the GOVUK.Modules system.

--- app/assets/javascripts/components/autocomplete.js
+++ app/assets/javascripts/components/autocomplete.js
@@ -1,10 +1,29 @@
 //= require vendor/accessible-autocomplete/dist/accessible-autocomplete.min.js
+window.GOVUK = window.GOVUK || {}
+window.GOVUK.Modules = window.GOVUK.Modules || {};
 
-var $autocompletes = document.querySelectorAll('[data-module="autocomplete"]')
-if ($autocompletes) {
-  $autocompletes.forEach(function ($el) {
+(function (Modules) {
+  function Autocomplete () { }
+
+  Autocomplete.prototype.start = function ($module) {
+    this.$module = $module[0]
+    var type = this.$module.dataset.autocompleteType
+
+    if (type === 'with-hint-on-options') {
+      this.initAutoCompleteWithHintOnOptions()
+    } else {
+      this.initAutoComplete()
+    }
+  }
+
+  Autocomplete.prototype.initAutoComplete = function () {
     var customAttributes = {}
-    var $select = $el.querySelector('select')
+    var $select = this.$module.querySelector('select')
+
+    if (!$select) {
+      return
+    }
+
     if ($select.attributes['data-contextual-guidance']) {
       customAttributes = {
         'data-contextual-guidance': $select.attributes['data-contextual-guidance'].value
@@ -18,54 +37,60 @@ if ($autocompletes) {
       showNoOptionsFound: true,
       customAttributes: customAttributes
     })
-  })
-}
+  }
 
-var $autocompleteWithHintOnOptions = document.querySelector('[data-module="autocomplete-with-hint-on-options"]')
-if ($autocompleteWithHintOnOptions) {
-  // Read options and associated data attributes and feed that as results for inputValueTemplate
-  var $select = $autocompleteWithHintOnOptions.querySelector('select')
-  var $options = $select.querySelectorAll('option')
+  Autocomplete.prototype.initAutoCompleteWithHintOnOptions = function () {
+    // Read options and associated data attributes and feed that as results for inputValueTemplate
+    var $select = this.$module.querySelector('select')
 
-  new window.accessibleAutocomplete({ // eslint-disable-line no-new, new-cap
-    element: $autocompleteWithHintOnOptions,
-    id: $select.id,
-    source: function (query, syncResults) {
-      var results = []
-      $options.forEach(function ($el) {
-        results.push({text: $el.textContent, hint: $el.dataset.hint || '', value: $el.value})
-      })
-      syncResults(query
-        ? results.filter(function (result) {
-          var valueContains = result.text.toLowerCase().indexOf(query.toLowerCase()) !== -1
-          var hintContains = result.hint.toLowerCase().indexOf(query.toLowerCase()) !== -1
-          return valueContains || hintContains
-        }) : []
-      )
-    },
-    minLength: 3,
-    autoselect: true,
-    showNoOptionsFound: true,
-    templates: {
-      inputValue: function (result) {
-        return result && result.text
-      },
-      suggestion: function (result) {
-        return result && result.text + '<span class="autocomplete__option-hint">' + result.hint + '</span>'
-      }
-    },
-    onConfirm: function (result) {
-      var value = result && result.value
-      var options = [].filter.call($select.options, function (option) {
-        return option.value === value
-      })
-
-      if (options.length) {
-        options[0].selected = true
-      }
+    if (!$select) {
+      return
     }
-  })
 
-  $select.style.display = 'none'
-  $select.id = $select.id + '-select'
-}
+    var $options = $select.querySelectorAll('option')
+
+    new window.accessibleAutocomplete({ // eslint-disable-line no-new, new-cap
+      element: this.$module,
+      id: $select.id,
+      source: function (query, syncResults) {
+        var results = []
+        $options.forEach(function ($el) {
+          results.push({text: $el.textContent, hint: $el.dataset.hint || '', value: $el.value})
+        })
+        syncResults(query
+          ? results.filter(function (result) {
+            var valueContains = result.text.toLowerCase().indexOf(query.toLowerCase()) !== -1
+            var hintContains = result.hint.toLowerCase().indexOf(query.toLowerCase()) !== -1
+            return valueContains || hintContains
+          }) : []
+        )
+      },
+      minLength: 3,
+      autoselect: true,
+      showNoOptionsFound: true,
+      templates: {
+        inputValue: function (result) {
+          return result && result.text
+        },
+        suggestion: function (result) {
+          return result && result.text + '<span class="autocomplete__option-hint">' + result.hint + '</span>'
+        }
+      },
+      onConfirm: function (result) {
+        var value = result && result.value
+        var options = [].filter.call($select.options, function (option) {
+          return option.value === value
+        })
+
+        if (options.length) {
+          options[0].selected = true
+        }
+      }
+    })
+
+    $select.style.display = 'none'
+    $select.id = $select.id + '-select'
+  }
+
+  Modules.Autocomplete = Autocomplete
+})(window.GOVUK.Modules)
