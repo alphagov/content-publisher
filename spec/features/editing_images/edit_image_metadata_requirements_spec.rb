d7ca9953a0e2a994df7bfba2f570949428120b0d
Ben Thorner <ben.thorner@digital.cabinet-office.gov.uk>
2019-02-26 14:45:46 +0000

    Support editing image metadata within a modal
    This changes the post-crop behaviour to now show the metadata edit page
    for the cropped image, which now matches the upload/edit image workflow.
    
    This also rewrites the feature test for uploading an inline image to
    more closely ressemble the non-modal equivalent. As it's not possible to
    manually navigate to a modal section, the metadata requirements feature
    spec has to manually navigate to the page via the 'Edit image' workflow.

--- spec/features/editing_images/edit_image_metadata_requirements_spec.rb
+++ spec/features/editing_images/edit_image_metadata_requirements_spec.rb
@@ -1,27 +1,50 @@
 # frozen_string_literal: true
 
-RSpec.feature "Edit image metadata with requirements issues" do
-  scenario do
+RSpec.feature "Edit image metadata with requirements issues", js: true do
+  scenario "lead image" do
     given_there_is_an_edition_with_images
-    when_i_visit_the_image_metadata_page
+    when_i_visit_the_images_page
+    and_i_edit_the_image_with_bad_metadata
+    then_i_see_an_error_to_fix_the_issues
+  end
+
+  scenario "inline image" do
+    given_there_is_an_edition_with_images
+    when_i_insert_an_inline_image
     and_i_edit_the_image_with_bad_metadata
     then_i_see_an_error_to_fix_the_issues
   end
 
   def given_there_is_an_edition_with_images
-    document_type = build(:document_type, images: true)
+    body_field = build(:field, id: "body", type: "govspeak")
+    document_type = build(:document_type, contents: [body_field], images: true)
     @image_revision = create(:image_revision, :on_asset_manager)
+
     @edition = create(:edition,
                       document_type_id: document_type.id,
                       image_revisions: [@image_revision])
   end
 
-  def when_i_visit_the_image_metadata_page
-    visit edit_image_path(@edition.document, @image_revision.image_id)
+  def when_i_visit_the_images_page
+    visit images_path(@edition.document)
+  end
+
+  def when_i_insert_an_inline_image
+    visit edit_document_path(@edition.document)
+
+    within(".app-c-markdown-editor") do
+      find("markdown-toolbar details").click
+      click_on "Image"
+    end
   end
 
   def and_i_edit_the_image_with_bad_metadata
-    @request = stub_publishing_api_put_content(@edition.content_id, {})
+    stub_any_publishing_api_put_content
+    stub_asset_manager_receives_an_asset
+    stub_asset_manager_deletes_any_asset
+
+    click_on "Edit image"
+    click_on "Crop image"
     fill_in "image_revision[alt_text]", with: ""
     click_on "Save"
   end
