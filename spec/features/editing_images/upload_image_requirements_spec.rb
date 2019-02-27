33d0af1b64dcbfa8349cfdeb274974c778418cda
Ben Thorner <ben.thorner@digital.cabinet-office.gov.uk>
2019-02-27 16:13:38 +0000

    Merge modal specs into existing image specs
    This merges the new specs for inline images into the existing specs for
    the 'lead image' workflow. A few compromises were necessary to
    accommodate the JS behaviour and additional body field on the document
    type; the only whacky thing is that the crop might be slightly off.

--- spec/features/editing_images/upload_image_requirements_spec.rb
+++ spec/features/editing_images/upload_image_requirements_spec.rb
@@ -1,15 +1,23 @@
 # frozen_string_literal: true
 
-RSpec.feature "Upload an image with requirements issues" do
-  scenario do
+RSpec.feature "Upload an image with requirements issues", js: true do
+  scenario "lead image" do
     given_there_is_an_edition
     when_i_visit_the_images_page
     and_i_upload_an_invalid_image
     then_i_should_see_an_error
   end
 
+  scenario "inline image" do
+    given_there_is_an_edition
+    when_i_insert_an_inline_image
+    and_i_upload_an_invalid_image
+    then_i_should_see_an_error
+  end
+
   def given_there_is_an_edition
-    document_type = build(:document_type, images: true)
+    body_field = build(:field, id: "body", type: "govspeak")
+    document_type = build(:document_type, contents: [body_field], images: true)
     @edition = create(:edition, document_type_id: document_type.id)
   end
 
@@ -18,8 +26,17 @@ RSpec.feature "Upload an image with requirements issues" do
     click_on "Change Lead image"
   end
 
+  def when_i_insert_an_inline_image
+    visit edit_document_path(@edition.document)
+
+    within(".app-c-markdown-editor") do
+      find("markdown-toolbar details").click
+      click_on "Image"
+    end
+  end
+
   def and_i_upload_an_invalid_image
-    find('form input[type="file"]').set(file_fixture("text-file.txt"))
+    find('form input[type="file"]').set(Rails.root.join(file_fixture("text-file.txt")))
     click_on "Upload"
   end
 
