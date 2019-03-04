5ab7784a8b5a816a921523bc8dd58472906cb0af
Kevin Dew <kevindew@me.com>
2019-03-04 16:28:45 +0000

    Set the seed db user to have GDS organisation
    Prior to this the seed users documents were assigned to an organisation
    that wasn't known to Content Publisher. This sets the
    organisation_content_id to use the GDS organisation content id, which
    allows the default filter to match GDS when working with the Publishing
    API.

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,5 +2,7 @@
 
 user = User.find_or_create_by!(name: "publisher")
 
+gds_organisation_content_id = "af07d5a5-df63-4ddc-9383-6a666845ebe9"
+
 user.update!(permissions: [User::PRE_RELEASE_FEATURES_PERMISSION],
-            organisation_content_id: SecureRandom.uuid)
+            organisation_content_id: gds_organisation_content_id)
