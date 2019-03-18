329599bfedc693b6a87b99967f2d0f60684c78ba
Emma Beynon <emma.beynon@digital.cabinet-office.gov.uk>
2019-03-18 11:13:48 +0000

    Set Action Mailer delivery methods
    This sets the production Action Mailer delivery method to Notify.
    
    We don't want to make Notify API calls in test and development environments so
    we override the delivery methods for these environments.  As both the :test and
    :file delivery methods make use of SMTP we need to set a default `from` address
    as it's a required param.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -50,4 +50,8 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # Log Action Mailer emails instead of sending them to Notify
+  config.action_mailer.delivery_method = :file
+  config.action_mailer.default_options = { from: "test@example.com" }
 end
