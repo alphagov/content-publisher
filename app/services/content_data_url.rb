614ab07cfd63c3f9c423152b8511cfa993b5d1dc
Emma Beynon <emma.beynon@digital.cabinet-office.gov.uk>
2019-04-04 14:02:44 +0000

    Display content data link to shared beta partners
    We only want to display a link to Content Data pages if users are current
    Content Data beta partners, otherwise they will not be able to view these
    pages.  In order to determine whether the link should be displayed we check the
    user's `organisation_content_id` against a list of Content Data beta partner
    content IDs.  We include the slugs for each organisation in a comment so that
    it's easier to understand which organisations are included in the list.

--- app/services/content_data_url.rb
+++ app/services/content_data_url.rb
@@ -1,6 +1,11 @@
 # frozen_string_literal: true
 
 class ContentDataUrl
+  # department-for-work-pensions, department-of-health-and-social-care, government-digital-service
+  CONTENT_DATA_BETA_PARTNERS = %w(b548a09f-8b35-4104-89f4-f1a40bf3136d
+                                  7cd6bf12-bbe9-4118-8523-f927b0442156
+                                  af07d5a5-df63-4ddc-9383-6a666845ebe9).freeze
+
   attr_reader :document
 
   def initialize(document)
@@ -10,5 +15,15 @@ class ContentDataUrl
   def url
     content_data_root = Plek.new.external_url_for("content-data-admin")
     content_data_root + "/metrics" + document.live_edition.base_path
+  end
+
+  def displayable?(user)
+    has_content_data_access?(user)
+  end
+
+private
+
+  def has_content_data_access?(user)
+    CONTENT_DATA_BETA_PARTNERS.include?(user.organisation_content_id)
   end
 end
--- spec/services/content_data_url_spec.rb
+++ spec/services/content_data_url_spec.rb
@@ -1,13 +1,26 @@
 # frozen_string_literal: true
 
 RSpec.describe ContentDataUrl do
+  let(:document) { build(:document, :with_live_edition) }
+
   describe "#url" do
     it "returns a Content Data Admin data page URL for the document" do
-      document = build(:document, :with_live_edition)
       base_path = document.current_edition.base_path
       data_page_url = ContentDataUrl.new(document).url
 
       expect(data_page_url).to eq("https://content-data-admin.test.gov.uk/metrics#{base_path}")
+    end
+  end
+
+  describe "#displayable" do
+    it "returns true if the user is part of a Content Data beta partner organisation" do
+      user = build(:user, organisation_content_id: "af07d5a5-df63-4ddc-9383-6a666845ebe9")
+      expect(ContentDataUrl.new(document).displayable?(user)).to be true
+    end
+
+    it "returns false if the user is not part of a Content Data beta partner organisation" do
+      user = build(:user, organisation_content_id: SecureRandom.uuid)
+      expect(ContentDataUrl.new(document).displayable?(user)).to be false
     end
   end
 end
