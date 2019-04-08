06fc8e56daba8317d80023329773dfee469a8361
Ben Thorner <ben.thorner@digital.cabinet-office.gov.uk>
2019-04-08 18:43:13 +0100

    Use instance variables to avoid passing them
    This takes the Update and Destroy interactor even closer to the way we
    right features by using instance variables to avoid passing them between
    private methods. Using instance variables is more appropriate than
    attribute readers, as the attributes are not always well defined.
    
    Using instance variables takes us even further away from the use of the
    interactor context, which still provides an early abort mechanism.
    Arguably the context is just an extention of the instance variable
    feature provided by the Ruby language.

--- app/interactors/images/destroy_interactor.rb
+++ app/interactors/images/destroy_interactor.rb
@@ -7,48 +7,42 @@ class Images::DestroyInteractor
 
   def call
     Edition.transaction do
-      edition = find_and_lock_edition
-      image_revision = find_image(edition)
-
-      updater = update_edition(edition, image_revision)
-
-      create_timeline_entry(edition)
-      update_preview(edition)
-
-      update_context(edition: edition,
-                     image_revision: image_revision,
-                     updater: updater)
+      find_and_lock_edition
+      find_image
+      update_edition
+      create_timeline_entry
+      update_preview
+      update_context
     end
   end
 
 private
 
   def find_and_lock_edition
-    Edition.lock.find_current(document: params[:document])
+    @edition = Edition.lock.find_current(document: params[:document])
   end
 
-  def find_image(edition)
-    edition.image_revisions.find_by!(image_id: params[:image_id])
+  def find_image
+    @image_revision = @edition.image_revisions.find_by!(image_id: params[:image_id])
   end
 
-  def update_edition(edition, image_revision)
-    updater = Versioning::RevisionUpdater.new(edition.revision, user)
-    updater.remove_image(image_revision)
-    edition.assign_revision(updater.next_revision, user).save!
-    updater
+  def update_edition
+    @updater = Versioning::RevisionUpdater.new(@edition.revision, user)
+    @updater.remove_image(@image_revision)
+    @edition.assign_revision(@updater.next_revision, user).save!
   end
 
-  def create_timeline_entry(edition)
-    TimelineEntry.create_for_revision(entry_type: :image_deleted, edition: edition)
+  def create_timeline_entry
+    TimelineEntry.create_for_revision(entry_type: :image_deleted, edition: @edition)
   end
 
-  def update_preview(edition)
-    PreviewService.new(edition).try_create_preview
+  def update_preview
+    PreviewService.new(@edition).try_create_preview
   end
 
-  def update_context(edition:, image_revision:, updater:)
-    context.edition = edition
-    context.image_revision = image_revision
-    context.removed_lead_image = updater.removed_lead_image?
+  def update_context
+    context.edition = @edition
+    context.image_revision = @image_revision
+    context.removed_lead_image = @updater.removed_lead_image?
   end
 end
--- app/interactors/images/update_interactor.rb
+++ app/interactors/images/update_interactor.rb
@@ -7,78 +7,73 @@ class Images::UpdateInteractor
 
   def call
     Edition.transaction do
-      edition = find_and_lock_edition
-      image_revision = find_and_update_image(edition)
+      find_and_lock_edition
+      find_and_update_image
+      check_for_issues
+      update_edition
 
-      check_for_issues(edition, image_revision)
-      updater = update_edition(edition, image_revision)
-
-      if updater.changed?
-        create_timeline_entry(updater, edition)
-        update_preview(edition)
+      if @updater.changed?
+        create_timeline_entry
+        update_preview
       end
 
-      update_context(edition: edition,
-                     image_revision: image_revision,
-                     updater: updater)
+      update_context
     end
   end
 
 private
 
   def find_and_lock_edition
-    Edition.lock.find_current(document: params[:document])
+    @edition = Edition.lock.find_current(document: params[:document])
   end
 
-  def find_and_update_image(edition)
-    image_revision = edition.image_revisions.find_by!(image_id: params[:image_id])
+  def find_and_update_image
+    image_revision = @edition.image_revisions.find_by!(image_id: params[:image_id])
     image_params = params.require(:image_revision).permit(:caption, :alt_text, :credit)
 
     updater = Versioning::ImageRevisionUpdater.new(image_revision, user)
     updater.assign(image_params)
-    updater.next_revision
+    @image_revision = updater.next_revision
   end
 
-  def check_for_issues(edition, image_revision)
-    checker = Requirements::ImageRevisionChecker.new(image_revision)
+  def check_for_issues
+    checker = Requirements::ImageRevisionChecker.new(@image_revision)
     issues = checker.pre_preview_metadata_issues
     return unless issues.any?
 
     context.fail!(issues: issues,
-                  edition: edition,
-                  image_revision: image_revision)
+                  edition: @edition,
+                  image_revision: @image_revision)
   end
 
-  def update_edition(edition, image_revision)
+  def update_edition
     is_lead_image = params[:lead_image] == "on"
-    updater = Versioning::RevisionUpdater.new(edition.revision, user)
+    @updater = Versioning::RevisionUpdater.new(@edition.revision, user)
 
-    updater.update_image(image_revision, is_lead_image)
-    edition.assign_revision(updater.next_revision, user).save! if updater.changed?
-
-    updater
+    @updater.update_image(@image_revision, is_lead_image)
+    @edition.assign_revision(@updater.next_revision, user).save! if @updater.changed?
   end
 
-  def create_timeline_entry(updater, edition)
-    timeline_entry_type = if updater.selected_lead_image?
+  def create_timeline_entry
+    timeline_entry_type = if @updater.selected_lead_image?
                             :lead_image_selected
-                          elsif updater.removed_lead_image?
+                          elsif @updater.removed_lead_image?
                             :lead_image_removed
                           else
                             :image_updated
                           end
 
-    TimelineEntry.create_for_revision(entry_type: timeline_entry_type, edition: edition)
+    TimelineEntry.create_for_revision(entry_type: timeline_entry_type, edition: @edition)
   end
 
-  def update_preview(edition)
-    PreviewService.new(edition).try_create_preview
+  def update_preview
+    PreviewService.new(@edition).try_create_preview
   end
 
-  def update_context(edition:, image_revision:, updater:)
-    context.edition = edition
-    context.image_revision = image_revision
-    context.selected_lead_image = updater.selected_lead_image?
-    context.removed_lead_image = updater.removed_lead_image?
+  def update_context
+    context.edition = @edition
+    context.image_revision = @image_revision
+    context.selected_lead_image = @updater.selected_lead_image?
+    context.removed_lead_image = @updater.removed_lead_image?
   end
 end
