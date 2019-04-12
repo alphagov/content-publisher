f083573e373bb6d37b44700752193d6536f940cc
Kevin Dew <kevindew@me.com>
2019-04-12 20:10:52 +0100

    Documents interactors

---
+++ app/interactors/documents/destroy_interactor.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+class Documents::DestroyInteractor
+  include Interactor
+  delegate :params,
+           :user,
+           :edition,
+           to: :context
+
+  def call
+    Edition.transaction do
+      find_and_lock_edition
+      discard_draft
+      create_timeline_entry
+    end
+  end
+
+private
+
+  def find_and_lock_edition
+    context.edition = Edition.lock.find_current(document: params[:document])
+  end
+
+  def discard_draft
+    DeleteDraftService.new(edition.document, user).delete
+  end
+
+  def create_timeline_entry
+    TimelineEntry.create_for_status_change(entry_type: :draft_discarded,
+                                           status: edition.status)
+  end
+end
