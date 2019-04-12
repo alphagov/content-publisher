e932c6d79088fd8968aac2ea6cc2cc90e807d336
Kevin Dew <kevindew@me.com>
2019-04-12 19:21:03 +0100

    Contacts interactors

---
+++ app/interactors/contacts/insert_interactor.rb
@@ -0,0 +1,64 @@
+# frozen_string_literal: true
+
+class Contacts::InsertInteractor
+  include Interactor
+  delegate :params,
+           :user,
+           :edition,
+           :empty_submission,
+           :edition_updated,
+           to: :context
+
+  def call
+    Edition.transaction do
+      find_and_lock_edition
+      check_for_submission
+
+      update_edition
+
+      create_timeline_entry
+      update_preview
+    end
+  end
+
+private
+
+  def find_and_lock_edition
+    context.edition = Edition.lock.find_current(document: params[:document])
+  end
+
+  def check_for_submission
+    context.fail!(empty_submission: true) if params[:contact_id].empty?
+  end
+
+  def update_edition
+    contact_markdown = "[Contact:#{params[:contact_id]}]\n"
+    revision = edition.revision
+
+    body = revision.contents.fetch("body", "").chomp
+    updated_body = if body.present?
+                     "#{body}\n\n#{contact_markdown}"
+                   else
+                     contact_markdown
+                   end
+
+    updater = Versioning::RevisionUpdater.new(revision, user)
+    updater.assign(contents: revision.contents.merge("body" => updated_body))
+
+    edition.assign_revision(updater.next_revision, user).save! if updater.changed?
+    context.edition_updated = updater.changed?
+  end
+
+  def create_timeline_entry
+    return unless edition_updated
+
+    TimelineEntry.create_for_revision(entry_type: :updated_content, edition: edition)
+  end
+
+  def update_preview
+    return unless edition_updated
+
+    PreviewService.new(edition).try_create_preview
+  end
+end
+
