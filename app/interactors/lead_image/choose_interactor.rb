73da71cecb2a657dc1e3820c333f18a7d1e11ff6
Kevin Dew <kevindew@me.com>
2019-04-16 09:56:49 +0100

    Lead image interactors

---
+++ app/interactors/lead_image/choose_interactor.rb
@@ -0,0 +1,52 @@
+# frozen_string_literal: true
+
+class LeadImage::ChooseInteractor
+  include Interactor
+  delegate :params,
+           :user,
+           :edition,
+           :image_revision,
+           :unchanged,
+           to: :context
+
+  def call
+    Edition.transaction do
+      find_and_lock_edition
+      find_image_revision
+
+      choose_lead_image
+
+      create_timeline_entry
+      update_preview
+    end
+  end
+
+private
+
+  def find_and_lock_edition
+    context.edition = Edition.lock.find_current(document: params[:document])
+  end
+
+  def find_image_revision
+    context.image_revision = edition.image_revisions.find_by!(image_id: params[:image_id])
+  end
+
+  def choose_lead_image
+    updater = Versioning::RevisionUpdater.new(edition.revision, user)
+    updater.assign(lead_image_revision: image_revision)
+
+    if updater.changed?
+      edition.assign_revision(updater.next_revision, user).save!
+    else
+      context.fail!(unchanged: true)
+    end
+  end
+
+  def create_timeline_entry
+    TimelineEntry.create_for_revision(entry_type: :lead_image_selected, edition: edition)
+  end
+
+  def update_preview
+    PreviewService.new(edition).try_create_preview
+  end
+end
