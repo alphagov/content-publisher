01b3ddadc21ea7460dce84061b81a7536f946d0b
Alex Jurubita <alexandru.jurubita@digital.cabinet-office.gov.uk>
2019-04-17 12:44:51 +0100

    Extend times options with custom input
    Given we want to allow users to enter any time we need to play back their custom input after submission.

--- app/helpers/time_options_helper.rb
+++ app/helpers/time_options_helper.rb
@@ -4,8 +4,9 @@ module TimeOptionsHelper
   DEFAULT_PUBLISHING_TIME = "9:00am"
 
   def time_options(selected = nil)
-    options = times.map { |time| [time, time] }
-    selected_options = times.map { |time| [time, time] if time == (selected || DEFAULT_PUBLISHING_TIME) }.compact
+    extended_times = extend_times_with_selection(times, selected)
+    options = extended_times.map { |time| [time, time] }
+    selected_options = extended_times.select { |time| time == (selected || DEFAULT_PUBLISHING_TIME) }.compact
     options_for_select = { options: options, selected_options: selected_options }
     options_for_select
   end
@@ -22,5 +23,18 @@ private
     incremented_hours.each { |hour| meridiem_hours << hour + "pm" }
     meridiem_hours[0] = "12:01am"
     meridiem_hours
+  end
+
+  def extend_times_with_selection(times, selected)
+    if times && selected
+      position = 0
+      selected_meridiem = selected.chars.last(2).join
+      times.each_with_index do |time, index|
+        time_meridiem = time.chars.last(2).join
+        position = index if selected < time && time_meridiem == selected_meridiem && !position
+      end
+      times.insert(position, selected)
+    end
+    times
   end
 end
