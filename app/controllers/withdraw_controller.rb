b70e1bcb2724eba9299808244c5b155c4b631f5c
Kevin Dew <kevindew@me.com>
2019-04-17 21:25:54 +0100

    Withdraw interactor

--- app/controllers/withdraw_controller.rb
+++ app/controllers/withdraw_controller.rb
@@ -14,39 +14,32 @@ class WithdrawController < ApplicationController
   end
 
   def create
-    unless current_user.has_permission?(User::MANAGING_EDITOR_PERMISSION)
+    result = Withdraw::CreateInteractor.call(params: params, user: current_user)
+    edition, no_permission, issues, api_error = result.to_h.values_at(:edition,
+                                                                      :no_permission,
+                                                                      :issues,
+                                                                      :api_error)
+    if no_permission
       # FIXME: this shouldn't be an exception but we've not worked out the
       # right response - maybe bad request or a redirect with flash?
       raise "Can't withdraw an edition without managing editor permissions"
-    end
+    elsif issues
+      flash["alert_with_items"] = {
+        "title" => I18n.t!("withdraw.new.flashes.requirements"),
+        "items" => issues.items,
+      }
 
-    public_explanation = params[:public_explanation]
-
-    Edition.find_and_lock_current(document: params[:document]) do |edition|
-      issues = Requirements::WithdrawalChecker.new(public_explanation, edition)
-                                              .pre_withdrawal_issues
-
-      if issues.any?
-        flash["alert_with_items"] = {
-          "title" => I18n.t!("withdraw.new.flashes.requirements"),
-          "items" => issues.items,
-        }
-
-        render :new,
-               assigns: { edition: edition, public_explanation: public_explanation, issues: issues },
-               status: :unprocessable_entity
-        next
-      end
-
-      #FIXME We should check that the edition is withdrawable before passing
-      # it to the WithdrawService
-      WithdrawService.new.call(edition, public_explanation, current_user)
+      render :new,
+             assigns: { edition: edition,
+                        public_explanation: params[:public_explanation],
+                        issues: issues },
+             status: :unprocessable_entity
+    elsif api_error
+      redirect_to withdraw_path(params[:document]),
+                  alert_with_description: t("withdraw.new.flashes.publishing_api_error"),
+                  public_explanation: params[:public_explanation]
+    else
       redirect_to document_path(edition.document)
     end
-  rescue GdsApi::BaseError => e
-    GovukError.notify(e)
-    redirect_to withdraw_path(params[:document]),
-      alert_with_description: t("withdraw.new.flashes.publishing_api_error"),
-      public_explanation: params[:public_explanation]
   end
 end
