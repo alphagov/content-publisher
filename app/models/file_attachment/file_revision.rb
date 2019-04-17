c02916fbfd31f0eb3022e151cca12065fbc54a4c
Kevin Dew <kevindew@me.com>
2019-04-17 18:09:33 +0100

    Add missing association metadata to FileAttachment::FileRevision
    This got missed in the initial modelling work.

--- app/models/file_attachment/file_revision.rb
+++ app/models/file_attachment/file_revision.rb
@@ -2,7 +2,13 @@
 
 # This is an immutable model
 class FileAttachment::FileRevision < ApplicationRecord
+  belongs_to :blob, class_name: "ActiveStorage::Blob"
+
   belongs_to :created_by, class_name: "User", optional: true
+
+  has_many :assets, class_name: "FileAttachment::Asset"
+
+  delegate :content_type, to: :blob
 
   def readonly?
     !new_record?
