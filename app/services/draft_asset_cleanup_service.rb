272ab5998b5c42bee8d02469150aeb8862d03cd4
Peter Hartshorn <peter.hartshorn@digital.cabinet-office.gov.uk>
2019-04-25 15:51:09 +0100

    Update DraftAssetCleanupService
    This service now cleans up file_attachments as well as images.

--- app/services/draft_asset_cleanup_service.rb
+++ app/services/draft_asset_cleanup_service.rb
@@ -7,10 +7,7 @@ class DraftAssetCleanupService
 
     return unless previous_revision
 
-    current_assets = current_revision.image_revisions.flat_map(&:assets)
-    previous_assets = previous_revision.image_revisions.flat_map(&:assets)
-
-    delete_assets(previous_assets - current_assets)
+    delete_assets(previous_assets(previous_revision) - current_assets(current_revision))
   end
 
 private
@@ -27,5 +24,17 @@ private
 
       asset.absent!
     end
+  end
+
+  def current_assets(current_revision)
+    current_image_assets = current_revision.image_revisions.flat_map(&:assets)
+    current_file_attachment_assets = current_revision.file_attachment_revisions.flat_map(&:assets)
+    current_image_assets + current_file_attachment_assets
+  end
+
+  def previous_assets(previous_revision)
+    previous_image_assets = previous_revision.image_revisions.flat_map(&:assets)
+    previous_file_attachment_assets = previous_revision.file_attachment_revisions.flat_map(&:assets)
+    previous_image_assets + previous_file_attachment_assets
   end
 end
--- spec/services/draft_asset_cleanup_service_spec.rb
+++ spec/services/draft_asset_cleanup_service_spec.rb
@@ -5,7 +5,12 @@ RSpec.describe DraftAssetCleanupService do
     context "when the previous revision has draft assets that aren't used" do
       it "deletes the assets that aren't used" do
         image_revision = create(:image_revision, :on_asset_manager, state: :draft)
-        preceding_revision = create(:revision, image_revisions: [image_revision])
+        file_attachment_revision = create(:file_attachment_revision, :on_asset_manager)
+
+        preceding_revision = create(:revision,
+                                    image_revisions: [image_revision],
+                                    file_attachment_revisions: [file_attachment_revision])
+
         current_revision = create(:revision, preceded_by: preceding_revision)
         edition = create(:edition, revision: current_revision)
 
@@ -15,16 +20,24 @@ RSpec.describe DraftAssetCleanupService do
 
         expect(request).to have_been_requested.at_least_once
         expect(image_revision.assets.map(&:state).uniq).to match(%w[absent])
+        expect(file_attachment_revision.assets.map(&:state).uniq).to match(%w[absent])
       end
     end
 
     context "when the previous revision has assets that are used on the current revision" do
       it "doesn't change the assets" do
         image_revision = create(:image_revision, :on_asset_manager, state: :draft)
-        preceding_revision = create(:revision, image_revisions: [image_revision])
+        file_attachment_revision = create(:file_attachment_revision, :on_asset_manager)
+
+        preceding_revision = create(:revision,
+                                    image_revisions: [image_revision],
+                                    file_attachment_revisions: [file_attachment_revision])
+
         current_revision = create(:revision,
                                   image_revisions: [image_revision],
+                                  file_attachment_revisions: [file_attachment_revision],
                                   preceded_by: preceding_revision)
+
         edition = create(:edition, revision: current_revision)
 
         request = stub_asset_manager_deletes_any_asset
@@ -33,13 +46,19 @@ RSpec.describe DraftAssetCleanupService do
 
         expect(request).not_to have_been_requested
         expect(image_revision.assets.map(&:state).uniq).to match(%w[draft])
+        expect(file_attachment_revision.assets.map(&:state).uniq).to match(%w[draft])
       end
     end
 
     context "when the previous revision has assets that are live" do
       it "doesn't change the assets" do
         image_revision = create(:image_revision, :on_asset_manager, state: :live)
-        preceding_revision = create(:revision, image_revisions: [image_revision])
+        file_attachment_revision = create(:file_attachment_revision, :on_asset_manager, state: :live)
+
+        preceding_revision = create(:revision,
+                                    image_revisions: [image_revision],
+                                    file_attachment_revisions: [file_attachment_revision])
+
         current_revision = create(:revision, preceded_by: preceding_revision)
         edition = create(:edition, revision: current_revision)
 
@@ -49,6 +68,7 @@ RSpec.describe DraftAssetCleanupService do
 
         expect(request).not_to have_been_requested
         expect(image_revision.assets.map(&:state).uniq).to match(%w[live])
+        expect(file_attachment_revision.assets.map(&:state).uniq).to match(%w[live])
       end
     end
 
@@ -65,7 +85,12 @@ RSpec.describe DraftAssetCleanupService do
     context "when assets to remove are not on asset manager" do
       it "marks the assets as absent" do
         image_revision = create(:image_revision, :on_asset_manager, state: :draft)
-        preceding_revision = create(:revision, image_revisions: [image_revision])
+        file_attachment_revision = create(:file_attachment_revision, :on_asset_manager, state: :draft)
+
+        preceding_revision = create(:revision,
+                                    image_revisions: [image_revision],
+                                    file_attachment_revisions: [file_attachment_revision])
+
         current_revision = create(:revision, preceded_by: preceding_revision)
         edition = create(:edition, revision: current_revision)
 
@@ -74,6 +99,7 @@ RSpec.describe DraftAssetCleanupService do
         DraftAssetCleanupService.new.call(edition)
 
         expect(image_revision.assets.map(&:state).uniq).to match(%w[absent])
+        expect(file_attachment_revision.assets.map(&:state).uniq).to match(%w[absent])
       end
     end
   end
