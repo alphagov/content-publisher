7610eace6a0eed010113a40f31dac6a1d59beec2
Emma Beynon <emma.beynon@digital.cabinet-office.gov.uk>
2019-05-02 12:49:50 +0000

    Display attachment links on file attachment page
    This makes use of the attachment link component to render an attachment link
    'preview' for each uploaded attachment. For now it won't link to anything while
    we work out which URL to link to in future work.  We also replace the
    attachment fixture we use for tests with a PDF so we can test for PDF-specific
    features such as displaying a count of pages.

--- spec/features/editing_file_attachments/upload_file_attachment_spec.rb
+++ spec/features/editing_file_attachments/upload_file_attachment_spec.rb
@@ -7,6 +7,7 @@ RSpec.feature "Upload file attachment" do
     and_i_go_to_insert_an_attachment
     and_i_upload_a_file_attachment
     then_i_can_see_the_attachment_markdown
+    and_i_can_see_a_preview_of_the_attachment_link
     and_the_attachment_has_been_uploaded_successfully
   end
 
@@ -30,15 +31,23 @@ RSpec.feature "Upload file attachment" do
     @asset_manager_request = stub_asset_manager_receives_an_asset(filename: @attachment_filename)
     @publishing_api_request = stub_publishing_api_put_content(@edition.content_id, {})
 
-    @attachment_filename = "attachment.csv"
+    @attachment_filename = "13kb-1-page-attachment.pdf"
     find('form input[type="file"]').set(Rails.root.join(file_fixture(@attachment_filename)))
     fill_in "title", with: "A title"
     click_on "Upload"
   end
 
   def then_i_can_see_the_attachment_markdown
-    expect(page).to have_content("[Attachment: attachment.csv]")
-    expect(page).to have_content("[AttachmentLink: attachment.csv]")
+    expect(page).to have_content("[Attachment: 13kb-1-page-attachment.pdf]")
+  end
+
+  def and_i_can_see_a_preview_of_the_attachment_link
+    attachment = FileAttachment::Revision.last
+
+    within(".gem-c-attachment-link") do
+      expect(page).to have_link(attachment.title, href: "#")
+      expect(page).to have_content("PDF, 13 KB, 1 page")
+    end
   end
 
   def and_the_attachment_has_been_uploaded_successfully
