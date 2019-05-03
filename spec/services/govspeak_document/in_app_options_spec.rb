a2e8132d802ed9fd97d58871602b3d073bf19480
Kevin Dew <kevindew@me.com>
2019-05-03 22:19:14 +0100

    Refactor hash comparison for payloads
    This changes the multiple expectations to a single one.

--- spec/services/govspeak_document/in_app_options_spec.rb
+++ spec/services/govspeak_document/in_app_options_spec.rb
@@ -13,12 +13,16 @@ RSpec.describe GovspeakDocument::InAppOptions do
 
       in_app_options = GovspeakDocument::InAppOptions.new("govspeak", edition)
       actual_image_options = in_app_options.to_h[:images].first
-      expect(actual_image_options[:alt_text]).to eq("Some alt text")
-      expect(actual_image_options[:caption]).to eq("An optional caption")
-      expect(actual_image_options[:credit]).to eq("An optional credit")
-      expect(actual_image_options[:url]).to match("/representations")
-      expect(actual_image_options[:url]).to match("filename.png")
-      expect(actual_image_options[:id]).to eq("filename.png")
+
+      expect(actual_image_options).to match(
+        a_hash_including(
+          id: "filename.png",
+          alt_text: "Some alt text",
+          caption: "An optional caption",
+          credit: "An optional credit",
+          url: a_string_matching(%r{/representations/.*/filename.png}),
+        ),
+      )
     end
 
     it "returns the local representation of a file attachment" do
@@ -33,13 +37,17 @@ RSpec.describe GovspeakDocument::InAppOptions do
       in_app_options = GovspeakDocument::InAppOptions.new("govspeak", edition)
       actual_attachment_options = in_app_options.to_h[:attachments].first
 
-      expect(actual_attachment_options[:filename]).to eq("13kb-1-page-attachment.pdf")
-      expect(actual_attachment_options[:title]).to eq("A title")
-      expect(actual_attachment_options[:content_type]).to eq("application/pdf")
-      expect(actual_attachment_options[:number_of_pages]).to eq(1)
-      expect(actual_attachment_options[:file_size]).to eq(13264)
-      # TODO add url expectation when we have an internal preview URL
-      expect(actual_attachment_options[:id]).to eq("13kb-1-page-attachment.pdf")
+      expect(actual_attachment_options).to match(
+        a_hash_including(
+          id: "13kb-1-page-attachment.pdf",
+          filename: "13kb-1-page-attachment.pdf",
+          title: "A title",
+          content_type: "application/pdf",
+          number_of_pages: 1,
+          file_size: 13264,
+          # TODO add url expectation when we have an internal preview URL
+        ),
+      )
     end
   end
 end
--- spec/services/govspeak_document/payload_options_spec.rb
+++ spec/services/govspeak_document/payload_options_spec.rb
@@ -14,12 +14,16 @@ RSpec.describe GovspeakDocument::PayloadOptions do
 
       payload_options = GovspeakDocument::PayloadOptions.new("govspeak", edition)
       actual_image_options = payload_options.to_h[:images].first
-      expect(actual_image_options[:alt_text]).to eq("Some alt text")
-      expect(actual_image_options[:caption]).to eq("An optional caption")
-      expect(actual_image_options[:credit]).to eq("An optional credit")
-      expect(actual_image_options[:url]).to match("filename.png")
-      expect(actual_image_options[:url]).to match("/media")
-      expect(actual_image_options[:id]).to eq("filename.png")
+
+      expect(actual_image_options).to match(
+        a_hash_including(
+          id: "filename.png",
+          alt_text: "Some alt text",
+          caption: "An optional caption",
+          credit: "An optional credit",
+          url: a_string_matching(%r{/media/.*/filename.png}),
+        ),
+      )
     end
 
     it "returns the remote asset options for a file attachment" do
@@ -35,14 +39,17 @@ RSpec.describe GovspeakDocument::PayloadOptions do
       payload_options = GovspeakDocument::PayloadOptions.new("govspeak", edition)
       actual_attachment_options = payload_options.to_h[:attachments].first
 
-      expect(actual_attachment_options[:filename]).to eq("13kb-1-page-attachment.pdf")
-      expect(actual_attachment_options[:title]).to eq("A title")
-      expect(actual_attachment_options[:content_type]).to eq("application/pdf")
-      expect(actual_attachment_options[:number_of_pages]).to eq(1)
-      expect(actual_attachment_options[:file_size]).to eq(13264)
-      expect(actual_attachment_options[:url]).to match("13kb-1-page-attachment.pdf")
-      expect(actual_attachment_options[:url]).to match("/media")
-      expect(actual_attachment_options[:id]).to eq("13kb-1-page-attachment.pdf")
+      expect(actual_attachment_options).to match(
+        a_hash_including(
+          id: "13kb-1-page-attachment.pdf",
+          filename: "13kb-1-page-attachment.pdf",
+          title: "A title",
+          content_type: "application/pdf",
+          number_of_pages: 1,
+          file_size: 13264,
+          url: a_string_matching(%r{/media/.*/13kb-1-page-attachment.pdf}),
+        ),
+      )
     end
   end
 end
