5d4c9c7e53ea9c9844c0e493ba2e0816cd50a954
Kevin Dew <kevindew@me.com>
2019-05-03 21:43:47 +0100

    Add helper for file attachment attributes
    There are multiple places where a file attachment is converted into a
    hash with the same properties. This creates a helper that can be used to
    share these rules and avoid the need to repeat.
    
    The in_app terminology originates from the GovspeakDocument set of
    classes where in app is used for data used in the app, whereas payload
    is used for the Publishing API payload.

---
+++ app/helpers/file_attachment_helper.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+module FileAttachmentHelper
+  def file_attachment_in_app_attributes(file_attachment)
+    file_attachment_attributes(file_attachment).merge(
+      url: "#",
+    )
+  end
+
+private
+
+  def file_attachment_attributes(file_attachment)
+    {
+      id: file_attachment.filename,
+      title: file_attachment.title,
+      filename: file_attachment.filename,
+      content_type: file_attachment.content_type,
+      file_size: file_attachment.byte_size,
+      number_of_pages: file_attachment.number_of_pages,
+    }
+  end
+end
