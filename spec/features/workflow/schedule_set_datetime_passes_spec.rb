b40f2b85d74f70407873d07aa4ce6d7f0fd82654
Emma Beynon <emma.beynon@digital.cabinet-office.gov.uk>
2019-05-31 10:53:34 +0000

    User can change scheduled datetime when it has passed
    This adds a test to ensure users can see the link to change the scheduled
    publishing datetime when it has passed and extracts the scheduling notice logic
    and components into their own partial.

--- spec/features/workflow/schedule_set_datetime_passes_spec.rb
+++ spec/features/workflow/schedule_set_datetime_passes_spec.rb
@@ -5,6 +5,7 @@ RSpec.feature "Set scheduled publishing date and time passes before scheduling" 
     given_there_is_an_edition_with_past_scheduled_publishing_datetime
     when_i_visit_the_summary_page
     then_i_cannot_see_a_schedule_link
+    and_i_can_see_a_change_date_link
   end
 
   def given_there_is_an_edition_with_past_scheduled_publishing_datetime
@@ -18,5 +19,9 @@ RSpec.feature "Set scheduled publishing date and time passes before scheduling" 
 
   def then_i_cannot_see_a_schedule_link
     expect(page).not_to have_button("Schedule")
+  end
+
+  def and_i_can_see_a_change_date_link
+    expect(page).to have_link("Change date")
   end
 end
