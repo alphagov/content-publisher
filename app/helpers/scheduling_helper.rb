f1219714a1d08d167337252c2ac228110cbcce7f
Kevin Dew <kevindew@me.com>
2019-06-05 17:36:51 +0100

    Rename ScheduledDatetimeHelper to SchedulingHelper
    Since this is used across a bunch of scheduling it makes sense to have a
    more generic naming. I've also renamed the methods to be distinct from
    the common variable names of scheduled_date and scheduled_time.

--- app/helpers/scheduling_helper.rb
+++ app/helpers/scheduling_helper.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
-module ScheduledDatetimeHelper
-  def scheduled_date(datetime)
+module SchedulingHelper
+  def format_scheduled_date(datetime)
     datetime&.strftime("%-d %B %Y")
   end
 
-  def scheduled_time(datetime)
+  def format_scheduled_time(datetime)
     datetime&.strftime("%l:%M%P")
   end
 end
