61ea662d623139268bd56869aeff93711a7be764
Emma Beynon <emma.beynon@digital.cabinet-office.gov.uk>
2019-06-06 14:37:31 +0000

    Create Clear Datetime interactor
    This extracts the logic for clearing a scheduled publishing datetime into an
    interactor.  We have made a content decision that we no longer need to create a
    timeline entry when a scheduled publishing datetime is clearer as this is an
    edit. This is consistent with not creating a timeline entry when a scheduled
    publishing datetime is updated.  We have also decided not to check if a
    scheduled publishing datetime is present before clearing it, as if it's not
    present the updater won't assign a new revision as it won't have changed.

---
+++ app/interactors/schedule/clear_datetime_interactor.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+class Schedule::ClearDatetimeInteractor
+  include Interactor
+  delegate :params,
+           :edition,
+           :user,
+           to: :context
+
+  def call
+    Edition.transaction do
+      find_and_lock_edition
+      clear_scheduled_publishing_datetime
+    end
+  end
+
+private
+
+  def find_and_lock_edition
+    context.edition = Edition.lock.find_current(document: params[:document])
+  end
+
+  def clear_scheduled_publishing_datetime
+    context.fail! unless edition.editable?
+
+    updater = Versioning::RevisionUpdater.new(edition.revision, user)
+    updater.assign(scheduled_publishing_datetime: nil)
+
+    if updater.changed?
+      edition.assign_revision(updater.next_revision, user).save!
+    end
+  end
+end
