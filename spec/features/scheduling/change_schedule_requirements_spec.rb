c9856b6fcdf87a4e906216de20028c24782f808d
Ben Thorner <ben.thorner@digital.cabinet-office.gov.uk>
2019-06-12 17:20:57 +0100

    Expand change (proposed) schedule specs
    Previously we checked all (proposed) schedule date/time issues in one
    class, so only one superficial check was necessary at the feature level.
    This adds a second scenario to the the two requirements features for
    changing a (proposed) schedule date/time to cover validation errors and
    semantic issues separately.

--- spec/features/scheduling/change_schedule_requirements_spec.rb
+++ spec/features/scheduling/change_schedule_requirements_spec.rb
@@ -1,12 +1,20 @@
 # frozen_string_literal: true
 
 RSpec.feature "Change schedule with requirements issues" do
-  scenario do
+  scenario "invalid date/time" do
     given_there_is_a_scheduled_edition
     when_i_visit_the_summary_page
     and_i_click_on_change_date
     and_i_enter_invalid_date_fields
-    then_i_see_an_error_to_fix_the_issues
+    then_i_see_an_error_to_fix_the_input
+  end
+
+  scenario "scheduling issues" do
+    given_there_is_a_scheduled_edition
+    when_i_visit_the_summary_page
+    and_i_click_on_change_date
+    and_i_enter_an_unschedulable_date
+    then_i_see_an_error_to_change_the_date
   end
 
   def given_there_is_a_scheduled_edition
@@ -26,7 +34,22 @@ RSpec.feature "Change schedule with requirements issues" do
     click_on "Save date"
   end
 
-  def then_i_see_an_error_to_fix_the_issues
+  def and_i_enter_an_unschedulable_date
+    @date = 1.day.ago
+    fill_in "schedule[date][day]", with: @date.day
+    fill_in "schedule[date][month]", with: @date.month
+    fill_in "schedule[date][year]", with: @date.year
+    select "11:00pm", from: "schedule[time]"
+    click_on "Save date"
+  end
+
+  def then_i_see_an_error_to_change_the_date
+    expect(page).to have_content(
+      I18n.t!("requirements.schedule_date.in_the_past.form_message"),
+    )
+  end
+
+  def then_i_see_an_error_to_fix_the_input
     expect(page).to have_content(
       I18n.t!("requirements.schedule_date.invalid.form_message"),
     )
