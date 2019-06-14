be778ef0181346c907d9a31fddd719dc390f38c7
Kevin Dew <kevindew@me.com>
2019-06-14 17:18:06 +0100

    Service to handle scheduled publishing failing
    This accepts an edition id as this is the easiest attribute available
    once the job has failed.

---
+++ app/services/scheduled_publishing_failed_service.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+class ScheduledPublishingFailedService
+  def call(edition_id)
+    edition = nil
+
+    Edition.transaction do
+      edition = Edition.lock.find_current(id: edition_id)
+      update_status(edition)
+    end
+
+    notify_editors(edition)
+  end
+
+private
+
+  def update_status(edition)
+    raise "Expected edition to be scheduled" unless edition.scheduled?
+
+    edition.assign_status(:failed_to_publish,
+                          edition.status.created_by,
+                          update_last_edited: false,
+                          status_details: edition.status.details)
+
+    edition.save!
+  end
+
+  def notify_editors(edition)
+    # TODO
+  end
+end
