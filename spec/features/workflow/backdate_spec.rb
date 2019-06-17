8ed9787ede4a9f779574f81f0aa28b23f79b7cee
Emma Beynon <emma.beynon@digital.cabinet-office.gov.uk>
2019-06-17 16:32:14 +0000

    Only show backdating if first edition & editable
    We are only allowing users to backdate a document on its first edition in order
    to avoid the risk of messy public edition history, so we have decided to only
    show the link to backdate the document when the first edition is in draft.

--- spec/features/workflow/backdate_spec.rb
+++ spec/features/workflow/backdate_spec.rb
@@ -1,13 +1,25 @@
 # frozen_string_literal: true
 
-RSpec.feature "Backdate the first edition of a document" do
-  scenario do
+RSpec.feature "Backdate content" do
+  scenario "first edition" do
     given_there_is_a_document_with_a_first_edition
     when_i_visit_the_summary_page
     and_i_click_to_backdate_the_content
     and_i_enter_a_date_to_backdate_the_content_to
     and_i_click_save
     then_i_see_the_date_the_content_was_backdated_to
+  end
+
+  scenario "subsequent edition" do
+    given_there_is_a_document_with_a_second_edition
+    when_i_visit_the_summary_page
+    then_i_cannot_see_a_backdate_section
+  end
+
+  scenario "published edition" do
+    given_there_is_a_published_first_edition
+    when_i_visit_the_summary_page
+    then_i_cannot_see_a_backdate_section
   end
 
   def given_there_is_a_document_with_a_first_edition
@@ -34,5 +46,20 @@ RSpec.feature "Backdate the first edition of a document" do
 
   def then_i_see_the_date_the_content_was_backdated_to
     expect(page).to have_content("1 January 2019")
+  end
+
+  def given_there_is_a_document_with_a_second_edition
+    @edition = create(:edition, number: 2)
+  end
+
+  def then_i_cannot_see_a_backdate_section
+    expect(page).not_to have_content(
+      I18n.t!("documents.show.content_settings.backdate.title"),
+    )
+    expect(page).not_to have_content("Edit Backdate")
+  end
+
+  def given_there_is_a_published_first_edition
+    @edition = create(:edition, :published, number: 1)
   end
 end
