1cfc7144830ccdda538bbfffd285e276d3fb3608
Emma Beynon <emma.beynon@digital.cabinet-office.gov.uk>
2019-06-24 10:24:48 +0000

    Add date and time combination helpers
    We have a few instances in the app where we display "[date] at [time]" or
    "[time] on [date]" so we add helper methods for these to the DateTimeHelper.

--- app/helpers/date_time_helper.rb
+++ app/helpers/date_time_helper.rb
@@ -8,4 +8,12 @@ module DateTimeHelper
   def format_time(datetime)
     datetime.strftime("%-l:%M%P")
   end
+
+  def format_date_at_time(datetime)
+    datetime.strftime("%-d %B %Y at %-l:%M%P")
+  end
+
+  def format_time_on_date(datetime)
+    datetime.strftime("%-l:%M%P on %-d %B %Y")
+  end
 end
