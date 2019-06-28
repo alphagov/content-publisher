6134e4528c1298bbe3c9bb89f8875384c2dc9d98
Alex Jurubita <alexandru.jurubita@digital.cabinet-office.gov.uk>
2019-06-28 11:19:49 +0100

    Add event listener for search topic and send selected suggestion to GTM

---
+++ app/assets/javascripts/modules/gtm-topics-listener.js
@@ -0,0 +1,22 @@
+var GtmTopicsListener = {}
+
+GtmTopicsListener.handleTopicClick = function (dataGtm) {
+  return function (event) {
+    var element = event.target
+    var topic = event.detail
+
+    if (!element || !topic.topicNames) {
+      return
+    }
+
+    var topicName = topic.topicNames.join(' > ')
+
+    window.dataLayer.push({
+      event: 'Click',
+      dataGtm: dataGtm,
+      dataGtmValue: topicName
+    })
+  }
+}
+
+window.addEventListener('search-topic', GtmTopicsListener.handleTopicClick('select-topic-from-search-results'))
