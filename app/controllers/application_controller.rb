f408731f4a7333ea5c2022837c6ed1386fcae02e
Ben Thorner <ben.thorner@digital.cabinet-office.gov.uk>
2019-07-12 14:44:46 +0100

    Redirect when a edition assertions fail
    Previously we let failed assertions propagate into 500 errors. This
    changes the approach to redirect to the document summary when edition
    assertions fail. Examples of when this might happen are when a user is
    working on an edition in multiple tabs (and forgets to refresh), or when
    multiple users are working on the same edition over a period.
    
    One special case is when the first edition is discarded, in which case
    there is no edition to redirect to, so we redirect to the index page.
    
    Another special case is the modal. Here we take a simpler approach that
    causes the modal to display an error, with the intention that the user
    will eventually refresh the page and trigger the normal assertion
    redirect. Note that this relies on the current situation where no modal
    action involves an action-specific behaviour when the assertion fails.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,6 +11,18 @@ class ApplicationController < ActionController::Base
 
   add_flash_types :alert_with_description, :alert_with_items, :confirmation, :tried_to_publish, :tried_to_preview
 
+  rescue_from EditionAssertions::StateError do |e|
+    Rails.logger.warn(e.message)
+
+    if rendering_context == "modal"
+      render nothing: true, status: :bad_request
+    elsif e.edition.first? && e.edition.discarded?
+      redirect_to documents_path
+    else
+      redirect_to document_path(e.edition.document)
+    end
+  end
+
   def rendering_context
     request.headers["Content-Publisher-Rendering-Context"] || "application"
   end
