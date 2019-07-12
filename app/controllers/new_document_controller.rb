08e0beefbab6462c5d93a999b9d2d3d1b5b088a1
Ben Thorner <ben.thorner@digital.cabinet-office.gov.uk>
2019-07-12 16:39:19 +0100

    Move new document actions into interactors
    This got missed during the original refactoring.

--- app/controllers/new_document_controller.rb
+++ app/controllers/new_document_controller.rb
@@ -1,71 +1,38 @@
 # frozen_string_literal: true
 
 class NewDocumentController < ApplicationController
-  def choose_supertype
-    @supertypes = Supertype.all
-  end
-
   def choose_document_type
-    if params[:supertype].blank?
-      flash.now["requirements"] = { "items" => supertype_issues.items }
+    result = NewDocument::ChooseSupertypeInteractor.call(params: params, user: current_user)
+    issues, @supertype = result.to_h.values_at(:issues, :supertype)
+
+    if result.issues
+      flash.now["requirements"] = { "items" => issues.items }
 
       render :choose_supertype,
-             assigns: { issues: supertype_issues, supertypes: Supertype.all },
+             assigns: { issues: issues },
              status: :unprocessable_entity
-      return
-    end
-
-    @supertype = Supertype.find(params[:supertype])
-
-    if @supertype.managed_elsewhere
+    elsif @supertype.managed_elsewhere
       redirect_to @supertype.managed_elsewhere_url
-      return
     end
   end
 
   def create
-    if params[:document_type].blank?
-      flash.now["requirements"] = { "items" => document_type_issues.items }
+    result = NewDocument::CreateInteractor.call(params: params, user: current_user)
+    issues, supertype, document_type, document = result.to_h.values_at(:issues,
+                                                                       :supertype,
+                                                                       :document_type,
+                                                                       :document)
+
+    if issues
+      flash.now["requirements"] = { "items" => issues.items }
 
       render :choose_document_type,
-             assigns: { issues: document_type_issues,
-                        supertype: Supertype.find(params[:supertype]) },
+             assigns: { issues: issues, supertype: supertype },
              status: :unprocessable_entity
-      return
-    end
-
-    document_type = DocumentType.find(params[:document_type])
-
-    if document_type.managed_elsewhere
+    elsif result.managed_elsewhere
       redirect_to document_type.managed_elsewhere_url
-      return
+    else
+      redirect_to edit_document_path(document)
     end
-
-    document = Document.create_initial(document_type_id: params[:document_type],
-                                       tags: default_tags,
-                                       user: current_user)
-
-    TimelineEntry.create_for_status_change(entry_type: :created,
-                                           status: document.current_edition.status)
-
-    redirect_to edit_document_path(document)
-  end
-
-private
-
-  def default_tags
-    current_user.organisation_content_id ? { primary_publishing_organisation: [current_user.organisation_content_id] } : {}
-  end
-
-  def document_type_issues
-    @document_type_issues ||= Requirements::CheckerIssues.new([
-      Requirements::Issue.new(:document_type, :not_selected),
-    ])
-  end
-
-  def supertype_issues
-    @supertype_issues ||= Requirements::CheckerIssues.new([
-      Requirements::Issue.new(:supertype, :not_selected),
-    ])
   end
 end
---
+++ app/interactors/new_document/choose_supertype_interactor.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+class NewDocument::ChooseSupertypeInteractor
+  include Interactor
+
+  delegate :user,
+           :params,
+           :supertype,
+           to: :context
+
+  def call
+    check_for_issues
+    find_supertype
+  end
+
+private
+
+  def check_for_issues
+    return if params[:supertype].present?
+
+    context.fail!(issues: supertype_issues)
+  end
+
+  def find_supertype
+    context.supertype = Supertype.find(params[:supertype])
+  end
+
+  def supertype_issues
+    Requirements::CheckerIssues.new([
+      Requirements::Issue.new(:supertype, :not_selected),
+    ])
+  end
+end
