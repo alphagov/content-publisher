58365607b11c1695a4950117bd479f46e3346d7e
Ben Thorner <ben.thorner@digital.cabinet-office.gov.uk>
2019-07-24 18:00:48 +0100

    Fix I18n coverage issue for tag checker
    Previously we did not cover the summary translation for the issue where
    a primary org is blank. This adds a test for this checker to ensure the
    translation is covered.

--- app/services/requirements/tag_checker.rb
+++ app/services/requirements/tag_checker.rb
@@ -12,13 +12,22 @@ module Requirements
     def pre_publish_issues
       issues = CheckerIssues.new
 
-      if edition.document_type.tags.map(&:id).include?("primary_publishing_organisation")
-        if revision.primary_publishing_organisation_id.blank?
-          issues << Issue.new(:primary_publishing_organisation, :blank)
-        end
+      if should_have_primary_org? && has_no_primary_org?
+        issues << Issue.new(:primary_publishing_organisation, :blank)
       end
 
       issues
+    end
+
+  private
+
+    def should_have_primary_org?
+      edition.document_type.tags.map(&:id)
+       .include?("primary_publishing_organisation")
+    end
+
+    def has_no_primary_org?
+      revision.primary_publishing_organisation_id.blank?
     end
   end
 end
