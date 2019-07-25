37bcde3fea642506b8a2e59f485fb11026d7b903
Alex Jurubita <alexandru.jurubita@digital.cabinet-office.gov.uk>
2019-07-25 18:08:34 +0100

    Update tests to reflect changes

--- spec/features/editing_content/insert_contact_no_js_spec.rb
+++ spec/features/editing_content/insert_contact_no_js_spec.rb
@@ -1,14 +1,14 @@
 # frozen_string_literal: true
 
-RSpec.feature "Insert contact", js: true do
+RSpec.feature "Insert contact without Javascript" do
   include AccessibleAutocompleteHelper
 
   scenario do
     given_there_is_an_edition
     when_i_go_to_edit_the_edition
-    and_i_click_to_insert_a_contact
-    and_i_select_a_contact
-    then_i_see_the_snippet_is_inserted
+    and_i_go_to_add_a_contact
+    when_i_select_a_contact
+    then_i_can_see_the_contact_in_the_body
   end
 
   let(:organisation) { { "content_id" => SecureRandom.uuid, "internal_name" => "Organisation" } }
@@ -36,21 +36,19 @@ RSpec.feature "Insert contact", js: true do
     click_on "Edit Content"
   end
 
-  def and_i_click_to_insert_a_contact
+  def and_i_go_to_add_a_contact
     @request = stub_publishing_api_put_content(@edition.content_id, {})
     find("markdown-toolbar details").click
     click_on "Contact"
   end
 
-  def and_i_select_a_contact
-    accessible_autocomplete_select "Contact",
-                                   for_id: "contact-id",
-                                   value: contact["content_id"]
+  def when_i_select_a_contact
+    select "Contact", from: "contact-id"
     click_on "Insert contact"
   end
 
-  def then_i_see_the_snippet_is_inserted
+  def then_i_can_see_the_contact_in_the_body
     expect(find_field("revision[contents][body]").value)
-      .to match(/\[Contact: #{contact['content_id']}\]/)
+      .to match(/\[Contact:#{contact['content_id']}\]/)
   end
 end
