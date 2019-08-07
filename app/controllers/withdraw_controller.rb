d490621910186dd33e88e679eb95decbe0c61f05
Ben Thorner <ben.thorner@digital.cabinet-office.gov.uk>
2019-08-07 14:56:30 +0100

    Use before_action to check withdraw permissions
    Previously we had a couple of approaches to enforcing that withdraw and
    unwithdraw actions can only be done by a user with the 'managing editor'
    permission. This refactors the enforcement code to consistently use a
    before_action and render the associated error page. Since we only have
    two instances of pages for this permission, it's not clear that we want
    to de-duplicate the code and the views yet.

--- app/controllers/withdraw_controller.rb
+++ app/controllers/withdraw_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class WithdrawController < ApplicationController
+  before_action :check_permission
+
   def new
     @edition = Edition.find_current(document: params[:document])
     @public_explanation = @edition.withdrawn? ? @edition.status.details.public_explanation : nil
@@ -8,25 +10,13 @@ class WithdrawController < ApplicationController
     assert_edition_state(@edition, assertion: "is published or withdrawn") do
       @edition.published? || @edition.published_but_needs_2i? || @edition.withdrawn?
     end
-
-    if current_user.has_permission?(User::MANAGING_EDITOR_PERMISSION)
-      render :new
-    else
-      render :non_managing_editor, status: :forbidden
-    end
   end
 
   def create
     result = Withdraw::CreateInteractor.call(params: params, user: current_user)
-    edition, no_permission, issues, api_error = result.to_h.values_at(:edition,
-                                                                      :no_permission,
-                                                                      :issues,
-                                                                      :api_error)
-    if no_permission
-      # FIXME: this shouldn't be an exception but we've not worked out the
-      # right response - maybe bad request or a redirect with flash?
-      raise "Can't withdraw an edition without managing editor permissions"
-    elsif issues
+    edition, issues, api_error = result.to_h.values_at(:edition, :issues, :api_error)
+
+    if issues
       flash.now["requirements"] = { "items" => issues.items }
 
       render :new,
@@ -41,5 +31,14 @@ class WithdrawController < ApplicationController
     else
       redirect_to document_path(edition.document)
     end
+  end
+
+private
+
+  def check_permission
+    return if current_user.has_permission?(User::MANAGING_EDITOR_PERMISSION)
+
+    @edition = Edition.find_current(document: params[:document])
+    render :non_managing_editor, status: :forbidden
   end
 end
--- app/interactors/withdraw/create_interactor.rb
+++ app/interactors/withdraw/create_interactor.rb
@@ -4,15 +4,12 @@ class Withdraw::CreateInteractor < ApplicationInteractor
   delegate :params,
            :user,
            :edition,
-           :no_permission,
            :issues,
            :api_error,
            :already_withdrawn,
            to: :context
 
   def call
-    check_permission
-
     Edition.transaction do
       find_and_lock_edition
       check_for_issues
@@ -23,12 +20,6 @@ class Withdraw::CreateInteractor < ApplicationInteractor
   end
 
 private
-
-  def check_permission
-    unless user.has_permission?(User::MANAGING_EDITOR_PERMISSION)
-      context.fail!(no_permission: true)
-    end
-  end
 
   def find_and_lock_edition
     context.edition = Edition.lock.find_current(document: params[:document])
