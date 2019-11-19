44ecc5c5dc35974e532a97ac8516650582c96646
Kevin Dew <kevindew@me.com>
2019-11-19 18:44:00 +0000

    Determine whether edition is political is not
    This is a port of the logic from Whitehall that is used to determine
    whether an edition is political [1]. This is simplified as Content
    Publisher is currently only working with news and press releases and
    will need to be expanded with the introduction of more document types
    (or logic abstracted to those document types).
    
    Whitehall checks whether content is associated with a minister and a
    political organisation. Content Publisher however associates with role
    appointments and isn't aware whether these are ministers or not. We
    expect that nearly all associations will be ministers so until we have
    the ability to determine between these just a check of a role
    appointment should suffice.
    
    [1]: https://github.com/alphagov/whitehall/blob/9afd13104412f70d0b739cada49dd1c5629a94b2/lib/political_content_identifier.rb

---
+++ lib/political_edition_identifier.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+class PoliticalEditionIdentifier
+  def self.political_organisation_ids
+    @political_organisation_ids ||= YAML.load_file(
+      Rails.root.join("config", "political_organisations.yml"),
+    )
+  end
+
+  attr_reader :edition
+
+  def initialize(edition)
+    @edition = edition
+  end
+
+  def political?
+    associated_with_role_appointments? || associated_with_political_organisations?
+  end
+
+private
+
+  def associated_with_role_appointments?
+    edition.tags["role_appointments"]&.any?
+  end
+
+  def associated_with_political_organisations?
+    organisation_ids = edition.supporting_organisation_ids +
+      [edition.primary_publishing_organisation_id].compact
+
+    (organisation_ids & self.class.political_organisation_ids).any?
+  end
+end
