fd123b5d419d3432de19efadeca889719f51f321
Jon Hallam <jonathan.hallam@digital.cabinet-office.gov.uk>
2019-11-25 17:49:58 +0000

    Add content-publisher to config.hosts
    This is in order to make content publisher work with govuk-docker. I also had to make changes to rubocop as this puts us over the block length limit

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -12,6 +12,8 @@ Rails.application.configure do
   # Show full error reports.
   config.consider_all_requests_local = true
 
+  config.hosts << "content-publisher.dev.gov.uk"
+
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
   if Rails.root.join("tmp", "caching-dev.txt").exist?
