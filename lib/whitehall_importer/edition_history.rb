a7e5ccdaba8216035f918b90e791b6581dec4c3f
Peter Hartshorn <peter.hartshorn@digital.cabinet-office.gov.uk>
2019-11-26 15:17:38 +0000

    Add unpublishing methods to EditionHistory
    This commit adds three methods, last_unpublishing_event, next_event and
    edited_after_unpublishing? to EditionHistory. These will be needed to
    split up an edited unpublished edition into a removed edition and a
    draft / submitted edition.

--- lib/whitehall_importer/edition_history.rb
+++ lib/whitehall_importer/edition_history.rb
@@ -22,7 +22,36 @@ module WhitehallImporter
       event
     end
 
+    def last_unpublishing_event
+      # we can determine the event that marks an unpublishing by a draft event following a published event
+      unpublishing_events = revision_history.select.with_index do |item, index|
+        next unless item["state"] == "draft"
+
+        previous_entry = revision_history[index - 1]
+        previous_entry && previous_entry["state"] == "published"
+      end
+
+      raise AbortImportError, "Edition is missing unpublishing event" if unpublishing_events.blank?
+
+      unpublishing_events.last
+    end
+
+    def next_event(event)
+      next_event_index = revision_history.index(event) + 1
+
+      raise AbortImportError, "Edition is missing next event for #{event}" unless revision_history[next_event_index]
+
+      revision_history[next_event_index]
+    end
+
+    def edited_after_unpublishing?
+      return false unless revision_history.second_to_last
+
+      revision_history.second_to_last["state"] != "published"
+    end
+
   private
+
     attr_reader :revision_history
   end
 end
--- spec/lib/whitehall_importer/edition_history_spec.rb
+++ spec/lib/whitehall_importer/edition_history_spec.rb
@@ -3,14 +3,14 @@
 RSpec.describe WhitehallImporter::EditionHistory do
   describe "#state_event" do
     it "returns the event associated with the state" do
-      revision_history = build(:whitehall_export_edition)["revision_history"]
+      revision_history = [build(:revision_history_event)]
       event = described_class.new(revision_history).state_event("draft")
 
       expect(event).to eq(revision_history.first)
     end
 
     it "aborts if the edition is missing a state event" do
-      revision_history = build(:whitehall_export_edition)["revision_history"]
+      revision_history = [build(:revision_history_event)]
 
       expect { described_class.new(revision_history).state_event("published") }
         .to raise_error(WhitehallImporter::AbortImportError)
@@ -19,17 +19,86 @@ RSpec.describe WhitehallImporter::EditionHistory do
 
   describe "#create_event" do
     it "returns the create event of the edition" do
-      revision_history = build(:whitehall_export_edition)["revision_history"]
+      revision_history = [build(:revision_history_event)]
       event = described_class.new(revision_history).create_event
 
       expect(event).to eq(revision_history.first)
     end
 
     it "aborts if the edition is missing the create event" do
-      revision_history = build(:whitehall_export_edition, revision_history: [])["revision_history"]
+      revision_history = []
 
       expect { described_class.new(revision_history).create_event }
         .to raise_error(WhitehallImporter::AbortImportError)
+    end
+  end
+
+  describe "#last_unpublishing_event" do
+    it "returns the last publishing event of the edition" do
+      revision_history = [
+        build(:revision_history_event),
+        build(:revision_history_event, event: "update", state: "published"),
+        build(:revision_history_event, event: "update", state: "draft"),
+      ]
+      event = described_class.new(revision_history).last_unpublishing_event
+
+      expect(event).to eq(revision_history.last)
+    end
+
+    it "aborts if the edition is missing the unpublishing event" do
+      revision_history = []
+
+      expect { described_class.new(revision_history).last_unpublishing_event }
+        .to raise_error(WhitehallImporter::AbortImportError)
+    end
+  end
+
+  describe "#next_event" do
+    it "returns the next event" do
+      revision_history = [
+        build(:revision_history_event),
+        build(:revision_history_event, event: "update", state: "published"),
+      ]
+      event = described_class.new(revision_history).next_event(revision_history.first)
+
+      expect(event).to eq(revision_history.last)
+    end
+
+    it "aborts if the edition is missing a next event" do
+      revision_history = [build(:revision_history_event)]
+
+      expect { described_class.new(revision_history).next_event(revision_history.first) }
+        .to raise_error(WhitehallImporter::AbortImportError)
+    end
+  end
+
+  describe "#edited_after_unpublishing?" do
+    it "returns true if second to last event isn't published" do
+      revision_history = [
+        build(:revision_history_event),
+        build(:revision_history_event),
+      ]
+      edited_boolean = described_class.new(revision_history).edited_after_unpublishing?
+
+      expect(edited_boolean).to be_truthy
+    end
+
+    it "returns false if second to last event doesn't exist" do
+      revision_history = [build(:revision_history_event)]
+      edited_boolean = described_class.new(revision_history).edited_after_unpublishing?
+
+      expect(edited_boolean).to be_falsey
+    end
+
+    it "returns false if second to last event is published" do
+      revision_history = [
+        build(:revision_history_event),
+        build(:revision_history_event, event: "update", state: "published"),
+        build(:revision_history_event, event: "update", state: "draft"),
+      ]
+      edited_boolean = described_class.new(revision_history).edited_after_unpublishing?
+
+      expect(edited_boolean).to be_falsey
     end
   end
 end
