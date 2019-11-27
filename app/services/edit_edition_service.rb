38b33d20a4b0fcd29096ff444663dae072d62c92
Kevin Dew <kevindew@me.com>
2019-11-27 21:29:25 +0000

    Set political and government status when editing an edition
    Each time we edit an edition we should update political and government
    status in case one of the edits change this value. It seems simpler to
    hook this in everywhere rather than having to selectively pick which
    interactors to add it to.

--- app/services/edit_edition_service.rb
+++ app/services/edit_edition_service.rb
@@ -13,9 +13,26 @@ class EditEditionService < ApplicationService
     edition.assign_attributes(
       attributes.merge(last_edited_by: user, last_edited_at: Time.current),
     )
+
+    determine_political
+    associate_with_government
   end
 
 private
 
   attr_reader :edition, :user, :attributes
+
+  def determine_political
+    identifier = PoliticalEditionIdentifier.new(edition)
+    edition.system_political = identifier.political?
+  end
+
+  def associate_with_government
+    date = edition.backdated_to || edition.document.first_published_at
+
+    edition.government_id = if date
+                              government = Government.for_date(date)
+                              government&.content_id
+                            end
+  end
 end
--- spec/services/edit_edition_service_spec.rb
+++ spec/services/edit_edition_service_spec.rb
@@ -36,5 +36,76 @@ RSpec.describe EditEditionService do
       expect { EditEditionService.call(live_edition, user) }
         .to raise_error("cannot edit a live edition")
     end
+
+    describe "marks an edition as political" do
+      it "sets system_political to true when the edition is identified as political" do
+        allow(PoliticalEditionIdentifier)
+          .to receive(:new)
+          .with(edition)
+          .and_return(instance_double(PoliticalEditionIdentifier, political?: true))
+
+        expect { EditEditionService.call(edition, user) }
+          .to change { edition.system_political }
+          .to(true)
+      end
+
+      it "sets system_political to false when the edition is not identified as political" do
+        edition = build(:edition, system_political: true)
+
+        allow(PoliticalEditionIdentifier)
+          .to receive(:new)
+          .with(edition)
+          .and_return(instance_double(PoliticalEditionIdentifier, political?: false))
+
+        expect { EditEditionService.call(edition, user) }
+          .to change { edition.system_political }
+          .to(false)
+      end
+    end
+
+    describe "associates the edition with a government" do
+      it "sets the government for a backdated edition" do
+        backdate = Time.zone.parse("2018-01-01")
+        government = build(:government)
+        edition = create(:edition, backdated_to: backdate)
+        allow(Government).to receive(:for_date).with(edition.backdated_to)
+                         .and_return(government)
+
+        expect { EditEditionService.call(edition, user) }
+          .to change { edition.government_id }
+          .to(government.content_id)
+      end
+
+      it "sets the government for an edition of a published document" do
+        publish_date = Time.zone.parse("2019-11-01")
+        government = build(:government)
+        allow(Government).to receive(:for_date).with(publish_date)
+                         .and_return(government)
+        edition = build(:edition, first_published_at: publish_date)
+
+        expect { EditEditionService.call(edition, user) }
+          .to change { edition.government_id }
+          .to(government.content_id)
+      end
+
+      it "sets the government to nil when there isn't a government for the date" do
+        publish_date = Time.zone.parse("2019-11-01")
+        allow(Government).to receive(:for_date).with(publish_date)
+                         .and_return(nil)
+        edition = build(:edition, first_published_at: publish_date)
+
+        expect { EditEditionService.call(edition, user) }
+          .not_to change { edition.government_id }
+          .from(nil)
+      end
+
+      it "sets the government to nil when an edition isn't backdated or the document published" do
+        edition = build(:edition)
+
+        expect { EditEditionService.call(edition, user) }
+          .not_to change { edition.government_id }
+          .from(nil)
+      end
+    end
   end
 end
