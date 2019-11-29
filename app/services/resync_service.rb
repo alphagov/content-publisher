101c8c00eabbf8828c6603e0dc222a74966fa861
ChrisBAshton <christopher.ashton@digital.cabinet-office.gov.uk>
2019-11-29 17:13:26 +0000

    Updates the government_id associated with the live edition

--- app/services/resync_service.rb
+++ app/services/resync_service.rb
@@ -12,6 +12,16 @@ class ResyncService < ApplicationService
     edition.update!(
       revision_synced: false,
       system_political: PoliticalEditionIdentifier.new(edition).political?,
+      government_id: government_id(edition, document),
     )
+  end
+
+private
+
+  def government_id(edition, document)
+    date = edition.backdated_to || document.first_published_at
+    return unless date
+
+    Government.for_date(date)&.content_id
   end
 end
--- spec/services/resync_service_spec.rb
+++ spec/services/resync_service_spec.rb
@@ -2,13 +2,22 @@
 
 RSpec.describe ResyncService do
   describe ".call" do
+    before do
+      PoliticalEditionIdentifier.stub_chain(:new, :political?).and_return(true)
+    end
+
     it "updates the system_political value associated with the current edition" do
       document = create(:document, :with_current_edition)
-      PoliticalEditionIdentifier.stub_chain(:new, :political?).and_return(true)
-
       expect(document.current_edition.system_political).to be false
       ResyncService.call(document)
       expect(document.current_edition.system_political).to be true
+    end
+
+    it "updates the government_id associated with the live edition" do
+      document = create(:document, :with_live_edition)
+      expect(document.live_edition.government_id).to be nil
+      ResyncService.call(document)
+      expect(document.live_edition.government_id).to eq "d4fbc1b9-d47d-4386-af04-ac909f868f92"
     end
   end
 end
