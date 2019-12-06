d5622d9e17c30c61794c83a03e0c893ac49787e7
Leena Gupte <leena.gupte@digital.cabinet-office.gov.uk>
2019-12-06 10:16:03 +0000

    Add rake tasks to call the ResyncService

---
+++ spec/lib/tasks/resync_spec.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+RSpec.describe "Resync tasks" do
+  describe "resync:document" do
+    it "resyncs a document" do
+      document = create(:document)
+
+      expect(ResyncService)
+        .to receive(:call)
+        .once
+        .with(document)
+
+      Rake::Task["resync:document"].invoke("#{document.content_id}:#{document.locale}")
+    end
+  end
+
+  describe "resync:all" do
+    it "resyncs all documentss" do
+      create(:document)
+      create(:document)
+
+      expect(ResyncService)
+        .to receive(:call)
+        .twice
+
+      Rake::Task["resync:all"].invoke
+    end
+  end
+end
