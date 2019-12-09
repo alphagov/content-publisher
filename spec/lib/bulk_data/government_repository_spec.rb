77a50c2933330b218a3cea197e883db75344d60c
Kevin Dew <kevindew@me.com>
2019-12-09 17:46:51 +0000

    GovernmentRepository class for BulkData
    The purpose of this class is to mediate communication between Publishing
    API and our cache adapter for governments.
    
    It writes entries to the cache using a cache key that is defined as a
    constant in this class. This is named as "government-v1" to indicate
    that this is version 1 of the data we are caching. If we are to make any
    backwards incompatible changes to the data we expect (such as adding an
    extra mandatory field) then we will need to increment this number. This
    will ensure that when the code change is deployed the old cache will not
    be used anymore and will expire naturally.
    
    It provides a number of methods to finding a particular government and
    a method to populate the cache.

---
+++ spec/lib/bulk_data/government_repository_spec.rb
@@ -0,0 +1,157 @@
+# frozen_string_literal: true
+
+RSpec.describe BulkData::GovernmentRepository do
+  let(:repository) { BulkData::GovernmentRepository.new }
+  let(:cache_key) { BulkData::GovernmentRepository::CACHE_KEY }
+
+  describe "#find" do
+    let(:government) { build(:government) }
+    before { BulkData::Cache.write(cache_key, [government.to_h]) }
+
+    it "can find a government for a particular content id" do
+      expect(repository.find(government.content_id)).to eq(government)
+    end
+
+    it "raises an error when the government isn't known" do
+      content_id = SecureRandom.uuid
+      expect { repository.find(content_id) }
+        .to raise_error("Government #{content_id} not found")
+    end
+  end
+
+  describe "#for_date" do
+    let(:government) do
+      build(:government, started_on: 3.weeks.ago, ended_on: 1.week.ago)
+    end
+
+    before { BulkData::Cache.write(cache_key, [government.to_h]) }
+
+    it "returns the government for that date" do
+      expect(repository.for_date(2.weeks.ago)).to eq(government)
+    end
+
+    it "returns nil if there isn't a government covering the date" do
+      expect(repository.for_date(4.weeks.ago)).to be_nil
+    end
+
+    it "returns nil if there isn't a date" do
+      expect(repository.for_date(nil)).to be_nil
+    end
+  end
+
+  describe "#current" do
+    it "returns the current government when there is one" do
+      government = build(:government, ended_on: nil)
+      BulkData::Cache.write(cache_key, [government.to_h])
+      expect(repository.current).to eq(government)
+    end
+
+    it "returns nil when there isn't a current government" do
+      government = build(:government, :past)
+      BulkData::Cache.write(cache_key, [government.to_h])
+      expect(repository.current).to be_nil
+    end
+  end
+
+  describe "#past" do
+    it "returns all the governments that aren't current" do
+      government_a = build(:government,
+                           started_on: Date.parse("2018-11-11"),
+                           ended_on: nil)
+      government_b = build(:government,
+                           started_on: Date.parse("2015-11-11"),
+                           ended_on: Date.parse("2018-11-11"))
+      government_c = build(:government,
+                           started_on: Date.parse("2012-11-11"),
+                           ended_on: Date.parse("2015-11-11"))
+
+      BulkData::Cache.write(
+        cache_key,
+        [government_a.to_h, government_b.to_h, government_c.to_h],
+      )
+
+      expect(repository.past).to eq([government_c, government_b])
+    end
+
+    it "returns empty if there are no past govenments" do
+      government = build(:government, ended_on: nil)
+      BulkData::Cache.write(cache_key, [government.to_h])
+      expect(repository.past).to be_empty
+    end
+  end
+
+  describe "#all" do
+    it "returns an array of governments sorted by started on date" do
+      government_a = build(:government,
+                           started_on: Date.parse("2018-11-11"),
+                           ended_on: Date.parse("2019-11-11"))
+      government_b = build(:government,
+                           started_on: Date.parse("2012-11-11"),
+                           ended_on: Date.parse("2015-11-11"))
+      government_c = build(:government,
+                           started_on: Date.parse("2015-11-11"),
+                           ended_on: Date.parse("2018-11-11"))
+      BulkData::Cache.write(
+        cache_key,
+        [government_a.to_h, government_b.to_h, government_c.to_h],
+      )
+
+      expect(repository.all).to eq([government_b, government_c, government_a])
+    end
+
+    it "raises a LocalDataUnavailableError when there isn't data" do
+      expect { repository.all }
+        .to raise_error(BulkData::LocalDataUnavailableError)
+    end
+  end
+
+  describe "#populate_cache" do
+    let(:governments) do
+      [build(:government).to_h, build(:government, :past).to_h]
+    end
+
+    let!(:get_editions_request) do
+      stub_publishing_api_get_editions(
+        governments,
+        document_types: %w[government],
+        fields: %w[content_id locale title details],
+        states: %w[published],
+        locale: "en",
+        per_page: 1000,
+      )
+    end
+
+    it "populates the cache with governments from the Publishing API" do
+      expect(BulkData::Cache).to receive(:write).with(cache_key, governments)
+      repository.populate_cache
+      expect(get_editions_request).to have_been_requested
+    end
+
+    it "raises a RemoteDataUnavailableError when Publishing API is unavailable" do
+      stub_publishing_api_isnt_available
+
+      expect { repository.populate_cache }
+        .to raise_error(BulkData::RemoteDataUnavailableError)
+    end
+
+    it "resets the all data stored on the class" do
+      BulkData::Cache.write(cache_key, [])
+      expect { repository.populate_cache }
+        .to change { repository.all.count }.from(0).to(2)
+    end
+
+    it "re-populates the cache when it was updated before the older_than time" do
+      travel_to(10.minutes.ago) { repository.populate_cache }
+
+      expect(BulkData::Cache).to receive(:write)
+      repository.populate_cache(older_than: 5.minutes.ago)
+    end
+
+    it "does nothing when cache was updated after the older_than time" do
+      travel_to(2.minutes.ago) { repository.populate_cache }
+
+      expect(BulkData::Cache).not_to receive(:write)
+      repository.populate_cache(older_than: 5.minutes.ago)
+    end
+  end
+end
