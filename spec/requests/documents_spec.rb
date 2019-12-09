3bac6910ad9189e6740eb106e85a9041dd9e7557
Kevin Dew <kevindew@me.com>
2019-12-09 21:37:20 +0000

    Minor refactoring of document request spec
    This removes unnecessary instance variables and reduces long lines, it
    also removes an unnecessary test expectation.

--- spec/requests/documents_spec.rb
+++ spec/requests/documents_spec.rb
@@ -5,25 +5,32 @@ RSpec.describe "Documents" do
     describe "history mode banner" do
       context "when in history mode" do
         it "shows the history mode banner" do
-          @edition = create(:edition, :political, :past_government)
-          get document_path(@edition.document)
-          expect(response.body).to include(I18n.t!("documents.show.historical.title", document_type: @edition.document_type.label.downcase))
-          expect(response.body).to include(I18n.t!("documents.show.historical.description", government_name: @edition.government.name))
+          edition = create(:edition, :political, :past_government)
+          get document_path(edition.document)
+          title = I18n.t!("documents.show.historical.title",
+                          document_type: edition.document_type.label.downcase)
+          description = I18n.t!("documents.show.historical.description",
+                                government_name: edition.government.name)
+          expect(response.body).to include(title)
+          expect(response.body).to include(description)
         end
 
         it "won't show the history mode banner when the edition was created under the current government" do
-          @edition = create(:edition, :political, :current_government)
-          get document_path(@edition.document)
-          expect(response.body).not_to include(I18n.t!("documents.show.historical.title", document_type: @edition.document_type.label.downcase))
-          expect(response.body).not_to include(I18n.t!("documents.show.historical.description", government_name: @edition.government.name))
+          edition = create(:edition, :political, :current_government)
+          get document_path(edition.document)
+          title = I18n.t!("documents.show.historical.title",
+                          document_type: edition.document_type.label.downcase)
+          expect(response.body).not_to include(title)
         end
       end
 
       context "when not in history_mode" do
         it "won't show the history mode banner" do
-          @edition = create(:edition, :not_political)
-          get document_path(@edition.document)
-          expect(response.body).not_to include(I18n.t!("documents.show.historical.title", document_type: @edition.document_type.label.downcase))
+          edition = create(:edition, :not_political)
+          get document_path(edition.document)
+          title = I18n.t!("documents.show.historical.title",
+                          document_type: edition.document_type.label.downcase)
+          expect(response.body).not_to include(title)
         end
       end
     end
