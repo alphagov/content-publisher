9acd129eb4e8fd0acaedea09fe1aac7a85cfe859
Leena Gupte <leena.gupte@digital.cabinet-office.gov.uk>
2019-12-10 09:16:51 +0000

    New class to clear linkset links after whitehall import
    Whitehall uses a mixture of [link-set links](https://github.com/alphagov/publishing-api/blob/master/doc/link-expansion.md#patch-link-set---link-set-links) and [edition links](https://github.com/alphagov/publishing-api/blob/master/doc/link-expansion.md#put-content---edition-links) depending on at what point the content was migrated.
    Content Publisher however uses edition links near exclusively. As part of migration we need to reset links that shouldn't be set as linkset links based on the document type.
    
    Based on [digging in Whitehall](https://github.com/alphagov/whitehall/blob/master/app/presenters/publishing_api/news_article_presenter.rb#L35-L43) and the [schema](https://github.com/alphagov/govuk-content-schemas/blob/master/formats/news_article.jsonnet#L44-L50)
    this should be the following fields for news/press releases:
    
    - related_policies
    - ministers
    - topical_events
    - world_locations
    - worldwide_organisations
    - roles
    - people
    - primary_publishing_organisation
    - organisations

---
+++ lib/whitehall_importer/clear_linkset_links.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+module WhitehallImporter
+  class ClearLinksetLinks
+    attr_reader :content_id
+
+    def self.call(*args)
+      new(*args).call
+    end
+
+    def initialize(content_id)
+      @content_id = content_id
+    end
+
+    def call
+      GdsApi.publishing_api_v2.patch_links(
+        content_id,
+        links: {
+          related_policies: [],
+          ministers: [],
+          topical_events: [],
+          world_locations: [],
+          worldwide_organisations: [],
+          roles: [],
+          people: [],
+          primary_publishing_organisation: [],
+          organisations: [],
+          government: [],
+        },
+      )
+    end
+  end
+end
