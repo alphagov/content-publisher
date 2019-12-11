e477cad152be44fb52360f32352ef123500470b3
Bruce Bolt <bruce.bolt@digital.cabinet-office.gov.uk>
2019-12-11 09:12:55 +0000

    Add full feature test for history mode
    Updates the existing test for overriding political status to test for
    backdating and displaying the history mode banner.
    
    Co-authored-by: Jon Hallam <jonathan.hallam@digital.cabinet-office.gov.uk>

--- spec/features/editing_content_settings/political_spec.rb
+++ spec/features/editing_content_settings/political_spec.rb
@@ -6,10 +6,15 @@ RSpec.feature "History mode" do
     and_i_am_a_managing_editor
     when_i_visit_the_summary_page
     then_i_see_that_the_content_is_not_political
+    and_i_do_not_see_the_history_mode_banner
     when_i_click_to_change_the_status
     then_i_enable_political_status
     and_i_see_that_the_content_is_political
     and_i_see_the_timeline_entry
+    and_i_do_not_see_the_history_mode_banner
+    when_i_click_to_backdate_the_content
+    and_i_enter_a_date_to_backdate_the_content_to
+    and_i_see_the_history_mode_banner
   end
 
   def given_there_is_a_not_political_document
@@ -56,5 +61,29 @@ RSpec.feature "History mode" do
     within("#document-history") do
       expect(page).to have_content(I18n.t!("documents.history.entry_types.political_status_changed"))
     end
+  end
+
+  def when_i_click_to_backdate_the_content
+    click_on "Change Backdate"
+  end
+
+  def and_i_enter_a_date_to_backdate_the_content_to
+    @request = stub_publishing_api_put_content(@edition.content_id, {})
+    fill_in "backdate[date][day]", with: "1"
+    fill_in "backdate[date][month]", with: "1"
+    fill_in "backdate[date][year]", with: "1999"
+    click_on "Save"
+  end
+
+  def and_i_do_not_see_the_history_mode_banner
+    expect(page).not_to have_content(
+      I18n.t!("documents.show.historical.title", document_type: @edition.document_type.label.downcase),
+    )
+  end
+
+  def and_i_see_the_history_mode_banner
+    expect(page).to have_content(
+      I18n.t!("documents.show.historical.title", document_type: @edition.document_type.label.downcase),
+    )
   end
 end
