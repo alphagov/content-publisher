bc8240eec7e8dab8b4f885c9b760b6f02570532a
Kevin Dew <kevindew@me.com>
2019-12-12 13:22:05 +0000

    Move history mode request specs to own file
    These should reflect the controller layout rather than URL structure.

--- spec/requests/documents_spec.rb
+++ spec/requests/documents_spec.rb
@@ -28,38 +28,4 @@ RSpec.describe "Documents" do
       end
     end
   end
-
-  describe "GET /documents/:document/history-mode" do
-    it "returns a forbidden status for user without managing editor permission" do
-      @edition = create(:edition)
-      login_as(create(:user))
-      get history_mode_path(@edition.document)
-      expect(response.status).to eq(403)
-      expect(response.body).to include(I18n.t!("history_mode.non_managing_editor.title", title: @edition.title))
-    end
-
-    it "does not return a forbidden status for user with managing editor permission" do
-      @edition = create(:edition)
-      login_as(create(:user, managing_editor: true))
-      get history_mode_path(@edition.document)
-      expect(response.status).to_not eq(403)
-    end
-  end
-
-  describe "POST /documents/:document/history-mode" do
-    it "returns a forbidden status for user without managing editor permission" do
-      @edition = create(:edition)
-      login_as(create(:user))
-      post history_mode_path(@edition.document)
-      expect(response.status).to eq(403)
-      expect(response.body).to include(I18n.t!("history_mode.non_managing_editor.title", title: @edition.title))
-    end
-
-    it "does not return a forbidden status for user with managing editor permission" do
-      @edition = create(:edition)
-      login_as(create(:user, managing_editor: true))
-      post history_mode_path(@edition.document)
-      expect(response.status).to_not eq(403)
-    end
-  end
 end
