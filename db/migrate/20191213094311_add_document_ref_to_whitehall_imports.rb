2a327c8811c60e8093051dc93bb3465f3127936e
Peter Hartshorn <peter.hartshorn@digital.cabinet-office.gov.uk>
2019-12-18 12:04:03 +0000

    Associate document with WhitehallImport
    This commits associates a document with a WhitehallImport, this is
    needed to simplify code in future pieces of work.
    
    Most code here has been taken from this PR:
    https://github.com/alphagov/content-publisher/pull/1530

---
+++ db/migrate/20191213094311_add_document_ref_to_whitehall_imports.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+class AddDocumentRefToWhitehallImports < ActiveRecord::Migration[6.0]
+  def change
+    add_reference :whitehall_imports,
+                  :document,
+                  foreign_key: { on_delete: :restrict },
+                  null: true
+  end
+end
