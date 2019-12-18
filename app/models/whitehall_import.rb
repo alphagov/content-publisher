b190f9a93f1aed7158324e6062efb93ef9df2af0
Peter Hartshorn <peter.hartshorn@digital.cabinet-office.gov.uk>
2019-12-18 12:04:03 +0000

    Associate document with WhitehallImport
    This commits associates a document with a WhitehallImport, this is
    needed to simplify code in future pieces of work.
    
    Most code here has been taken from this PR:
    https://github.com/alphagov/content-publisher/pull/1530

--- app/models/whitehall_import.rb
+++ app/models/whitehall_import.rb
@@ -3,6 +3,8 @@
 # Represents the raw import of a document from Whitehall Publisher and
 # the import status of the document into Content Publisher
 class WhitehallImport < ApplicationRecord
+  belongs_to :document, optional: true
+
   enum state: { importing: "importing",
                 completed: "completed",
                 failed: "failed" }
