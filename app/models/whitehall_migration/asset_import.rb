9107b76150db50e43756c661b6eaace3c83e8b7e
ChrisBAshton <christopher.ashton@digital.cabinet-office.gov.uk>
2019-12-23 14:04:22 +0000

    Create WhitehallMigration::AssetImport factory
    This factory is about to be used by a number of subsequent
    commits. For ease of rebasing/editing, I've encapsulated it in
    one commit.
    
    For the record, I attempted to name the factories `:image` and
    `:file_attachment`, but as these names clashed with existing
    factory names, it was trying to set `image`/`file_attachment`
    attributes instead. I've gone into more detail here:
    https://github.com/thoughtbot/factory_bot/issues/528#issuecomment-569951414

--- app/models/whitehall_migration/asset_import.rb
+++ app/models/whitehall_migration/asset_import.rb
@@ -3,6 +3,10 @@
 # Represents the raw import of an asset from Whitehall Publisher and
 # the import status of the asset into Content Publisher
 class WhitehallMigration::AssetImport < ApplicationRecord
+  belongs_to :document_import
+  belongs_to :image_revision, class_name: "Image::Revision", optional: true
+  belongs_to :file_attachment_revision, class_name: "FileAttachment::Revision", optional: true
+
   enum state: { redirected: "redirected",
                 removed: "removed",
                 migration_failed: "migration_failed",
---
+++ spec/factories/whitehall_migration_asset_import_factory.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+FactoryBot.define do
+  factory :whitehall_migration_asset_import, class: WhitehallMigration::AssetImport do
+    document_import { association :whitehall_migration_document_import }
+    state { "pending" }
+
+    for_image
+
+    trait :for_image do
+      file_attachment_revision { nil }
+      image_revision { build(:image_revision) }
+      original_asset_url { "https://asset-manager.gov.uk/blah/847150/foo.jpg" }
+    end
+
+    trait :for_file_attachment do
+      image_revision { nil }
+      file_attachment_revision { build(:file_attachment_revision) }
+      original_asset_url { "https://asset-manager.gov.uk/blah/847150/foo.pdf" }
+    end
+  end
+end
