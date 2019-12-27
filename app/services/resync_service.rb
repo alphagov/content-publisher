08d086ddfc3c33e3edef399146ea7d66a7768e64
Leena Gupte <leena.gupte@digital.cabinet-office.gov.uk>
2019-12-27 15:10:19 +0000

    Check that links are correct

--- app/services/resync_service.rb
+++ app/services/resync_service.rb
@@ -123,6 +123,7 @@ private
     end
 
     raise WhitehallImporter::AbortImportError, "Versions don't match" unless versions_match?(edition_in_publishing_api, proposed_edition)
+    raise WhitehallImporter::AbortImportError, "Links don't match" unless links_match?(content_id, proposed_edition)
   end
 
   def versions_match?(edition_in_publishing_api, proposed_edition)
@@ -133,11 +134,26 @@ private
       edition_in_publishing_api["public_updated_at"] == proposed_edition["public_updated_at"] &&
       edition_in_publishing_api["document_type"]["id"] == proposed_edition["document_type"] &&
       edition_in_publishing_api["schema_name"] == proposed_edition["schema_name"] &&
-      body_text_similar_enough?(edition_in_publishing_api["details"]["body"], proposed_edition["details"]["body"])
+      details_match?(edition_in_publishing_api["details"], proposed_edition["details"])
+  end
+
+  def details_match?(pub_api_details, proposed_details)
+    body_text_similar_enough?(pub_api_details["body"], proposed_details["body"]) &&
+    images_match?()
   end
 
   def body_text_similar_enough?(pub_api_body, proposed_body)
     # See https://www.rubydoc.info/gems/damerau-levenshtein/1.1.0#API_Description
     DamerauLevenshtein.distance(pub_api_body, proposed_body, 1, 100) < 100
+  end
+
+  def links_match?(content_id, proposed_edition)
+    links_in_publishing_api = GdsApi.publishing_api.get_links(content_id).to_h
+    proposed_links = proposed_edition["links"]
+
+    links_in_publishing_api["government"].sort == proposed_links["government"].sort &&
+      links_in_publishing_api["organisations"].sort == proposed_links["organisations"].sort &&
+      links_in_publishing_api["taxons"].sort == proposed_links["taxons"].sort &&
+      links_in_publishing_api["primary_publishing_organisation"] == proposed_links["primary_publishing_organisation"]
   end
 end
