04e7eca89a4b65e219b83bf3c6d582722a78b5c4
Leena Gupte <leena.gupte@digital.cabinet-office.gov.uk>
2019-12-30 14:08:35 +0000

    Use safe navigation operator
    This stops us trying to sort a nil list

--- app/services/publishing_api_comparison_service.rb
+++ app/services/publishing_api_comparison_service.rb
@@ -57,9 +57,9 @@ private
     links_in_publishing_api = GdsApi.publishing_api.get_links(edition.content_id).to_h
     proposed_links = proposed_edition["links"]
 
-    links_in_publishing_api["government"].sort == proposed_links["government"].sort &&
-      links_in_publishing_api["organisations"].sort == proposed_links["organisations"].sort &&
-      links_in_publishing_api["taxons"].sort == proposed_links["taxons"].sort &&
+    links_in_publishing_api["government"]&.sort == proposed_links["government"]&.sort &&
+      links_in_publishing_api["organisations"]&.sort == proposed_links["organisations"]&.sort &&
+      links_in_publishing_api["taxons"]&.sort == proposed_links["taxons"]&.sort &&
       links_in_publishing_api["primary_publishing_organisation"] == proposed_links["primary_publishing_organisation"]
   end
 end
