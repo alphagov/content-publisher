d6dcb366816feaac9a85546635e3d44565edf87d
Kevin Dew <kevindew@me.com>
2019-12-31 18:08:57 +0000

    Move format configuration tests to respective models
    The format directory is something of an unusual one in Content
    Publisher. It contains just configuration files which is rather strange
    for a Rails app. The tests directory for this is a bit stranger since it
    has a mix of feature tests and configuration tests.
    
    This moves these configuration tests out of the spec/formats directory
    and into the corresponding model directory.

--- spec/models/document_type_spec.rb
+++ spec/models/document_type_spec.rb
@@ -1,6 +1,32 @@
 # frozen_string_literal: true
 
 RSpec.describe DocumentType do
+  describe "all configured document types are valid" do
+    DocumentType.all.each do |document_type|
+      describe "Document type #{document_type.id}" do
+        it "has the required attributes" do
+          expect(document_type.id).to_not be_blank
+          expect(document_type.label).to_not be_blank
+        end
+
+        if document_type.managed_elsewhere
+          it "has the required attributes for managed_elsewhere" do
+            expect(document_type.managed_elsewhere.keys).to contain_exactly("hostname", "path")
+          end
+        else
+          it "has the required attributes for publishing_metadata" do
+            expect(document_type.publishing_metadata.rendering_app).to_not be_blank
+            expect(document_type.publishing_metadata.schema_name).to_not be_blank
+          end
+
+          it "has a valid document type" do
+            expect(document_type.id).to be_in(GovukSchemas::DocumentTypes.valid_document_types)
+          end
+        end
+      end
+    end
+  end
+
   describe ".find" do
     it "returns a DocumentType when it's a known document_type" do
       expect(DocumentType.find("press_release")).to be_a(DocumentType)
--- spec/models/supertype_spec.rb
+++ spec/models/supertype_spec.rb
@@ -1,6 +1,18 @@
 # frozen_string_literal: true
 
 RSpec.describe Supertype do
+  describe "all configured supertypes are valid" do
+    Supertype.all.each do |supertype|
+      describe "Supertype #{supertype.id}" do
+        it "has the required attributes for #{supertype.id}" do
+          expect(supertype.id).to_not be_blank
+          expect(supertype.label).to_not be_blank
+          expect(supertype.description).to_not be_blank
+        end
+      end
+    end
+  end
+
   describe ".find" do
     it "returns a Supertype when it's a known supertype" do
       expect(Supertype.find("news")).to be_a(Supertype)
