95a7836b47752ca33f698eceed6d8740657abd16
Leena Gupte <leena.gupte@digital.cabinet-office.gov.uk>
2019-12-31 12:50:39 +0000

    Check if descriptions match

--- spec/lib/whitehall_importer/integrity_checker_spec.rb
+++ spec/lib/whitehall_importer/integrity_checker_spec.rb
@@ -9,6 +9,7 @@ RSpec.describe WhitehallImporter::IntegrityChecker do
         content_id: edition.content_id,
         base_path: edition.base_path,
         title: edition.title,
+        description: edition.summary,
       )
 
       integrity_check = WhitehallImporter::IntegrityChecker.new(edition)
@@ -31,6 +32,13 @@ RSpec.describe WhitehallImporter::IntegrityChecker do
 
       integrity_check = WhitehallImporter::IntegrityChecker.new(edition)
       expect(integrity_check.problems).to include("title doesn't match")
+    end
+
+    it "returns a problem when the descriptions don't match" do
+      stub_publishing_api_has_item(content_id: edition.content_id, description: "description")
+
+      integrity_check = WhitehallImporter::IntegrityChecker.new(edition)
+      expect(integrity_check.problems).to include("description doesn't match")
     end
   end
 end
