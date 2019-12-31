b40190affd4edca867b0b4ab6c1e7c400838565e
Bruce Bolt <bruce.bolt@digital.cabinet-office.gov.uk>
2019-12-31 11:03:01 +0000

    Use EditionHistory class for getting create event
    There was duplicated code for identifying the create event for a
    document.  Therefore updating to use the EditionHistory class method
    instead.

--- lib/whitehall_importer/import.rb
+++ lib/whitehall_importer/import.rb
@@ -52,8 +52,7 @@ module WhitehallImporter
         raise AbortImportError, "Document with content_id #{content_id} already exists"
       end
 
-      event = whitehall_document["editions"].first["revision_history"].select { |h| h["event"] == "create" }.first
-      raise AbortImportError, "First edition is missing a create event" unless event
+      event = first_edition_history.create_event!
 
       Document.create!(
         content_id: content_id,
