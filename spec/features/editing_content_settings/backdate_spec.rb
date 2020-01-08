d2848f203c5322528e1d1530abd53037b28879a2
Aga Dufrat <agnieszka.dufrat@digital.cabinet-office.gov.uk>
2020-01-08 12:10:00 +0100

    Amend editing_content_settings specs for document history page
    In this commit:
    
    - Updated specs to conform with new document history page
    
    - spec/features/editing_content_settings/history_mode_spec.rb, removes
      alias methods as they aren't used and rename a method accordingly and
      make steps a bit more readable with spaces.

--- spec/features/editing_content_settings/backdate_spec.rb
+++ spec/features/editing_content_settings/backdate_spec.rb
@@ -10,10 +10,12 @@ RSpec.feature "Backdate content" do
     and_i_enter_a_date_to_backdate_the_content_to
     and_i_click_save
     then_i_see_the_content_has_been_backdated
+    and_i_see_the_backdate_timeline_entry
 
     when_i_click_to_backdate_the_content
     and_i_click_clear_backdate
     then_i_see_the_content_is_no_longer_backdated
+    and_i_see_the_cleared_backdate_timeline_entry
   end
 
   def given_there_is_a_document_with_a_first_edition
@@ -31,6 +33,7 @@ RSpec.feature "Backdate content" do
   end
 
   def when_i_click_to_backdate_the_content
+    click_on "Document summary"
     click_on "Change Backdate"
   end
 
@@ -47,6 +50,11 @@ RSpec.feature "Backdate content" do
 
   def then_i_see_the_content_has_been_backdated
     expect(page).to have_content("1 January 2019")
+    expect(page).to_not have_content(I18n.t!("documents.show.content_settings.backdate.no_backdate"))
+  end
+
+  def and_i_see_the_backdate_timeline_entry
+    click_on "Document history"
     expect(page).to have_content(I18n.t!("documents.history.entry_types.backdated",
                                          date: "01 January 2019"))
   end
@@ -59,6 +67,10 @@ RSpec.feature "Backdate content" do
     expect(page).to have_content(
       I18n.t!("documents.show.content_settings.backdate.no_backdate"),
     )
+  end
+
+  def and_i_see_the_cleared_backdate_timeline_entry
+    click_on "Document history"
     expect(page).to have_content(
       I18n.t!("documents.history.entry_types.backdate_cleared"),
     )
