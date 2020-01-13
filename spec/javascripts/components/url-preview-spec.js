a97d8b65061866cdcb0af498d1241d66cf592739
Kevin Dew <kevindew@me.com>
2020-01-13 22:47:45 +0000

    Enrich UrlPreview component tests with fetch-mock
    This is a bit of a re-working of these tests to test more scenarios
    because of the options provided by fetch-mock.
    
    The 'should only display the preview URL if input has value' test has
    been updated to test after the fetch-mock request completes - I think
    the previous iteration may have only tested the initial HTML state.
    
    It also adds tests for the two error conditions, which were previously
    harder to test.
    
    Finally, I've simplified the dom lookups by storing the urlPreview element
    to make the output more succinct.

--- spec/javascripts/components/url-preview-spec.js
+++ spec/javascripts/components/url-preview-spec.js
@@ -1,10 +1,10 @@
 /* eslint-env jasmine, jquery */
-/* global GOVUK */
+/* global GOVUK, fetchMock */
 
 describe('URL preview component', function () {
   'use strict'
 
-  var container
+  var container, urlPreview
 
   beforeEach(function () {
     container = document.createElement('div')
@@ -26,41 +26,67 @@ describe('URL preview component', function () {
 
     document.body.appendChild(container)
     var element = document.querySelector('[data-module="url-preview"]')
-    new GOVUK.Modules.UrlPreview().start($(element))
+    urlPreview = new GOVUK.Modules.UrlPreview()
+    urlPreview.start($(element))
   })
 
   afterEach(function () {
     document.body.removeChild(container)
   })
 
-  it('should only display the default message if input is empty', function () {
-    // Interact with input and leave empty
-    var input = document.querySelector('[data-url-preview="input"]')
-    input.dispatchEvent(new window.Event('blur'))
+  it('displays a default message when the input is empty', function () {
+    urlPreview.input.value = ''
+    urlPreview.input.dispatchEvent(new window.Event('blur'))
 
-    var defaultMessage = document.querySelector('.js-url-preview-default-message')
-    expect(defaultMessage).toBeVisible()
-
-    var previewURL = document.querySelector('.js-url-preview-url')
-    expect(previewURL).toBeHidden()
-
-    var errorMessage = document.querySelector('.js-url-preview-error-message')
-    expect(errorMessage).toBeHidden()
+    expect(urlPreview.defaultMessage).toBeVisible()
+    expect(urlPreview.urlPreview).toBeHidden()
+    expect(urlPreview.errorMessage).toBeHidden()
   })
 
-  it('should only display the preview URL if input has value', function () {
-    // Interact with input and set value
-    var input = document.querySelector('[data-url-preview="input"]')
-    input.value = 'My title'
-    input.dispatchEvent(new window.Event('blur'))
+  it('displays the preview URL when a user inputs a value and a slug is generated', function (done) {
+    fetchMock.get(/generate-path\?title=My\+title/, 'my-title')
 
-    var defaultMessage = document.querySelector('.js-url-preview-default-message')
-    expect(defaultMessage).toBeHidden()
+    urlPreview.input.value = 'My title'
+    urlPreview.input.dispatchEvent(new window.Event('blur'))
 
-    var previewURL = document.querySelector('.js-url-preview-url')
-    expect(previewURL).toBeVisible()
+    fetchMock.flush(true).then(function () {
+      expect(urlPreview.defaultMessage).toBeHidden()
+      expect(urlPreview.urlPreview).toBeVisible()
+      expect(urlPreview.urlPreview).toContainText('my-title')
+      expect(urlPreview.errorMessage).toBeHidden()
+      done()
+    })
+  })
 
-    var errorMessage = document.querySelector('.js-url-preview-error-message')
-    expect(errorMessage).toBeHidden()
+  it('displays an error when the request to generate a path fails', function (done) {
+    fetchMock.get(/generate-path\?title=My\+title/, 500)
+
+    urlPreview.input.value = 'My title'
+    urlPreview.input.dispatchEvent(new window.Event('blur'))
+
+    fetchMock.flush(true).then(function () {
+      expect(urlPreview.defaultMessage).toBeHidden()
+      expect(urlPreview.urlPreview).toBeHidden()
+      expect(urlPreview.errorMessage).toBeVisible()
+      done()
+    })
+  })
+
+  it('displays an error if the request takes longer than 5 seconds', function (done) {
+    jasmine.clock().withMock(function () {
+      fetchMock.get(/generate-path\?title=My\+title/, 'my-title', { delay: 10000 })
+
+      urlPreview.input.value = 'My title'
+      urlPreview.input.dispatchEvent(new window.Event('blur'))
+
+      jasmine.clock().tick(5001)
+
+      fetchMock.flush(true).then(function () {
+        expect(urlPreview.defaultMessage).toBeHidden()
+        expect(urlPreview.urlPreview).toBeHidden()
+        expect(urlPreview.errorMessage).toBeVisible()
+        done()
+      })
+    })
   })
 })
