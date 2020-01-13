2d94bcefe7ff26ba1024220168543d0886558f98
Kevin Dew <kevindew@me.com>
2020-01-13 23:30:26 +0000

    Add tests for contact and video embed modals
    This adds tests for these two classes that are used to run these modals.
    These tests are very similar which reflects the code in the tested
    classes is also very similar. After pondering for a while I came to the
    conclusion that if you were to add more modals like these you'd probably
    want to reduce the duplication in the underlying classes which may
    itself present the opportunities to reduce the test duplication.

---
+++ spec/javascripts/modules/video-embed-modal-spec.js
@@ -0,0 +1,82 @@
+/* eslint-env jasmine */
+/* global VideoEmbedModal, fetchMock, buildModalDialogue, removeModalDialogue */
+
+describe('VideoEmbedModal', function () {
+  'use strict'
+
+  var modal, openLink, videoEmbedModal
+
+  beforeEach(function () {
+    modal = buildModalDialogue()
+
+    openLink = document.createElement('a')
+    openLink.dataset.modalAction = 'open'
+    openLink.href = 'https://example.com/video-embed'
+
+    videoEmbedModal = new VideoEmbedModal(openLink)
+    videoEmbedModal.init()
+  })
+
+  afterEach(function () {
+    removeModalDialogue()
+  })
+
+  describe('open action', function () {
+    it('opens from a click and renders the linked resource in the modal dynamic section', function (done) {
+      fetchMock.get('https://example.com/video-embed', '<h1>Video response</h1>')
+      openLink.click()
+
+      fetchMock.flush(true).then(function () {
+        var dynamicSection = modal.querySelector('.js-dynamic-section')
+        expect(dynamicSection).toBeVisible()
+        expect(dynamicSection).toContainHtml('<h1>Video response</h1>')
+        done()
+      })
+    })
+  })
+
+  describe('insert action', function () {
+    beforeEach(function () {
+      modal.open()
+      var body = '<form action="https://example.com/video-embed" method="post" data-modal-action="insert">' +
+                   '<button>Submit</button>' +
+                 '</form>'
+      videoEmbedModal.workflow.renderSuccess({ body: body })
+    })
+
+    it('closes the modal and adds the response to the editor on success', function (done) {
+      fetchMock.post('https://example.com/video-embed', '[link](https://www.youtube.com/id)')
+      modal.querySelector('button').click()
+      spyOn(videoEmbedModal.editor, 'insertBlock')
+      fetchMock.flush(true).then(function () {
+        expect(modal).toBeHidden()
+        expect(videoEmbedModal.editor.insertBlock).toHaveBeenCalledWith('[link](https://www.youtube.com/id)')
+        done()
+      })
+    })
+
+    it('shows the response when there is a validation issue', function (done) {
+      fetchMock.post(
+        'https://example.com/video-embed',
+        { status: 422, body: '<h1>Validation issues</h1>' }
+      )
+      modal.querySelector('button').click()
+      fetchMock.flush(true).then(function () {
+        var dynamicSection = modal.querySelector('.js-dynamic-section')
+        expect(dynamicSection).toBeVisible()
+        expect(dynamicSection).toContainHtml('<h1>Validation issues</h1>')
+        done()
+      })
+    })
+
+    it('shows the modal error view on a different error response', function (done) {
+      fetchMock.post('https://example.com/video-embed', 500)
+      modal.querySelector('button').click()
+      fetchMock.flush(true).then(function () {
+        var errorSection = modal.querySelector('#error')
+        expect(errorSection).toBeVisible()
+        done()
+      })
+    })
+  })
+})
