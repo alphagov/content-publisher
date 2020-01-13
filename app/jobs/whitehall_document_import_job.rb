uk-gov-mirror/alphagov.content-publisher

Queue import of documents from Whitehall
This creates a new Sidekiq job that allows the import of a single
document from Whitehall to be queued when we import by organisation.
This behaviour is currently being performed by a rake task, which has
been retained to ensure we can import adhoc documents.  However, the
import_and_sync method has been updated to take a
WhitehallMigration::DocumentImport model, rather than the Whitehall
document ID.

Trello - https://trello.com/c/HxOpHnf9/1272-queue-a-job-to-import-document-from-whitehall

Co-authored-by: Bruce Bolt <bruce.bolt@digital.cabinet-office.gov.uk>

app/jobs/whitehall_document_import_job.rb
@@ -1,0 +1,7 @@
+# frozen_string_literal: true
+
+class WhitehallDocumentImportJob < ApplicationJob
+  def perform(whitehall_migration_document_import)
+    WhitehallImporter.import_and_sync(whitehall_migration_document_import)
+  end
+end

lib/tasks/import.rake
@@ -17,8 +17,11 @@ namespace :import do
 
   desc "Import a single document from Whitehall Publisher using Whitehall's internal document ID e.g. import:whitehall_document[123]"
   task :whitehall_document, [:document_id] => :environment do |_, args|
-    whitehall_export = Services.whitehall.document_export(args.document_id)
-    whitehall_import = WhitehallImporter.import_and_sync(whitehall_export)
+    whitehall_migration_document_import = WhitehallMigration::DocumentImport.create!(
+      whitehall_document_id: args.document_id,
+      state: "pending",
+    )
+    whitehall_import = WhitehallImporter.import_and_sync(whitehall_migration_document_import)
 
     unless whitehall_import.completed?
       puts whitehall_import.state.humanize

alphagov/content-publisher

app/jobs/whitehall_document_import_job.rb
@@ -1,0 +1,7 @@
+# frozen_string_literal: true
+
+class WhitehallDocumentImportJob < ApplicationJob
+  def perform(document_import)
+    WhitehallImporter.import_and_sync(document_import)
+  end
+end

lib/tasks/import.rake
@@ -17,8 +17,11 @@ namespace :import do
 
   desc "Import a single document from Whitehall Publisher using Whitehall's internal document ID e.g. import:whitehall_document[123]"
   task :whitehall_document, [:document_id] => :environment do |_, args|
-    whitehall_export = GdsApi.whitehall_export.document_export(args.document_id)
-    whitehall_import = WhitehallImporter.import_and_sync(whitehall_export.to_hash)
+    whitehall_import = WhitehallMigration::DocumentImport.create!(
+      whitehall_document_id: args.document_id,
+      state: "pending",
+    )
+    WhitehallImporter.import_and_sync(whitehall_import)
 
     unless whitehall_import.completed?
       puts whitehall_import.state.humanize