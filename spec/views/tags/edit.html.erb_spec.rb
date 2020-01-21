5ec33909a5e956269a76cc0ff10fd0e72e19e78b
Kevin Dew <kevindew@me.com>
2020-01-21 21:23:28 +0000

    Test tags edit warning in the view
    This was previously tested in a feature test and has now been moved into
    a view test to be consistent with the [test strategy][]
    
    [test strategy]: https://github.com/alphagov/content-publisher/blob/bd10dd6c488102b53ba2ce1fa85fa923d16bdd46/docs/testing-strategy.md

---
+++ spec/views/tags/edit.html.erb_spec.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+RSpec.describe "tags/edit.html.erb" do
+  it "shows a warning when editing the primary organisation tag of an access limited edition" do
+    tag_field = build(:tag_field,
+                      type: "single_tag",
+                      id: "primary_publishing_organisation",
+                      document_type: "organisation")
+    document_type = build(:document_type, tags: [tag_field])
+    edition = build(:edition,
+                    :access_limited,
+                    document_type_id: document_type.id,
+                    created_by: current_user)
+    stub_publishing_api_has_linkables(
+      [{ "content_id" => current_user.organisation_content_id,
+         "internal_name" => "Organisation" }],
+      document_type: tag_field.document_type,
+    )
+
+    assign(:edition, edition)
+    assign(:revision, edition.revision)
+    render
+
+    expect(rendered)
+      .to have_content(I18n.t!("tags.edit.organisation_warning"))
+  end
+end
