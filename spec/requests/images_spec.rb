d73fe751ea3e5f3a6883b8b517c51ec8f5a39569
Kevin Dew <kevindew@me.com>
2020-01-21 22:00:40 +0000

    Reduce code duplication testing 404s for image requests
    This uses the requests that return status shared example to reduce the
    code duplication needed to test the 404 handling in image revisions.

--- spec/requests/images_spec.rb
+++ spec/requests/images_spec.rb
@@ -17,6 +17,18 @@ RSpec.describe "Images" do
     let(:route_params) { [edition.document, "image_id"] }
   end
 
+  it_behaves_like "requests that return status",
+                  "when an image revision belongs to a different edition",
+                  status: :not_found,
+                  routes: { edit_image_path: %i[get patch],
+                            crop_image_path: %i[get patch],
+                            download_image_path: %i[get],
+                            destroy_image_path: %i[delete] } do
+    let(:edition) { create(:edition) }
+    let(:image_revision) { create(:image_revision) }
+    let(:route_params) { [edition.document, image_revision] }
+  end
+
   describe "POST /documents/:document/images" do
     let(:edition) { create(:edition) }
     before { stub_publishing_api_put_content(edition.content_id, {}) }
@@ -45,28 +57,21 @@ RSpec.describe "Images" do
   end
 
   describe "GET /documents/:document/images/:image_id/crop" do
-    let(:image_revision) { create(:image_revision) }
-
     it "returns successfully for an image belonging to the edition" do
+      image_revision = create(:image_revision)
       edition = create(:edition, image_revisions: [image_revision])
 
       get crop_image_path(edition.document, image_revision.image_id)
       expect(response).to have_http_status(:ok)
     end
-
-    it "returns a 404 when image isn't for the edition" do
-      edition = create(:edition)
-      get crop_image_path(edition.document, image_revision.image_id)
-      expect(response).to have_http_status(:not_found)
-    end
   end
 
   describe "PATCH /documents/:document/images/:image_id/crop" do
     let(:image_revision) { create(:image_revision) }
+    let(:edition) { create(:edition, image_revisions: [image_revision]) }
     let(:crop) { { crop_x: 0, crop_y: 0, crop_width: 960, crop_height: 640 } }
 
     it "redirects to edit image when in the upload wizard" do
-      edition = create(:edition, image_revisions: [image_revision])
       patch crop_image_path(edition.document, image_revision.image_id, wizard: "upload"),
             params: { image_revision: crop }
 
@@ -76,34 +81,20 @@ RSpec.describe "Images" do
     end
 
     it "redirects to the images path when outside the wizard" do
-      edition = create(:edition, image_revisions: [image_revision])
       patch crop_image_path(edition.document, image_revision.image_id),
             params: { image_revision: crop }
 
       expect(response).to redirect_to(images_path(edition.document))
     end
-
-    it "returns a 404 when image isn't for the edition" do
-      edition = create(:edition)
-      post crop_image_path(edition.document, image_revision.image_id)
-      expect(response).to have_http_status(:not_found)
-    end
   end
 
   describe "GET /documents/:document/images/:image_id/edit" do
-    let(:image_revision) { create(:image_revision) }
-
     it "returns successfully for an image belonging to the edition" do
+      image_revision = create(:image_revision)
       edition = create(:edition, image_revisions: [image_revision])
 
       get edit_image_path(edition.document, image_revision.image_id)
       expect(response).to have_http_status(:ok)
-    end
-
-    it "returns a 404 when image isn't for the edition" do
-      edition = create(:edition)
-      get edit_image_path(edition.document, image_revision.image_id)
-      expect(response).to have_http_status(:not_found)
     end
   end
 
@@ -156,12 +147,6 @@ RSpec.describe "Images" do
       expect(response.body)
         .to include(I18n.t!("requirements.alt_text.blank.form_message"))
     end
-
-    it "returns a 404 when image isn't for the edition" do
-      edition = create(:edition)
-      patch edit_image_path(edition.document, image_revision.image_id)
-      expect(response).to have_http_status(:not_found)
-    end
   end
 
   describe "DELETE /documents/:document/images/:image_id" do
@@ -189,28 +174,15 @@ RSpec.describe "Images" do
         I18n.t!("images.index.flashes.deleted", file: image_revision.filename),
       )
     end
-
-    it "returns a 404 when image isn't for the edition" do
-      edition = create(:edition)
-      delete destroy_image_path(edition.document, image_revision.image_id)
-      expect(response).to have_http_status(:not_found)
-    end
   end
 
   describe "GET /documents/:document/images/:image_id/download" do
-    let(:image_revision) { create(:image_revision) }
-
     it "provides an image download" do
+      image_revision = create(:image_revision)
       edition = create(:edition, image_revisions: [image_revision])
       get download_image_path(edition.document, image_revision.image_id)
       expect(response.headers["Content-Disposition"])
         .to match(/attachment; filename="#{image_revision.filename}";/)
-    end
-
-    it "returns a 404 when image isn't for the edition" do
-      edition = create(:edition)
-      get download_image_path(edition.document, image_revision.image_id)
-      expect(response).to have_http_status(:not_found)
     end
   end
 end
