8a77b368800da08923c80aec80058ae1a9b40fe2
Kevin Dew <kevindew@me.com>
2020-01-21 21:58:44 +0000

    Add request returns status shared example
    This adds a shared example that can be used in request test to test that
    multiple endpoints and methods return the same status code. It is
    intended to be used to reduce duplicate examples.
    
    To set the correct context for this example I've renamed
    AssertEditionStateExamples to be RequestExamples.

--- spec/support/request_examples.rb
+++ spec/support/request_examples.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-module AssertEditionStateExamples
+module RequestExamples
   # an example calling this is expected to provided text of the scenario name
   # and a hash of path helpers with the methods to be checked.
   # Within the example we expect a let of an edition to be defined, which
@@ -23,6 +23,33 @@ module AssertEditionStateExamples
             process(method.to_sym, public_send(path.to_sym, *route_params))
 
             expect(response).to redirect_to(document_path(edition.document))
+          end
+        end
+      end
+    end
+  end
+
+  # an example calling this is expected to provided text of the scenario name,
+  # the status code expected and a hash of path helpers with the methods to be
+  # checked.
+  #
+  # For example:
+  #   it_behaves_like "requests that return status",
+  #                   "scenario we're testing",
+  #                   status: :not_found,
+  #                   routes: { our_path: %i[get post delete] } do
+  #     let(:edition) { create(:edition, :certain_state) }
+  #     let(:route_params) { [edition.document] }
+  #   end
+  #
+  shared_examples "requests that return status" do |scenario, status:, routes:|
+    describe scenario do
+      routes.each do |path, methods|
+        methods.each do |method|
+          it "returns a #{status} for #{method} #{path}" do
+            process(method.to_sym, public_send(path.to_sym, *route_params))
+
+            expect(response).to have_http_status(status)
           end
         end
       end
