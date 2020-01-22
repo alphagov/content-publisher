99d48d035f56fb9118307f08515b35976e3c23ba
Rosa Fox <rosa.fox@digital.cabinet-office.gov.uk>
2020-01-22 11:12:22 +0000

    Add entry_body to timeline_entry_whitehall_imported_entries
    - Required for upcoming implementation of: https://trello.com/c/o8PhjdUc/1312-map-whitehall-fact-check-requests-fact-checking-tab-in-whitehall-to-content-publisher-timeline-entries-on-import and https://trello.com/c/tNsWXZZj/1314-map-whitehall-editorial-remarks-notes-tab-in-whitehall-to-content-publisher-timeline-entries-on-import
    
    - Will allow us to store the notes body content as well as the fact check data
    
    Co-authored-by: Graham Lewis <graham.lewis@digital.cabinet-office.gov.uk>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_14_163932) do
+ActiveRecord::Schema.define(version: 2020_01_22_110833) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -324,6 +324,7 @@ ActiveRecord::Schema.define(version: 2020_01_14_163932) do
   create_table "timeline_entry_whitehall_imported_entries", force: :cascade do |t|
     t.string "entry_type", null: false
     t.datetime "created_at"
+    t.json "body", default: "{}", null: false
   end
 
   create_table "users", force: :cascade do |t|
