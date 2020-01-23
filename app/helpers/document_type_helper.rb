67c1d5fdcd46e47217af611c9fee9146fd2a21e4
ChrisBAshton <christopher.ashton@digital.cabinet-office.gov.uk>
2020-01-23 11:26:45 +0000

    Refactor DocumentTypeHelper to be more concise
    We now only use DocumentTypeHelper to retrieve explicit/default
    values for fields, and not for anything higher up in the i18n
    tree.
    We can therefore simplify its API to take an edition (which can
    be used to determine the document type ID) and a short substring
    of i18n key which we will prefix the document type ID to.

--- app/helpers/document_type_helper.rb
+++ app/helpers/document_type_helper.rb
@@ -1,15 +1,21 @@
 # frozen_string_literal: true
 
 module DocumentTypeHelper
-  def t_doctype_exists?(i18n_key)
+  def t_doctype_field?(edition, partial_i18n_key)
+    i18n_key = prefix_doctype_field(edition, partial_i18n_key)
     I18n.exists?(i18n_key) || I18n.exists?(doctype_default_key(i18n_key))
   end
 
-  def t_doctype(i18n_key)
+  def t_doctype_field(edition, partial_i18n_key)
+    i18n_key = prefix_doctype_field(edition, partial_i18n_key)
     I18n.t!(I18n.exists?(i18n_key) ? i18n_key : doctype_default_key(i18n_key))
   end
 
 private
+
+  def prefix_doctype_field(edition, partial_i18n_key)
+    "document_types.#{edition.document_type.id}.fields.#{partial_i18n_key}"
+  end
 
   def doctype_default_key(i18n_key)
     parts = i18n_key.split(".")
--- spec/helpers/document_type_helper_spec.rb
+++ spec/helpers/document_type_helper_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe DocumentTypeHelper, type: :helper do
             },
           },
         },
-        some_document: {
+        news_story: {
           fields: {
             title: {
               label: "My title",
@@ -20,44 +20,43 @@ RSpec.describe DocumentTypeHelper, type: :helper do
             },
           },
         },
-        some_document_no_fields_specified: {},
       },
     }
     I18n.backend.store_translations(:en, en)
   end
 
-  describe "#t_doctype_exists?" do
+  after { I18n.backend.reload! }
+
+  let(:edition) { build(:edition, document_type_id: "news_story") }
+
+  describe "#t_doctype_field?" do
     it "returns true if explicit i18n value exists" do
-      expect(t_doctype_exists?("document_types.some_document.fields.title.label")).to be true
+      expect(t_doctype_field?(edition, "title.label")).to be true
     end
 
     it "returns true if default i18n value exists" do
-      expect(t_doctype_exists?("document_types.some_document.fields.foo.label")).to be true
+      expect(t_doctype_field?(edition, "foo.label")).to be true
     end
 
     it "returns false if neither explicit nor default i18n value exists" do
-      expect(t_doctype_exists?("document_types.some_document.some_unknown_field")).to be false
+      expect(t_doctype_field?(edition, "some_unknown_field")).to be false
     end
   end
 
-  describe "#t_doctype" do
+  describe "#t_doctype_field" do
     it "returns the explicit i18n value if it exists" do
-      expect(t_doctype("document_types.some_document.fields.title.label")).to eq("My title")
-    end
-
-    it "inherits entire hashes from the default if explicit search has no match" do
-      expect(t_doctype("document_types.some_document_no_fields_specified.fields.foo.label")).to eq("Foo")
+      expect(t_doctype_field(edition, "title.label")).to eq("My title")
     end
 
     it "inherits entire explicit values from the default if explicit search has no match" do
-      expect(t_doctype("document_types.some_document.fields.foo.label")).to eq("Foo")
+      expect(t_doctype_field(edition, "foo.label")).to eq("Foo")
     end
 
     it "raises a I18n::MissingTranslationData exception if no explicit or default matches" do
-      expect { t_doctype("document_types.some_document.some_unknown_field") }
+      expect { t_doctype_field(edition, "some_unknown_field") }
         .to raise_error(
           I18n::MissingTranslationData,
-          "translation missing: en.document_types.default.some_unknown_field",
+          "translation missing: en.document_types.default.fields.some_unknown_field",
         )
     end
   end
