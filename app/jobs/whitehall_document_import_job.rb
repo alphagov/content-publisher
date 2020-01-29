da7911a32ee0dd62dc9b440f1758afab0d933139
Emma Beynon <emma.beynon@digital.cabinet-office.gov.uk>
2020-01-29 10:45:30 +0000

    Retry Whitehall Document Import Job
    We have decided to retry the Whitehall Document Import Job when a
    `GdsApi::BaseError` is raised as we have a number of external GDS APIs which
    could time out or fail for any number of reasons and they should be safe to
    retry. At this point we've decided not to retry on a generic `StandardError`
    exception as our current expectation is that this would most likely be raised
    due to an issue in the document data or our code, in which case retrying would
    be pointless and we'd be better off logging the error and retrying.  We may
    decide to revisit this decision if we see a lot of these exceptions or they're
    being raised due to issues that can be fixed by a retry.
    
    We've decided to opt for 5 retries with the wait inbetween tries increasing
    exponentially as we have some precendence for this approach in jobs such as
    `EmailDeliveryJob` where retries are attempted after an API failure.

--- app/jobs/whitehall_document_import_job.rb
+++ app/jobs/whitehall_document_import_job.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 class WhitehallDocumentImportJob < ApplicationJob
+  # retry at 3s, 18s, 83s, 258s, 627s
+  retry_on(GdsApi::BaseError, attempts: 5, wait: :exponentially_longer)
+
   def perform(document_import)
     WhitehallImporter.import_and_sync(document_import)
     document_import.whitehall_migration&.check_migration_finished
--- spec/jobs/whitehall_document_import_job_spec.rb
+++ spec/jobs/whitehall_document_import_job_spec.rb
@@ -23,4 +23,14 @@ RSpec.describe WhitehallDocumentImportJob do
     expect(whitehall_migration_document_import.whitehall_migration).to receive(:check_migration_finished)
     WhitehallDocumentImportJob.perform_now(whitehall_migration_document_import)
   end
+
+  context "when a GdsApi::BaseError exception is raised" do
+    it "retries the job" do
+      allow(WhitehallImporter).to receive(:import_and_sync)
+                              .and_raise(GdsApi::BaseError)
+      WhitehallDocumentImportJob.perform_now(whitehall_migration_document_import)
+
+      expect(WhitehallDocumentImportJob).to have_been_enqueued
+    end
+  end
 end
