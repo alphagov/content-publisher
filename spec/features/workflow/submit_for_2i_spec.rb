5106d6c9da95206b144abc8d884f72891de78dd9
Ben Thorner <ben.thorner@digital.cabinet-office.gov.uk>
2020-02-05 11:26:15 +0000

    Remove unhappy edge case steps in feature
    Previously we checked that editing an edition under review did not
    revert its state to 'draft', which isn't a core aspect of the 'Submit
    for 2i' feature. Our code is a long way away from allowing this to
    happen, noting that we only reset the 'draft' state in one place.
    This removes the extra steps, as they're not clearly adding value.

--- spec/features/workflow/submit_for_2i_spec.rb
+++ spec/features/workflow/submit_for_2i_spec.rb
@@ -8,9 +8,6 @@ RSpec.feature "Submit for 2i" do
     then_i_see_the_edition_is_submitted
     and_i_see_a_link_to_the_edition
     and_i_see_the_timeline_entry
-
-    when_i_edit_the_edition
-    then_i_see_it_is_still_in_review
   end
 
   def given_there_is_a_draft_edition
@@ -37,22 +34,8 @@ RSpec.feature "Submit for 2i" do
     end
   end
 
-  def then_i_see_it_is_still_in_review
-    expect(page).to have_content I18n.t!("documents.show.submitted_for_review.title")
-    expect(page).to have_content I18n.t!("user_facing_states.submitted_for_review.name")
-  end
-
   def and_i_see_a_link_to_the_edition
     review_url = find_field(I18n.t!("documents.show.submitted_for_review.label")).value
     expect(review_url).to match(document_url(@edition.document))
-  end
-
-  def when_i_edit_the_edition
-    stub_any_publishing_api_put_content
-    click_on "Document summary"
-
-    click_on "Change Content"
-    fill_in "title", with: "a new title"
-    click_on "Save"
   end
 end
