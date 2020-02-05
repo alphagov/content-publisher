9f33033ded977c718ad56465b7c5bbb46d7607e8
Graham Lewis <graham.lewis@digital.cabinet-office.gov.uk>
2020-02-05 15:21:46 +0000

    Display imported fact checks from Whitehall
    Internal notes with content are highlighted to the user, those without content
    are not.
    
    There are two things we need to maintain with respect to fact checks: a request
    (i.e. a request sent to a department or individual) and a response (i.e. the
    response coming back from the recipient(s) of the original request). These are
    migrated as two seperate entries.

--- spec/views/documents/history/_whitehall_entry.html.erb_spec.rb
+++ spec/views/documents/history/_whitehall_entry.html.erb_spec.rb
@@ -44,4 +44,62 @@ RSpec.describe "documents/history/_whitehall_entry.html.erb" do
     expect(rendered).to have_content(note)
     expect(rendered).to have_selector(".app-timeline-entry--highlighted")
   end
+
+  it "does not highlight an imported fact check request timeline entry without content" do
+    timeline_entry = create(:timeline_entry,
+                            :whitehall_imported,
+                            whitehall_entry_type: :fact_check_request)
+    timeline_entry.details[:contents] = {}
+
+    render partial: "documents/history/whitehall_entry",
+                    locals: { entry: timeline_entry }
+    expect(rendered).to_not have_selector(".app-timeline-entry--highlighted")
+  end
+
+  it "shows the contents of an imported fact check request timeline entry" do
+    email = "someone@somewhere.com"
+    instructions = "Do something, then do something else"
+    timeline_entry = create(:timeline_entry,
+                            :whitehall_imported,
+                            whitehall_entry_type: :fact_check_request)
+    timeline_entry.details[:contents] = {
+      email_address: email,
+      instructions: instructions,
+    }
+
+    render partial: "documents/history/whitehall_entry",
+                    locals: { entry: timeline_entry }
+    expect(rendered).to have_content(email)
+    expect(rendered).to have_content(instructions)
+    expect(rendered).to have_selector(".app-timeline-entry--highlighted")
+  end
+
+  it "does not highlight an imported fact check response timeline entry without content" do
+    timeline_entry = create(:timeline_entry,
+                            :whitehall_imported,
+                            whitehall_entry_type: :fact_check_response)
+    timeline_entry.details[:contents] = {}
+
+    render partial: "documents/history/whitehall_entry",
+                    locals: { entry: timeline_entry }
+    expect(rendered).to_not have_selector(".app-timeline-entry--highlighted")
+  end
+
+  it "shows the contents of an imported fact check response timeline entry" do
+    email = "someone@somewhere.com"
+    comments = "I have done what you requested"
+    timeline_entry = create(:timeline_entry,
+                            :whitehall_imported,
+                            whitehall_entry_type: :fact_check_response)
+    timeline_entry.details[:contents] = {
+      email_address: email,
+      comments: comments,
+    }
+
+    render partial: "documents/history/whitehall_entry",
+                    locals: { entry: timeline_entry }
+    expect(rendered).to have_content(email)
+    expect(rendered).to have_content(comments)
+    expect(rendered).to have_selector(".app-timeline-entry--highlighted")
+  end
 end
