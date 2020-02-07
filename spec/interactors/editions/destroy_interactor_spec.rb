b52f2658c2bdf1093707c9cc0ec2017bfb2380b0
Kevin Dew <kevindew@me.com>
2020-02-07 20:11:59 +0000

    Add tests for Editions::DestroyInteractor
    The impetus to add tests for this was learning that there is actually a
    problem where the edition model used in this interactor is not up to
    date and thus a timeline entry is created with the wrong status.
    
    This adds tests for the whole file however the one on line 20 is a
    failing test that captures this problem.

---
+++ spec/interactors/editions/destroy_interactor_spec.rb
@@ -0,0 +1,55 @@
+# frozen_string_literal: true
+
+RSpec.describe Editions::DestroyInteractor do
+  describe ".call" do
+    let(:edition) { create(:edition) }
+    let(:user) { create :user }
+
+    let(:params) do
+      ActionController::Parameters.new(document: edition.document.to_param)
+    end
+
+    before do
+      stub_publishing_api_unreserve_path(edition.base_path)
+      stub_publishing_api_discard_draft(edition.content_id)
+    end
+
+    it "discards an edition" do
+      result = Editions::DestroyInteractor.call(params: params, user: user)
+      expect(result).to be_success
+      expect(result.edition).to be_discarded
+    end
+
+    it "delegates to the DeleteDraftEditionService" do
+      expect(DeleteDraftEditionService)
+        .to receive(:call)
+        .with(edition.document, user)
+      Editions::DestroyInteractor.call(params: params, user: user)
+    end
+
+    it "creates a timeline entry" do
+      expect { Editions::DestroyInteractor.call(params: params, user: user) }
+        .to change { TimelineEntry.where(entry_type: :draft_discarded).count }
+        .by(1)
+    end
+
+    context "when the Publishing API is down" do
+      before { stub_publishing_api_isnt_available }
+
+      it "fails with an api_error flag" do
+        result = Editions::DestroyInteractor.call(params: params, user: user)
+        expect(result).to be_failure
+        expect(result.api_error).to be(true)
+      end
+    end
+
+    context "when the edition isn't editable" do
+      let(:edition) { create(:edition, :published) }
+
+      it "raises a state error" do
+        expect { Editions::DestroyInteractor.call(params: params, user: user) }
+          .to raise_error(EditionAssertions::StateError)
+      end
+    end
+  end
+end
