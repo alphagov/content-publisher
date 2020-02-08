93f3cdc8978b9586803cb8601ba8d45f2d2b4f94
Kevin Dew <kevindew@me.com>
2020-02-08 01:39:11 +0000

    Fix link handling in removal timeline entry
    This fixes a bug where we had nil set on the wrong argument in link_to -
    it needs to be the first argument if we want the link text to match the
    href. It also resolves a problem where we only expected the
    alternative_url to be a path so suffixed it on the hostname, to resolve
    this we use URI.join which will default a scheme and hostname from Plek
    and overwrite those based on the value of alternative_url.
    
    As this was a bit fiddly and something that was broken I've added tests
    for it.

--- spec/views/documents/history/_content_publisher_entry.html.erb_spec.rb
+++ spec/views/documents/history/_content_publisher_entry.html.erb_spec.rb
@@ -25,4 +25,51 @@ RSpec.describe "documents/history/_content_publisher_entry.html.erb" do
                                       text: "Amazing internal note")
     expect(rendered).to have_content(I18n.t!("documents.history.entry_types.internal_note"))
   end
+
+  context "when the timeline entry is for a removal" do
+    it "can show an explanatory_note" do
+      removal = create(:removal, explanatory_note: "My note")
+      timeline_entry = create(:timeline_entry,
+                              entry_type: :removed,
+                              details: removal)
+      render partial: "documents/history/content_publisher_entry",
+             locals: { entry: timeline_entry }
+      expect(rendered).to have_content("My note")
+    end
+
+    it "can show a link to an alternative URL" do
+      removal = create(:removal, alternative_url: "https://example.com")
+      timeline_entry = create(:timeline_entry,
+                              entry_type: :removed,
+                              details: removal)
+      render partial: "documents/history/content_publisher_entry",
+             locals: { entry: timeline_entry }
+      expect(rendered).to have_content("Alternative URL https://example.com",
+                                       normalize_ws: true)
+      expect(rendered).to have_link("https://example.com",
+                                    href: "https://example.com")
+    end
+
+    it "can show a link to an alternative URL that is a path" do
+      removal = create(:removal, alternative_url: "/path")
+      timeline_entry = create(:timeline_entry,
+                              entry_type: :removed,
+                              details: removal)
+      render partial: "documents/history/content_publisher_entry",
+             locals: { entry: timeline_entry }
+      expect(rendered).to have_link("https://www.test.gov.uk/path",
+                                    href: "https://www.test.gov.uk/path")
+    end
+
+    it "can show a redirect" do
+      removal = create(:removal, redirect: true, alternative_url: "https://example.com")
+      timeline_entry = create(:timeline_entry,
+                              entry_type: :removed,
+                              details: removal)
+      render partial: "documents/history/content_publisher_entry",
+             locals: { entry: timeline_entry }
+      expect(rendered).to have_content("Redirected to https://example.com",
+                                       normalize_ws: true)
+    end
+  end
 end
