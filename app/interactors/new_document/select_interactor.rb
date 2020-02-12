88878a9503efd6e55555677606c7677c2728e005
Leena Gupte <leena.gupte@digital.cabinet-office.gov.uk>
2020-02-12 12:06:50 +0000

    Add a SelectInteractor
    This combines the actions taken by:
    - NewDocument::ChooseSupertypeInteractor
    - NewDocument::CreateInteractor

---
+++ app/interactors/new_document/select_interactor.rb
@@ -0,0 +1,46 @@
+class NewDocument::SelectInteractor < ApplicationInteractor
+  delegate :params,
+           :user,
+           :document,
+           :document_type_selection,
+           :selected_option,
+           to: :context
+
+  def call
+    find_selection
+    check_for_issues
+    return unless selected_option.document_type?
+
+    create_document
+    create_timeline_entry
+  end
+
+private
+
+  def find_selection
+    context.document_type_selection = DocumentTypeSelection.find(params[:type])
+    context.selected_option = document_type_selection.find_option(params[:selected_option_id])
+  end
+
+  def check_for_issues
+    issues = Requirements::CheckerIssues.new
+    issues.create(:document_type_selection, :not_selected) unless selected_option
+
+    context.fail!(issues: issues) if issues.any?
+  end
+
+  def create_document
+    context.document = CreateDocumentService.call(
+      document_type_id: selected_option.id, tags: default_tags, user: user,
+    )
+  end
+
+  def create_timeline_entry
+    TimelineEntry.create_for_status_change(entry_type: :created,
+                                           status: document.current_edition.status)
+  end
+
+  def default_tags
+    user.organisation_content_id ? { primary_publishing_organisation: [user.organisation_content_id] } : {}
+  end
+end
---
+++ spec/interactors/new_document/select_interactor_spec.rb
@@ -0,0 +1,55 @@
+RSpec.describe NewDocument::SelectInteractor do
+  describe ".call" do
+    let(:user) { build(:user, organisation_content_id: "org-id") }
+
+    it "succeeds with valid parameters" do
+      result = described_class.call(params: { type: "root", selected_option_id: "news" })
+      expect(result).to be_success
+    end
+
+    it "fails if the selected_option_id is empty" do
+      result = described_class.call(params: { type: "root", selected_option_id: "" })
+      expect(result).not_to be_success
+    end
+
+    it "fails if the selected_option_id isn't passed in" do
+      result = described_class.call(params: { type: "root" })
+      expect(result).not_to be_success
+    end
+
+    it "returns the document type selection" do
+      document_type_selection = DocumentTypeSelection.find("root")
+
+      result = described_class.call(params: { type: "root", selected_option_id: "news" })
+      expect(result.document_type_selection.id).to eq(document_type_selection.id)
+    end
+
+    it "returns the selected_option" do
+      selected_option = DocumentTypeSelection.find("root").find_option("news")
+
+      result = described_class.call(params: { type: "root", selected_option_id: "news" })
+      expect(result.selected_option.id).to eq(selected_option.id)
+    end
+
+    context "when the selected document type doesn't have subtypes and can be created" do
+      let(:params) do
+        {
+          type: "news",
+          selected_option_id: "news_story",
+        }
+      end
+
+      it "creates a new document" do
+        expect { described_class.call(params: params, user: user) }
+          .to change(Document, :count)
+          .by(1)
+      end
+
+      it "creates a timeline entry" do
+        expect { described_class.call(params: params, user: user) }
+          .to change(TimelineEntry, :count)
+          .by(1)
+      end
+    end
+  end
+end
