4f51310fe79a368ea630d634db41ae98e0bcb467
Leena Gupte <leena.gupte@digital.cabinet-office.gov.uk>
2020-02-12 11:54:01 +0000

    Add a DocumentSelectionType model
    This takes the values from document_type_selections.yml

---
+++ app/models/document_type_selection.rb
@@ -0,0 +1,22 @@
+class DocumentTypeSelection
+  include InitializeWithHash
+
+  attr_reader :id, :options
+
+  def self.find(id)
+    item = all.find { |document_type_selection| document_type_selection.id == id }
+    item || (raise NotFoundError, "Document type selection #{id} not found")
+  end
+
+  def self.all
+    @all ||= begin
+      hashes = YAML.load_file(Rails.root.join("config/document_type_selections.yml"))
+
+      hashes.map do |hash|
+        new(hash)
+      end
+    end
+  end
+
+  class NotFoundError < RuntimeError; end
+end
