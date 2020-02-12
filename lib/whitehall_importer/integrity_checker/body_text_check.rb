f94b4b76a932b52076867a3e508b28a3c8e0c29a
Bruce Bolt <bruce.bolt@digital.cabinet-office.gov.uk>
2020-02-12 09:15:59 +0000

    Do not compare inline attachment file size in integrity check
    There are differences between the file size format for inline attachments
    published in Whitehall compared to those published in Content Publisher.
    We should therefore not compare these when performing the integrity check.

--- lib/whitehall_importer/integrity_checker/body_text_check.rb
+++ lib/whitehall_importer/integrity_checker/body_text_check.rb
@@ -8,7 +8,24 @@ module WhitehallImporter
     end
 
     def sufficiently_similar?
-      Sanitize.clean(publishing_api_body_text).squish == Sanitize.clean(proposed_body_text).squish
+      proposed_body = remove_attachment_file_size(proposed_body_text)
+      publishing_api_body = remove_attachment_file_size(publishing_api_body_text)
+      Sanitize.clean(publishing_api_body).squish == Sanitize.clean(proposed_body).squish
+    end
+
+  private
+
+    def remove_attachment_file_size(body)
+      file_size_selector = ".attachment-inline .file-size, .gem-c-attachment-link .gem-c-attachment-link__attribute:nth-of-type(2)"
+      remove_html_elements(body, file_size_selector)
+    end
+
+    def remove_html_elements(body, selector)
+      doc = Nokogiri.HTML(body)
+      doc.search(selector).each do |el|
+        el.replace("")
+      end
+      doc.to_html
     end
   end
 end
--- spec/lib/whitehall_importer/integrity_checker/body_text_check_spec.rb
+++ spec/lib/whitehall_importer/integrity_checker/body_text_check_spec.rb
@@ -15,6 +15,32 @@ RSpec.describe WhitehallImporter::IntegrityChecker::BodyTextCheck do
       expect(integrity_check.sufficiently_similar?).to be true
     end
 
+    it "returns true even if there is a mismatch in inline atttachment URL filesize" do
+      proposed_body = %(
+        <p>
+          <span class="gem-c-attachment-link">
+            <a class="govuk-link" href="filename.pdf" target="_blank">Test File</a>
+            (<span class="gem-c-attachment-link__attribute"><abbr title="Portable Document Format" class="gem-c-attachment-link__abbr">PDF</abbr></span>,
+              <span class="gem-c-attachment-link__attribute">391 KB</span>, <span class="gem-c-attachment-link__attribute">9 pages</span>)
+          </span>
+        </p>
+      )
+
+      publishing_api_body = %(
+        <p>
+          <span class="attachment-inline">
+            <a href="/filename.pdf">Test File</a>
+            (<span class="type">PDF</span>,
+              <span class="file-size">391KB</span>,
+              <span class="page-length">9 pages</span>)
+          </span>
+        </p>
+      )
+
+      integrity_check = described_class.new(proposed_body, publishing_api_body)
+      expect(integrity_check.sufficiently_similar?).to be true
+    end
+
     it "returns false when the body text doesn't match" do
       integrity_check = described_class.new("Some text", "Some different text")
       expect(integrity_check.sufficiently_similar?).to be false
