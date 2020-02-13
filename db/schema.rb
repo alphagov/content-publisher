01da91664203539fdaae907b62fcccc38b04fe52
Peter Hartshorn <peter.hartshorn@digital.cabinet-office.gov.uk>
2020-02-13 15:03:35 +0000

    Backfill published_at column for edition
    This backfills the published_at column for previously published
    editions, we use when the published status was created as a means to
    work this time out as this should reflect the actual publishing time.
    
    This relies on:
    https://github.com/alphagov/content-publisher/pull/1777
    
    Trello:
    https://trello.com/c/ShhyTqqU/1459-create-store-and-backfill-publishedat-on-editions

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_18_153946) do
+ActiveRecord::Schema.define(version: 2020_02_18_161942) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
