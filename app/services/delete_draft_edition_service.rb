9c1e8a761a03b83b1cc1cf13f30d544757a59867
Kevin Dew <kevindew@me.com>
2020-02-13 21:23:08 +0000

    Switch DeleteDraftEditionService to use helper services
    This reduces the code and tests for this class.

--- app/services/delete_draft_edition_service.rb
+++ app/services/delete_draft_edition_service.rb
@@ -9,7 +9,7 @@ class DeleteDraftEditionService < ApplicationService
     raise "Trying to delete a live edition" if edition.live?
 
     begin
-      delete_assets(edition.assets)
+      DeleteDraftAssetsService.call(edition)
       discard_draft(edition)
     rescue GdsApi::BaseError
       edition.update!(revision_synced: false)
@@ -17,7 +17,7 @@ class DeleteDraftEditionService < ApplicationService
     end
 
     reset_live_edition if document.live_edition
-    discard_path_reservations(edition) if edition.first?
+    DiscardPathReservationsService.call(edition) if edition.first?
     document.reload_current_edition
   end
 
@@ -25,17 +25,6 @@ private
 
   attr_reader :edition, :user
   delegate :document, to: :edition
-
-  def discard_path_reservations(edition)
-    paths = edition.revisions.map(&:base_path).uniq.compact
-    publishing_app = PreviewDraftEditionService::Payload::PUBLISHING_APP
-
-    paths.each do |path|
-      GdsApi.publishing_api.unreserve_path(path, publishing_app)
-    rescue GdsApi::HTTPNotFound
-      Rails.logger.warn("Tried to discard unreserved path #{path}")
-    end
-  end
 
   def reset_live_edition
     document.live_edition.update!(current: true)
@@ -59,18 +48,5 @@ private
 
     AssignEditionStatusService.call(edition, user: user, state: :discarded)
     edition.update!(current: false)
-  end
-
-  def delete_assets(assets)
-    assets.each do |asset|
-      next unless asset.draft?
-
-      begin
-        GdsApi.asset_manager.delete_asset(asset.asset_manager_id)
-      rescue GdsApi::HTTPNotFound
-        Rails.logger.warn("No asset to delete for id #{asset.asset_manager_id}")
-      end
-      asset.absent!
-    end
   end
 end
