9470f3e30888f4da5037f50fc9533046b69de6ef
Bruce Bolt <bruce.bolt@digital.cabinet-office.gov.uk>
2020-02-14 11:06:13 +0000

    Add extra test scenarios for body text check
    Added in an extra two scenarios that the body text check should be
    permitting but previously had no test coverage.

--- spec/lib/whitehall_importer/integrity_checker/body_text_check_spec.rb
+++ spec/lib/whitehall_importer/integrity_checker/body_text_check_spec.rb
@@ -5,6 +5,16 @@ RSpec.describe WhitehallImporter::IntegrityChecker::BodyTextCheck do
       expect(integrity_check.sufficiently_similar?).to be true
     end
 
+    it "returns true when the body text whitespace does not match" do
+      integrity_check = described_class.new("Some text", "Some     text")
+      expect(integrity_check.sufficiently_similar?).to be true
+    end
+
+    it "returns true when the HTML does not match" do
+      integrity_check = described_class.new("<b>Some text</b>", "Some text")
+      expect(integrity_check.sufficiently_similar?).to be true
+    end
+
     it "returns false when the body text doesn't match" do
       integrity_check = described_class.new("Some text", "Some different text")
       expect(integrity_check.sufficiently_similar?).to be false
