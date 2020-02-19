a02c2ccf2e72db6017aadd760f1528b6c59c86d1
Leena Gupte <leena.gupte@digital.cabinet-office.gov.uk>
2020-02-19 17:46:01 +0000

    Show helpful error in selection uniqueness test

--- spec/models/document_type_selection_spec.rb
+++ spec/models/document_type_selection_spec.rb
@@ -34,7 +34,7 @@ RSpec.describe DocumentTypeSelection do
 
     it "only allows unique document type selections" do
       ids = document_type_selections.pluck("id")
-      expect(ids.size).to eq(ids.uniq.size)
+      expect(ids).to eq(ids.uniq), "duplicate document type selection ids in: #{ids}"
     end
 
     it "only allows unique options in each document_type_selection" do
