3fe1c56856b9050e6ef02c21dccc2f170c515a71
Bruce Bolt <bruce.bolt@digital.cabinet-office.gov.uk>
2020-02-20 08:59:35 +0000

    Add "What Managing Editors can do" page
    This is a new page that will tell users what being a managing editor
    means, and informs them if they have that permission or not.

--- spec/features/publisher_information/visit_publisher_information_pages_spec.rb
+++ spec/features/publisher_information/visit_publisher_information_pages_spec.rb
@@ -7,6 +7,13 @@ RSpec.feature "User can view the publisher information pages" do
     then_i_can_see_beta_capabilities_page
     when_i_click_on_the_how_to_use_publisher_link_in_footer
     then_i_can_see_how_to_use_publisher_page
+    when_i_click_on_the_what_managing_editors_can_do_link_in_footer
+    then_i_can_see_the_what_managing_editors_can_do_page
+    and_i_see_i_am_not_a_managing_editor
+    when_i_have_the_managing_editor_permission
+    and_i_click_on_the_what_managing_editors_can_do_link_in_footer
+    then_i_can_see_the_what_managing_editors_can_do_page
+    and_i_see_i_am_a_managing_editor
   end
 
   def given_im_on_the_home_page
@@ -47,5 +54,31 @@ RSpec.feature "User can view the publisher information pages" do
     within(".page-title") do
       expect(page).to have_content(I18n.t("publisher_information.how_to_use_publisher.title"))
     end
+  end
+
+  def when_i_click_on_the_what_managing_editors_can_do_link_in_footer
+    within(".govuk-footer") do
+      click_on "What Managing Editors can do"
+    end
+  end
+
+  alias :and_i_click_on_the_what_managing_editors_can_do_link_in_footer :when_i_click_on_the_what_managing_editors_can_do_link_in_footer
+
+  def then_i_can_see_the_what_managing_editors_can_do_page
+    within(".page-title") do
+      expect(page).to have_content(I18n.t("publisher_information.what_managing_editors_can_do.title"))
+    end
+  end
+
+  def when_i_have_the_managing_editor_permission
+    current_user.update_attribute(:permissions, [User::MANAGING_EDITOR_PERMISSION])
+  end
+
+  def and_i_see_i_am_not_a_managing_editor
+    expect(page).to have_content(I18n.t("publisher_information.what_managing_editors_can_do.user_status.without_managing_editor_status"))
+  end
+
+  def and_i_see_i_am_a_managing_editor
+    expect(page).to have_content(I18n.t("publisher_information.what_managing_editors_can_do.user_status.with_managing_editor_status"))
   end
 end
