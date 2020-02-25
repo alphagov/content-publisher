f98b8db892ee36ff14a5baf2804743144409ef80
Ben Thorner <ben.thorner@digital.cabinet-office.gov.uk>
2020-02-25 14:45:26 +0000

    Move deleting a lead image into scenario
    Previously the feature test for deleting a lead image was separate
    from the other deletion scenarios. While the outcome is slightly
    different, the user actions are so similar that it sets a confusing
    precedent to have it in a different file. This incorporates the
    scenario into the general feature test for deleting an image.

--- spec/features/editing_images/delete_image_spec.rb
+++ spec/features/editing_images/delete_image_spec.rb
@@ -7,12 +7,29 @@ RSpec.feature "Delete an image" do
     and_i_see_the_timeline_entry
   end
 
+  scenario "lead image" do
+    given_there_is_an_edition_with_a_lead_image
+    when_i_visit_the_images_page
+    when_i_delete_the_lead_image
+    then_i_see_the_lead_image_is_gone
+    and_i_see_the_timeline_entry
+  end
+
   scenario "inline image", js: true do
     given_there_is_an_edition_with_images
     when_i_insert_an_inline_image
     and_i_delete_the_image
     then_i_see_the_image_is_gone
     and_i_see_the_timeline_entry
+  end
+
+  def given_there_is_an_edition_with_a_lead_image
+    document_type = build(:document_type, images: true)
+    @image_revision = create(:image_revision, :on_asset_manager)
+
+    @edition = create(:edition,
+                      document_type: document_type,
+                      lead_image_revision: @image_revision)
   end
 
   def given_there_is_an_edition_with_images
@@ -42,9 +59,22 @@ RSpec.feature "Delete an image" do
     click_on "Delete image"
   end
 
+  def when_i_delete_the_lead_image
+    stub_publishing_api_put_content(@edition.content_id, {})
+    click_on "Delete lead image"
+  end
+
   def then_i_see_the_image_is_gone
     expect(page).to have_content(I18n.t!("images.index.flashes.deleted", file: @image_revision.filename))
     expect(page).not_to have_selector("#image-#{@image_revision.image_id}")
+  end
+
+  def then_i_see_the_lead_image_is_gone
+    expect(page).to have_content(I18n.t!("images.index.flashes.lead_image.deleted", file: @image_revision.filename))
+    expect(page).not_to have_selector("#image-#{@image_revision.image_id}")
+
+    visit document_path(@edition.document)
+    expect(page).to have_content(I18n.t!("documents.show.lead_image.no_lead_image"))
   end
 
   def and_i_see_the_timeline_entry
