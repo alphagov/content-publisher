fd5721fa72b41f5e33b1d0002fbda66769e90bb4
Peter Hartshorn <peter.hartshorn@digital.cabinet-office.gov.uk>
2020-03-02 17:03:48 +0000

    Add ChangeHistory class
    This commit adds a ChangeHistory class which will be used for
    determining what change history is relevant to what edition.
    
    The rules are as follows:
    * change history shouldn't contain the first published at change note
    * change history shouldn't contain the current editions change note
    * change history entrys should be ordered newest to oldest

---
+++ spec/lib/whitehall_importer/change_history_spec.rb
@@ -0,0 +1,71 @@
+RSpec.describe WhitehallImporter::ChangeHistory do
+  describe "#for" do
+    let(:first_edition) { build(:whitehall_export_edition, :superseded) }
+
+    it "returns the change history for the editions preceding the given edition number" do
+      second_edition = build(:whitehall_export_edition, :superseded,
+                             change_note: "Some change",
+                             published_at: Time.zone.now.beginning_of_week.rfc3339)
+      third_edition = build(:whitehall_export_edition, :published,
+                            change_note: "Other change",
+                            published_at: Time.zone.now.beginning_of_day.rfc3339)
+      whitehall_export = build(:whitehall_export_document,
+                               editions: [first_edition, second_edition, third_edition])
+
+      expect(described_class.new(whitehall_export).for(4)).to match([
+        { "id" => anything, "note" => "Other change", "public_timestamp" => Time.zone.now.beginning_of_day.rfc3339 },
+        { "id" => anything, "note" => "Some change", "public_timestamp" => Time.zone.now.beginning_of_week.rfc3339 },
+      ])
+    end
+
+    it "returns draft edition change history if it has been previously unpublished" do
+      edition = build(:whitehall_export_edition,
+                      change_note: "Some change",
+                      unpublishing: build(:whitehall_export_unpublishing),
+                      revision_history: [build(:whitehall_export_revision_history_event,
+                                               state: "published",
+                                               created_at: Time.zone.now.beginning_of_day.rfc3339)])
+      whitehall_export = build(:whitehall_export_document, editions: [first_edition, edition])
+
+      expect(described_class.new(whitehall_export).for(3)).to match([
+        { "id" => anything, "note" => "Some change", "public_timestamp" => Time.zone.now.beginning_of_day.rfc3339 },
+      ])
+    end
+
+    it "doesn't include the first edition in change history" do
+      whitehall_export = build(:whitehall_export_document, editions: [first_edition])
+      expect(described_class.new(whitehall_export).for(2)).to be_empty
+    end
+
+    it "doesn't include draft editions in change history" do
+      edition = build(:whitehall_export_edition)
+      whitehall_export = build(:whitehall_export_document, editions: [first_edition, edition])
+
+      expect(described_class.new(whitehall_export).for(3)).to be_empty
+    end
+
+    it "doesn't include minor change editions in change history" do
+      edition = build(:whitehall_export_edition, :published, minor_change: true, change_note: "")
+      whitehall_export = build(:whitehall_export_document, editions: [first_edition, edition])
+
+      expect(described_class.new(whitehall_export).for(3)).to be_empty
+    end
+
+    it "raises if an edition has a major change but no change note" do
+      edition = build(:whitehall_export_edition, :published, change_note: "")
+      whitehall_export = build(:whitehall_export_document, editions: [first_edition, edition])
+
+      expect { described_class.new(whitehall_export).for(2) }
+        .to raise_error(WhitehallImporter::AbortImportError, "Edition has a major change but no change note")
+    end
+
+    it "raises if an edition has a major change but no publish event" do
+      edition = build(:whitehall_export_edition, :published,
+                      revision_history: [build(:whitehall_export_revision_history_event)])
+      whitehall_export = build(:whitehall_export_document, editions: [first_edition, edition])
+
+      expect { described_class.new(whitehall_export).for(2) }
+        .to raise_error(WhitehallImporter::AbortImportError, "Edition has a major change but no publish event")
+    end
+  end
+end
