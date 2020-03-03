3dd8c2c9b8c078aacbd8028d61077934180bbe13
Rosa Fox <rosa.fox@digital.cabinet-office.gov.uk>
2020-03-03 17:06:51 +0000

    Only filter by valid states
    Co-authored-by: Kevin Dew <kevin.dew@digital.cabinet-office.gov.uk>

--- app/controllers/whitehall_migration_controller.rb
+++ app/controllers/whitehall_migration_controller.rb
@@ -7,8 +7,9 @@ class WhitehallMigrationController < ApplicationController
 
   def document_imports
     @whitehall_migration = WhitehallMigration.find(params[:migration_id])
+    @state = params[:state] if WhitehallMigration::DocumentImport.states.keys.include?(params[:state])
     scope = @whitehall_migration.document_imports
-    scope = scope.where(state: params[:state]) if params[:state]
+    scope = scope.where(state: @state) if @state
     @document_imports = scope.order(:id).page(params.fetch(:page, 1)).per(50)
   end
 
