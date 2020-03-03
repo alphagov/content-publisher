33ce69111b3ea1fcd12a19b4e0873bf66789b64a
Graham Lewis <graham.lewis@digital.cabinet-office.gov.uk>
2020-03-03 14:15:15 +0000

    Allows a single change note to be deleted from the history of a document.
    Co-authored-by: Bruce Bolt <bruce.bolt@digital.cabinet-office.gov.uk>

--- lib/tasks/change_history.rake
+++ lib/tasks/change_history.rake
@@ -8,4 +8,16 @@ namespace :change_history do
       puts [entry["id"], entry["public_timestamp"], entry["note"]].join(" | ")
     end
   end
+
+  desc "Delete a single change note for a document, e.g. change_history:delete[content_id change-note-id]"
+  task :delete, %i[content_id change_history_id] => :environment do |_, args|
+    Tasks::EditionUpdater.call(args.content_id,
+                               locale: ENV.fetch("LOCALE", "en"),
+                               user_email: ENV["USER_EMAIL"]) do |edition, updater|
+      entry = edition.change_history.find { |item| item["id"] == args.change_history_id }
+      raise "No change history entry with id #{args.change_history_id}" unless entry
+
+      updater.assign(change_history: edition.change_history - [entry])
+    end
+  end
 end
--- spec/lib/tasks/change_history_spec.rb
+++ spec/lib/tasks/change_history_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe "Change History tasks" do
   before do
     populate_default_government_bulk_data
     stub_any_publishing_api_put_content
+    allow(Tasks::EditionUpdater).to receive(:call).and_call_original
   end
 
   describe "change_history:show" do
@@ -42,6 +43,59 @@ RSpec.describe "Change History tasks" do
       }.to output(
         "#{change_history.first[:id]} | 2020-02-20T16:00:00+00:00 | Test\n",
       ).to_stdout
+    end
+  end
+
+  describe "change_history:delete" do
+    let(:id_to_delete) { SecureRandom.uuid }
+
+    let(:change_history) do
+      [
+        {
+          id: SecureRandom.uuid,
+          public_timestamp: "2020-02-20T18:00:00+00:00",
+          note: "Test 1",
+        }, {
+          id: id_to_delete,
+          public_timestamp: "2020-02-20T17:00:00+00:00",
+          note: "Test 2",
+        }, {
+          id: SecureRandom.uuid,
+          public_timestamp: "2020-02-20T16:00:00+00:00",
+          note: "Test 3",
+        }
+      ]
+    end
+
+    let(:edition) do
+      create(:edition,
+             locale: "en",
+             change_history: change_history)
+    end
+
+    before { Rake::Task["change_history:delete"].reenable }
+
+    it "deletes a change history entry" do
+      expected_change_history = [
+        change_history.first.with_indifferent_access,
+        change_history.last.with_indifferent_access,
+      ]
+
+      Rake::Task["change_history:delete"].invoke(edition.content_id, id_to_delete)
+
+      expect(edition.reload.revision.metadata_revision.change_history)
+        .to eq(expected_change_history)
+    end
+
+    it "calls the edition updater" do
+      Rake::Task["change_history:delete"].invoke(edition.content_id, id_to_delete)
+
+      expect(Tasks::EditionUpdater).to have_received(:call)
+    end
+
+    it "raises an error when the change history ID does not exist" do
+      expect { Rake::Task["change_history:delete"].invoke(edition.content_id, "abc123") }
+        .to raise_error("No change history entry with id abc123")
     end
   end
 end
