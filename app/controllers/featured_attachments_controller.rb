5e0ea0d13d1afe4cec7e1f00a63f55c2fbe1374e
Leena Gupte <leena.gupte@digital.cabinet-office.gov.uk>
2020-03-04 16:12:16 +0000

    Restrict access to featured attachments index
    Only allow users to access the attachments index page for document types
    that allow featured attachments.

--- app/controllers/featured_attachments_controller.rb
+++ app/controllers/featured_attachments_controller.rb
@@ -2,5 +2,9 @@ class FeaturedAttachmentsController < ApplicationController
   def index
     @edition = Edition.find_current(document: params[:document])
     assert_edition_state(@edition, &:editable?)
+
+    assert_edition_feature(@edition, assertion: "supports featured attachments") do
+      @edition.document_type.attachments.featured?
+    end
   end
 end
