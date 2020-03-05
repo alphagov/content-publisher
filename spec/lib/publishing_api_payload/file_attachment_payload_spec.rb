6d992a6fa1427b82343fcae0efc08aa3090595ca
Leena Gupte <leena.gupte@digital.cabinet-office.gov.uk>
2020-03-05 12:27:06 +0000

    Create a payload for file attachment data
    This applies to both new and publications types:
    [news](https://github.com/alphagov/govuk-content-schemas/blob/e68bb912939d1467ab6ed49b190dc4b8d5061477/formats/news_article.jsonnet#L16-L22)
    [publication](https://github.com/alphagov/govuk-content-schemas/blob/e68bb912939d1467ab6ed49b190dc4b8d5061477/formats/publication.jsonnet#L33-L40)
    
    These attributes have been delegated to a new class because in future
    work, the attachment details required for publications will need to
    expand to include [extra information](https://github.com/alphagov/govuk-content-schemas/blob/6d1390ad2598dddddf41181cd38549814715d589/formats/shared/definitions/asset_links.jsonnet#L32-L40)

---
+++ spec/lib/publishing_api_payload/file_attachment_payload_spec.rb
@@ -0,0 +1,24 @@
+RSpec.describe PublishingApiPayload::FileAttachmentPayload do
+  describe "#payload" do
+    it "generates a file attachment payload for the publishing_api" do
+      attachment = build(:file_attachment_revision)
+      edition = create(:edition, file_attachment_revisions: [attachment])
+
+      payload = described_class.new(attachment, edition.document).payload
+
+      expected_payload = {
+        attachment_type: "file",
+        content_type: attachment.content_type,
+        file_size: attachment.byte_size,
+        filename: attachment.filename,
+        id: attachment.filename,
+        locale: edition.locale,
+        number_of_pages: attachment.number_of_pages,
+        title: attachment.title,
+        url: attachment.asset_url,
+      }
+
+      expect(payload).to match a_hash_including(expected_payload)
+    end
+  end
+end
