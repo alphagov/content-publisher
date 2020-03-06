312623e53a3a2f3487ee9d5da0bb24db0ab26a80
Leena Gupte <leena.gupte@digital.cabinet-office.gov.uk>
2020-03-06 14:36:23 +0000

    Retry Resync document job on error

--- app/jobs/resync_document_job.rb
+++ app/jobs/resync_document_job.rb
@@ -1,4 +1,10 @@
 class ResyncDocumentJob < ApplicationJob
+  retry_on(
+    GdsApi::BaseError,
+    attempts: 5,
+    wait: :exponentially_longer,
+  ) { |_job, error| GovukError.notify(error) }
+
   def perform(document)
     ResyncDocumentService.call(document)
   end
--- spec/jobs/resync_document_job_spec.rb
+++ spec/jobs/resync_document_job_spec.rb
@@ -1,10 +1,21 @@
 RSpec.describe ResyncDocumentJob do
-  it "delegates to the ResyncDocumentService" do
-    document = build(:document)
+  include ActiveJob::TestHelper
 
+  let(:document) { create(:document) }
+
+  before { populate_default_government_bulk_data }
+
+  it "delegates to the ResyncDocumentService" do
     expect(ResyncDocumentService)
       .to receive(:call)
       .with(document)
     described_class.perform_now(document)
+  end
+
+  it "retries the job when an exception is raised" do
+    allow(ResyncDocumentService).to receive(:call).and_raise(GdsApi::BaseError)
+    described_class.perform_now(document)
+
+    expect(described_class).to have_been_enqueued
   end
 end
