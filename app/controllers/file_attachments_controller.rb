7ff1d2e503e77530eedbab5ddb716f0434c19d55
Leena Gupte <leena.gupte@digital.cabinet-office.gov.uk>
2020-03-09 17:25:02 +0000

    Add confirmation page for deleting attachments
    This page will used in the next commit.
    
    Then intention is for this `confirm_delete` route to be used in the
    future to confirm the deletion of inline attachments, so a wizard param
    is being used to correctly route users back the the correct page when
    they:
    
    - click back
    - click cancel
    - confirm the deletion

--- app/controllers/file_attachments_controller.rb
+++ app/controllers/file_attachments_controller.rb
@@ -31,6 +31,14 @@ class FileAttachmentsController < ApplicationController
       .find_by!(file_attachment_id: params[:file_attachment_id])
   end
 
+  def confirm_delete
+    @edition = Edition.find_current(document: params[:document])
+    assert_edition_state(@edition, &:editable?)
+
+    @attachment = @edition.file_attachment_revisions
+      .find_by!(file_attachment_id: params[:file_attachment_id])
+  end
+
   def preview
     result = FileAttachments::PreviewInteractor.call(params: params, user: current_user)
     can_preview, api_error = result.to_h.values_at(:can_preview, :api_error)
