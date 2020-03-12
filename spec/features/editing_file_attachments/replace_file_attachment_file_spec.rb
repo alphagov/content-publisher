524d1b25acf707b764b28c95ec1b4d2668437e76
Leena Gupte <leena.gupte@digital.cabinet-office.gov.uk>
2020-03-12 16:41:51 +0000

    Add feature test for editing featured attachments

--- spec/features/editing_file_attachments/replace_file_attachment_file_spec.rb
+++ spec/features/editing_file_attachments/replace_file_attachment_file_spec.rb
@@ -1,8 +1,18 @@
 RSpec.feature "Replace a file attachment file", js: true do
-  scenario do
+  scenario "inline" do
     given_there_is_an_edition_with_an_attachment
     when_i_click_to_insert_an_attachment
     and_i_click_on_edit_file
+    and_i_upload_a_replacement_attachment_file
+    and_i_click_save
+    then_i_see_the_replacement_file
+    and_i_see_the_timeline_entry
+  end
+
+  scenario "featured" do
+    given_there_is_an_edition_with_featured_attachments
+    when_i_go_to_change_an_attachment
+    and_i_click_on_edit_attachment
     and_i_upload_a_replacement_attachment_file
     and_i_click_save
     then_i_see_the_replacement_file
@@ -19,6 +29,14 @@ RSpec.feature "Replace a file attachment file", js: true do
                       file_attachment_revisions: [@attachment_revision])
   end
 
+  def given_there_is_an_edition_with_featured_attachments
+    @attachment_revision = create(:file_attachment_revision)
+
+    @edition = create(:edition,
+                      document_type: build(:document_type, attachments: "featured"),
+                      file_attachment_revisions: [@attachment_revision])
+  end
+
   def when_i_click_to_insert_an_attachment
     visit content_path(@edition.document)
     find("markdown-toolbar details").click
@@ -26,8 +44,16 @@ RSpec.feature "Replace a file attachment file", js: true do
     expect(page).to have_content("74 Bytes")
   end
 
+  def when_i_go_to_change_an_attachment
+    visit featured_attachments_path(@edition.document)
+  end
+
   def and_i_click_on_edit_file
     click_on "Edit file"
+  end
+
+  def and_i_click_on_edit_attachment
+    click_on "Edit attachment"
   end
 
   def and_i_upload_a_replacement_attachment_file
