2318aee73816462350325b9ae7a2378d3623654f
Ben Thorner <ben.thorner@digital.cabinet-office.gov.uk>
2020-03-17 15:51:59 +0000

    Extract change history validation into class
    Previously the validation for change history was defined inline as
    part of the model. Since we intend to add another complex validation
    for featured attachment ordering, this extracts the validation into
    a separate class using 'validates', so we have a more scalable
    precendent for these data integrity checks.
    
    https://guides.rubyonrails.org/active_record_validations.html#custom-validators
    
    I thought they fitted best in a 'metadata_revision/' subdirectory,
    but I don't mind if they go somewhere else. The unit spec now calls
    the 'validates_each' method of the new validator directly.

--- spec/models/metadata_revision/change_history_validator_spec.rb
+++ spec/models/metadata_revision/change_history_validator_spec.rb
@@ -1,5 +1,9 @@
-RSpec.describe MetadataRevision do
-  describe "validating change_history" do
+RSpec.describe MetadataRevision::ChangeHistoryValidator do
+  describe "#validate_each" do
+    let(:record) { build :metadata_revision }
+    let(:attribute) { :change_history }
+    let(:validator) { described_class.new(attributes: [attribute]) }
+
     def change_history_item(id: SecureRandom.uuid,
                             note: "Note",
                             public_timestamp: Time.zone.today.rfc3339)
@@ -12,7 +16,8 @@ RSpec.describe MetadataRevision do
         change_history_item(public_timestamp: Time.zone.yesterday.rfc3339),
       ]
 
-      expect(build(:metadata_revision, change_history: change_history)).to be_valid
+      validator.validate_each(record, attribute, change_history)
+      expect(record).to be_valid
     end
 
     it "copes when two items have the same public_timestamp" do
@@ -21,13 +26,14 @@ RSpec.describe MetadataRevision do
         change_history_item(public_timestamp: Time.zone.yesterday.rfc3339),
       ]
 
-      expect(build(:metadata_revision, change_history: change_history)).to be_valid
+      validator.validate_each(record, attribute, change_history)
+      expect(record).to be_valid
     end
 
     it "fails validation if 'id' is missing" do
       change_history = [change_history_item.except("id")]
 
-      expect { build(:metadata_revision, change_history: change_history).valid? }
+      expect { validator.validate_each(record, attribute, change_history) }
         .to raise_error(ActiveModel::StrictValidationFailed,
                         "Change history has an entry with invalid keys")
     end
@@ -35,7 +41,7 @@ RSpec.describe MetadataRevision do
     it "fails validation if 'note' is missing" do
       change_history = [change_history_item.except("note")]
 
-      expect { build(:metadata_revision, change_history: change_history).valid? }
+      expect { validator.validate_each(record, attribute, change_history) }
         .to raise_error(ActiveModel::StrictValidationFailed,
                         "Change history has an entry with invalid keys")
     end
@@ -43,7 +49,7 @@ RSpec.describe MetadataRevision do
     it "fails validation if 'public_timestamp' is missing" do
       change_history = [change_history_item.except("public_timestamp")]
 
-      expect { build(:metadata_revision, change_history: change_history).valid? }
+      expect { validator.validate_each(record, attribute, change_history) }
         .to raise_error(ActiveModel::StrictValidationFailed,
                         "Change history has an entry with invalid keys")
     end
@@ -51,7 +57,7 @@ RSpec.describe MetadataRevision do
     it "fails validation if the 'id' is not a valid UUID" do
       change_history = [change_history_item(id: "1234567-123-123-123-01234567890")]
 
-      expect { build(:metadata_revision, change_history: change_history).valid? }
+      expect { validator.validate_each(record, attribute, change_history) }
         .to raise_error(ActiveModel::StrictValidationFailed,
                         "Change history has an entry with a non UUID id")
     end
@@ -59,7 +65,7 @@ RSpec.describe MetadataRevision do
     it "fails validation if 'public_timestamp' is not a valid date" do
       change_history = [change_history_item(public_timestamp: "20201-13-32 29:61:61")]
 
-      expect { build(:metadata_revision, change_history: change_history).valid? }
+      expect { validator.validate_each(record, attribute, change_history) }
         .to raise_error(ActiveModel::StrictValidationFailed,
                         "Change history has an entry with an invalid timestamp")
     end
@@ -70,7 +76,7 @@ RSpec.describe MetadataRevision do
         change_history_item(public_timestamp: Time.zone.today.rfc3339),
       ]
 
-      expect { build(:metadata_revision, change_history: change_history).valid? }
+      expect { validator.validate_each(record, attribute, change_history) }
         .to raise_error(ActiveModel::StrictValidationFailed,
                         "Change history is not in a reverse chronological ordering")
     end
