2ebadc4284274493947996e0b8d25ef50ae7f3cc
Ben Thorner <ben.thorner@digital.cabinet-office.gov.uk>
2020-03-18 12:51:57 +0000

    Add a new feature to reorder featured attachments
    This adds a new 'reorder' page/route and the associated interactor
    to set a new featured attachment ordering. Since the feature behaves
    differently with/out JS, there is a separate scenario for each case.
    
    I did consider splitting these changes across several commits, but
    they all seem simple and non-contentious (except maybe the views),
    and having them in one commit can make any changes easier to apply.
    
    As with d8b5001c, if an attachment is not found in the ordering
    submitted by the user (e.g. if another user uploaded it while the
    reordering was going on), we arbitrarily order it at the top.

---
+++ app/interactors/featured_attachments/update_order_interactor.rb
@@ -0,0 +1,54 @@
+class FeaturedAttachments::UpdateOrderInteractor < ApplicationInteractor
+  delegate :params,
+           :user,
+           :edition,
+           :attachments,
+           :updater,
+           to: :context
+
+  def call
+    Edition.transaction do
+      find_and_lock_edition
+      reorder_attachments
+      update_edition
+      create_timeline_entry
+      update_preview
+    end
+  end
+
+  def find_and_lock_edition
+    context.edition = Edition.lock.find_current(document: params[:document])
+    assert_edition_state(edition, &:editable?)
+
+    assert_edition_feature(edition, assertion: "supports featured attachments") do
+      edition.document_type.attachments.featured?
+    end
+  end
+
+  def reorder_attachments
+    new_ordering = edition.featured_attachments.sort_by do |attachment|
+      ordering_params[attachment.featured_attachment_id].to_i
+    end
+
+    context.updater = Versioning::RevisionUpdater.new(edition.revision, user)
+    updater.assign(featured_attachment_ordering: new_ordering.map(&:featured_attachment_id))
+    context.fail! unless updater.changed?
+  end
+
+  def update_edition
+    EditDraftEditionService.call(edition, user, revision: updater.next_revision)
+    edition.save!
+  end
+
+  def create_timeline_entry
+    TimelineEntry.create_for_revision(entry_type: :attachments_reordered, edition: edition)
+  end
+
+  def update_preview
+    FailsafeDraftPreviewService.call(edition)
+  end
+
+  def ordering_params
+    @ordering_params ||= params.require(:attachments).permit(ordering: {})[:ordering].to_h
+  end
+end
