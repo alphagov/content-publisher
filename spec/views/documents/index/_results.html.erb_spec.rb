7d5eb58a3d9cbe3832d6ef81a0424b7ee719f7ae
Ben Thorner <ben.thorner@digital.cabinet-office.gov.uk>
2020-03-19 12:37:55 +0000

    Add spec to cover an untitled document
    Previously we found that a user can abort setting the title for a
    document, which results it being shown as '‘’' on many pages, and
    as an empty string on the document summary and index pages.
    
    This adds a test to ensure we're using the fallback on the latter
    two, as in other scenarios it's unlikely the user is blocked if we
    were to forget to use the fallback title.
    
    Since the title of a page is rendered by the layout, we need to render
    the application layout explicitly in order to check the title on the
    document summary page.

---
+++ spec/views/documents/index/_results.html.erb_spec.rb
@@ -0,0 +1,12 @@
+RSpec.describe "documents/index/_results.html.erb" do
+  describe "Results list" do
+    it "shows a fallback for untitled documents" do
+      edition = create(:edition, title: nil)
+      assign(:editions, Kaminari.paginate_array([edition]).page)
+      assign(:sort, "")
+      assign(:filter_params, {})
+      render
+      expect(rendered).to include(I18n.t!("documents.untitled_document"))
+    end
+  end
+end
