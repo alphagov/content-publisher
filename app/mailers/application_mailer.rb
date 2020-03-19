b1dc68c7d6af841579e1bfae1810241caa36d2d2
Benjamin Eskola <benjamin.eskola@digital.cabinet-office.gov.uk>
2020-03-19 11:35:55 +0000

    Apply suggestions from code review
    Co-Authored-By: Kevin Dew <kevindew@me.com>

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -4,6 +4,6 @@ class ApplicationMailer < Mail::Notify::Mailer
   end
 
   def template_id
-    @template_id ||= ENV.fetch("GOVUK_NOTIFY_TEMPLATE_ID", "fake-test-template-id")
+    ENV.fetch("GOVUK_NOTIFY_TEMPLATE_ID", "fake-test-template-id")
   end
 end
