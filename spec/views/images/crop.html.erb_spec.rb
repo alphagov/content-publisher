5940b75a3a3e5ca6a323a2db34ac4f9a0a8a20ea
Ben Thorner <ben.thorner@digital.cabinet-office.gov.uk>
2020-03-25 15:45:23 +0000

    Add missing view spec for crop page button
    This adds a spec to cover the 'Continue' text of the button on the page
    to crop an image, which is meant to reassure the user they don't need to
    do anything if their image is already at exact dimensions.
    
    Since 'rendering_context' is set application-wide, it seems reasonable
    to define this at the top-level spec helper, alongside 'current_user'.

---
+++ spec/views/images/crop.html.erb_spec.rb
@@ -0,0 +1,38 @@
+RSpec.describe "images/crop.html.erb" do
+  describe "image crop" do
+    it "shows a 'Save' button with JS" do
+      assign(:image_revision, create(:image_revision))
+      assign(:edition, build(:edition))
+      render
+      expect(rendered).to have_button("Save")
+    end
+
+    it "shows a 'Continue' button without JS" do
+      assign(:image_revision, create(:image_revision))
+      assign(:edition, build(:edition))
+      render
+      expect(rendered).to have_selector(".app-no-js button", text: "Continue")
+    end
+
+    context "when the image has exact dimensions" do
+      it "shows a 'Continue' button instead" do
+        assign(:image_revision, create(:image_revision,
+                                       width: Image::WIDTH,
+                                       height: Image::HEIGHT))
+
+        assign(:edition, build(:edition))
+        render
+        expect(rendered).to have_selector(".app-js-only button", text: "Continue")
+      end
+    end
+
+    context "when uploading a new image" do
+      it "shows a 'Save and continue' button" do
+        assign(:image_revision, create(:image_revision))
+        assign(:edition, build(:edition))
+        render(template: "images/crop", locals: { params: { wizard: "upload" } })
+        expect(rendered).to have_button("Save and continue")
+      end
+    end
+  end
+end
