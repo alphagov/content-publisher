6b736929a3b9b522071a18dc3c569406c6e70e8c
Peter Hartshorn <peter.hartshorn@digital.cabinet-office.gov.uk>
2020-03-26 11:36:24 +0000

    Make change history check more realistic
    Previously this code assumed that publishing-api would return
    change_history for draft editions. This has turned out to be incorrect
    as publishing-api only returns change_history for live editions. This
    has lead to some discrepancies when checking the state of change history
    between cp and pub-api.
    
    The difference in behaviour is that content-publisher can return
    change_history for non-live editions if the draft edition is a major
    change with a change_note and is not the first edition. See ->
    https://github.com/alphagov/content-publisher/commit/72b6556e218d66d49e4fd21f14743ea49926d36f#diff-4e6bc683d583eff1852c25cce42073d2R26
    
    The above means that content_history can be different when comparing
    drafts so we need to trim the latest draft change_note in cp to reflect
    the state in publishing-api.

--- lib/whitehall_importer/integrity_checker.rb
+++ lib/whitehall_importer/integrity_checker.rb
@@ -58,9 +58,9 @@ module WhitehallImporter
       ).sufficiently_similar?
 
       problems << "change history doesn't match" unless ChangeHistoryCheck.new(
+        edition,
         proposed_payload.dig("details", "change_history"),
         publishing_api_content["details"].fetch("change_history", []),
-        edition,
       ).match?
 
       problems
