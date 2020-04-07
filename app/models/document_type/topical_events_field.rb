90bfd4ff200b05b91d5762c4e683a95d96aaa898
Ben Thorner <ben.thorner@digital.cabinet-office.gov.uk>
2020-04-07 10:48:27 +0100

    Move non-DRY #payload into individual fields
    Previously the parent MultiTagField class defined a payload method
    that was only non-overridden in two classes. Since each multi-tag
    subclass tests its own implementation, it makes sense to define the
    abstract method in each class, but preserve the interface.

--- app/models/document_type/topical_events_field.rb
+++ app/models/document_type/topical_events_field.rb
@@ -2,4 +2,10 @@ class DocumentType::TopicalEventsField < DocumentType::MultiTagField
   def id
     "topical_events"
   end
+
+  def payload(edition)
+    return {} if edition.tags[id].blank?
+
+    { links: { id.to_sym => edition.tags[id] } }
+  end
 end
