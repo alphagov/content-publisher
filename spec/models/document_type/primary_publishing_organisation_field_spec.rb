cd224f62f577bdcaabaefe4d8f509f649a256ec3
Ben Thorner <ben.thorner@digital.cabinet-office.gov.uk>
2020-04-14 13:34:40 +0100

    Fix inaccurate tests for primary org field
    Previously there seems to have been some confusion about the format
    of the inbound params for this field, which are actually an array
    and are no longer in a 'tags' namespace. This updates the tests to
    use more representative data for params.

--- spec/models/document_type/primary_publishing_organisation_field_spec.rb
+++ spec/models/document_type/primary_publishing_organisation_field_spec.rb
@@ -35,14 +35,19 @@ RSpec.describe DocumentType::PrimaryPublishingOrganisationField do
     let(:edition) { build(:edition) }
 
     it "returns no issues when there are none" do
-      params = { primary_publishing_organisation: SecureRandom.uuid }
+      params = { primary_publishing_organisation: [SecureRandom.uuid] }
       issues = described_class.new.pre_update_issues(edition, params)
       expect(issues).to be_empty
     end
 
     it "returns an issue if no primary publishing organisation is provided" do
-      params = {}
+      params = { primary_publishing_organisation: [] }
       issues = described_class.new.pre_update_issues(edition, params)
+      expect(issues).to have_issue(:primary_publishing_organisation, :blank, styles: %i[form summary])
+    end
+
+    it "returns an issue if the field is not in the params" do
+      issues = described_class.new.pre_update_issues(edition, {})
       expect(issues).to have_issue(:primary_publishing_organisation, :blank, styles: %i[form summary])
     end
   end
