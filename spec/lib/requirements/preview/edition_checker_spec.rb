a17a8a9a9f5c07eb6e01a095a0a7b2f62ee3dae0
Ben Thorner <ben.thorner@digital.cabinet-office.gov.uk>
2020-04-22 15:51:08 +0100

    Add missing specs for preview/publish checkers
    This adds a spec to check that we call the lower-level checkers to
    find any 'preview' or 'publish' issues.

---
+++ spec/lib/requirements/preview/edition_checker_spec.rb
@@ -0,0 +1,19 @@
+RSpec.describe Requirements::Preview::EditionChecker do
+  describe ".issues" do
+    it "delegates to lower-level checkers" do
+      edition = build :edition
+      low_level_issues = Requirements::CheckerIssues.new(%i[issue])
+      checkers = Requirements::Preview::EditionChecker::CHECKERS
+
+      checkers.each do |checker|
+        allow(checker)
+          .to receive(:call)
+          .with(edition)
+          .and_return(low_level_issues)
+      end
+
+      issues = described_class.call(edition)
+      expect(issues.to_a).to eq(checkers.map { :issue })
+    end
+  end
+end
