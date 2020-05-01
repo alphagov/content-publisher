319c23ea228bdeea5d44575f2db2109d82eb2212
Kevin Dew <kevindew@me.com>
2020-05-01 11:15:35 +0100

    Dry up Publisher Information spec
    This moves the repeat logic to helper methods.

--- spec/features/publisher_information/visit_publisher_information_pages_spec.rb
+++ spec/features/publisher_information/visit_publisher_information_pages_spec.rb
@@ -23,65 +23,45 @@ RSpec.feature "User can view the publisher information pages" do
   end
 
   def when_i_click_on_the_publisher_updates_link_in_footer
-    within(".govuk-footer") do
-      click_on "What’s new in Content Publisher"
-    end
+    click_footer_link("What’s new in Content Publisher")
   end
 
   def then_i_can_see_publisher_updates_page
-    within("main h1") do
-      expect(page).to have_content(I18n.t("publisher_information.publisher_updates.title"))
-    end
+    expect_page_to_have_h1(I18n.t("publisher_information.publisher_updates.title"))
   end
 
   def when_i_click_on_the_beta_capabilities_link_in_footer
-    within(".govuk-footer") do
-      click_on "What the Beta can and cannot do"
-    end
+    click_footer_link("What the Beta can and cannot do")
   end
 
   def then_i_can_see_beta_capabilities_page
-    within("main h1") do
-      expect(page).to have_content(I18n.t("publisher_information.beta_capabilities.title"))
-    end
+    expect_page_to_have_h1(I18n.t("publisher_information.beta_capabilities.title"))
   end
 
   def when_i_click_on_the_how_to_use_publisher_link_in_footer
-    within(".govuk-footer") do
-      click_on "How to use Content Publisher"
-    end
+    click_footer_link("How to use Content Publisher")
   end
 
   def then_i_can_see_the_how_to_use_publisher_page
-    within("main h1") do
-      expect(page).to have_content(I18n.t("publisher_information.how_to_use_publisher.title"))
-    end
+    expect_page_to_have_h1(I18n.t("publisher_information.how_to_use_publisher.title"))
   end
 
   def when_i_click_on_the_request_training_link_in_footer
-    within(".govuk-footer") do
-      click_on "Request Content Publisher training"
-    end
+    click_footer_link("Request Content Publisher training")
   end
 
   def then_i_can_see_the_request_training_page
-    within("main h1") do
-      expect(page).to have_content(I18n.t("publisher_information.request_training.title"))
-    end
+    expect_page_to_have_h1(I18n.t("publisher_information.request_training.title"))
   end
 
   def when_i_click_on_the_what_managing_editors_can_do_link_in_footer
-    within(".govuk-footer") do
-      click_on "What Managing Editors can do"
-    end
+    click_footer_link("What Managing Editors can do")
   end
 
   alias :and_i_click_on_the_what_managing_editors_can_do_link_in_footer :when_i_click_on_the_what_managing_editors_can_do_link_in_footer
 
   def then_i_can_see_the_what_managing_editors_can_do_page
-    within("main h1") do
-      expect(page).to have_content(I18n.t("publisher_information.what_managing_editors_can_do.title"))
-    end
+    expect_page_to_have_h1(I18n.t("publisher_information.what_managing_editors_can_do.title"))
   end
 
   def when_i_have_the_managing_editor_permission
@@ -94,5 +74,13 @@ RSpec.feature "User can view the publisher information pages" do
 
   def and_i_see_i_am_a_managing_editor
     expect(page).to have_content(I18n.t("publisher_information.what_managing_editors_can_do.user_status.with_managing_editor_status"))
+  end
+
+  def click_footer_link(text)
+    within(".govuk-footer") { click_on text }
+  end
+
+  def expect_page_to_have_h1(content)
+    within("main h1") { expect(page).to have_content(content) }
   end
 end
