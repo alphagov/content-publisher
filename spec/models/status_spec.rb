af0c7f162a114a9aae064185a5f4eca2deb40962
Ben Thorner <ben.thorner@digital.cabinet-office.gov.uk>
2020-05-11 16:38:46 +0100

    Stop enforcing translation for states
    Previously we used to require all edition states have a corresponding
    translation, but this doesn't make sense for 'superseded' since it's
    not possible to render it in the UI. This removes the enforcement and
    the unused translation that it was enforcing; other translations are
    now enforced as part of our general I18n coverage.

--- spec/models/status_spec.rb
+++
@@ -1,9 +0,0 @@
-RSpec.describe Status do
-  describe ".states" do
-    described_class.states.each_key do |state|
-      it "has a translation for `#{state}`" do
-        expect(I18n.exists?("user_facing_states.#{state}.name")).to be true
-      end
-    end
-  end
-end
