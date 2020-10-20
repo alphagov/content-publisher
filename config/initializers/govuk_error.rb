0d8aeb71d181448fa1cb99a6ab524f5c93896f0b
ChrisBAshton <christopher.ashton@digital.cabinet-office.gov.uk>
2020-10-20 13:20:20 +0100

    Remove unneeded `should_capture` code
    As of govuk_app_config 2.5.0, excluding errors of type `PG::Error`
    that occur during the data sync on integration/staging now happens
    automatically. See https://github.com/alphagov/govuk_app_config/pull/160
    
    Trello: https://trello.com/c/rAlvGlSp/2123-5-ignore-data-sync-errors-in-govukappconfig

--- config/initializers/govuk_error.rb
+++ config/initializers/govuk_error.rb
@@ -1,17 +1,3 @@
 GovukError.configure do |config|
   config.excluded_exceptions << "ApplicationController::Forbidden"
-
-  # Don't capture postgres errors that occur during the time that the data sync
-  # is running in integration and staging environments
-  # This lambda is called with Ruby Exception objects, Raven::Event objects
-  # and may be called with other types.
-  config.should_capture = lambda { |error_or_event|
-    data_sync_ignored_error = error_or_event.is_a?(PG::Error) ||
-      (error_or_event.respond_to?(:cause) && error_or_event.cause.is_a?(PG::Error))
-    data_sync_environment = ENV.fetch("SENTRY_CURRENT_ENV", "")
-                               .match(/integration|staging/)
-    data_sync_time = Time.zone.now.hour <= 5
-
-    !(data_sync_ignored_error && data_sync_environment && data_sync_time)
-  }
 end
