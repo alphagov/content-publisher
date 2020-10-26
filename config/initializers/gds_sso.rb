2a76b43d4c09a6ec875363a40a1506fd1289c98f
Ben Thorner <ben.thorner@digital.cabinet-office.gov.uk>
2020-10-26 15:46:20 +0000

    Remove redundant GDS SSO initializer
    This was made redundant in [1].
    
    [1]: https://github.com/alphagov/gds-sso/pull/241

--- config/initializers/gds_sso.rb
+++
@@ -1,6 +0,0 @@
-GDS::SSO.config do |config|
-  config.user_model   = "User"
-  config.oauth_id     = ENV["OAUTH_ID"]
-  config.oauth_secret = ENV["OAUTH_SECRET"]
-  config.oauth_root_url = Plek.new.external_url_for("signon")
-end
