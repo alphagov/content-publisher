69ebc6e687424bcd23790adeb2400bb38fba91b2
Kevin Dew <kevindew@me.com>
2022-02-10 21:15:08 +0000

    Super basic date / time input

---
+++ app/models/document_type/time_field.rb
@@ -0,0 +1,46 @@
+class DocumentType::TimeField
+  attr_reader :name, :label, :precision
+
+  def initialize(options)
+    @name = options.fetch("name")
+    @label = options.fetch("label")
+    @precision = options.fetch("precision")
+  end
+
+  def id
+    "time"
+  end
+
+  def payload(edition)
+    {
+      details: { name.to_sym => edition.contents[name] }
+    }
+  end
+
+  def updater_params(_edition, params)
+    value = if params[name].nil?
+              nil
+            elsif precision == "date"
+              day, month, year = params[name].values_at(:day, :month, :year)
+              Date.strptime("#{day}-#{month}-#{year}", "%d-%m-%Y")
+            else
+              # @TODO
+              # Time.parse(params[name])
+              Time.now
+            end
+
+    { contents: { name.to_sym => value } }
+  end
+
+  def form_issues(_edition, _params)
+    Requirements::CheckerIssues.new
+  end
+
+  def preview_issues(_edition)
+    Requirements::CheckerIssues.new
+  end
+
+  def publish_issues(_edition)
+    Requirements::CheckerIssues.new
+  end
+end
--- config/initializers/time_formats.rb
+++ config/initializers/time_formats.rb
@@ -1,3 +1,4 @@
 Time::DATE_FORMATS[:date] = "%-d %B %Y"
 Time::DATE_FORMATS[:time] = "%-l:%M%P"
 Time::DATE_FORMATS[:time_on_date] = "%-l:%M%P on %-d %B %Y"
+Date::DATE_FORMATS[:default] = "%-d %B %Y"
