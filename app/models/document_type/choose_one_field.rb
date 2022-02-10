a9e48d4769cfd996aad06d5de71f8963f8b64367
Kevin Dew <kevindew@me.com>
2022-02-10 23:22:54 +0000

    Add choose one

---
+++ app/models/document_type/choose_one_field.rb
@@ -0,0 +1,41 @@
+class DocumentType::ChooseOneField
+  attr_reader :name, :label, :input, :options
+
+  def initialize(options)
+    @name = options.fetch("name")
+    @label = options.fetch("label")
+    @input = options.fetch("input", "radio")
+    @options = options.fetch("options")
+  end
+
+  def id
+    "choose_one"
+  end
+
+  def payload(edition)
+    {
+      details: { name.to_sym => edition.contents[name] }
+    }
+  end
+
+  def updater_params(_edition, params)
+    { contents: { name.to_sym => params[name] } }
+  end
+
+  def form_issues(_edition, _params)
+    Requirements::CheckerIssues.new
+  end
+
+  def preview_issues(_edition)
+    Requirements::CheckerIssues.new
+  end
+
+  def publish_issues(_edition)
+    Requirements::CheckerIssues.new
+  end
+
+  def value(item)
+    option = options.find { |o| o["name"] == item }
+    option && option["label"]
+  end
+end
