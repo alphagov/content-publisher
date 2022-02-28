49da037e26a7e27c7ebbf2fc660efc60997091a1
Edward Kerry <edward.kerry@digital.cabinet-office.gov.uk>
2022-02-28 17:38:43 +0000

    Add attachment arguments
    In order to present a link to the new accessible format request form, attachments will need to provide the attachment_id and the owning_document_content_id.
    
    Initially, the new flow of requesting accessible formats will be a pilot and only available for documents
    owned by a few organisations. This code will not need to be changed when the change is
    released to all orgs.
    
    Co-authored-by: Peter Hartshorn <peter.hartshorn@digital.cabinet-office.gov.uk>

--- spec/lib/govspeak_document/in_app_options_spec.rb
+++ spec/lib/govspeak_document/in_app_options_spec.rb
@@ -44,16 +44,16 @@ RSpec.describe GovspeakDocument::InAppOptions do
 
       in_app_options = described_class.new("govspeak", edition)
       actual_attachment_options = in_app_options.to_h[:attachments].first
-
       expect(actual_attachment_options).to match(
         a_hash_including(
-          id: "13kb-1-page-attachment.pdf",
+          attachment_id: attachment_revision.file_attachment_id,
           filename: "13kb-1-page-attachment.pdf",
           title: "A title",
           content_type: "application/pdf",
           number_of_pages: 1,
           file_size: 13_264,
           url: preview_file_attachment_path(edition.document, attachment_revision.file_attachment),
+          owning_document_content_id: edition.content_id,
           alternative_format_contact_email: "foo@bar.com",
         ),
       )
