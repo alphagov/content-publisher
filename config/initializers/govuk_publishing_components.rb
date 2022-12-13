61a23c6487bb8da7214f79cbffe2eb97d0cb0161
davidgisbey <david.gisbey@digital.cabinet-office.gov.uk>
2022-12-13 14:22:07 +0000

    Remove application_print_stylesheet config for publishing components
    The print stylesheet has been removed in this release and no longer
    needs to be configured.
    
    See https://github.com/alphagov/govuk_publishing_components/pull/3110.

--- config/initializers/govuk_publishing_components.rb
+++ config/initializers/govuk_publishing_components.rb
@@ -1,8 +1,6 @@
 # config/initializers/govuk_publishing_components.rb
 GovukPublishingComponents.configure do |c|
   c.component_guide_title = "Content Publisher"
-  c.application_print_stylesheet = nil
-
   c.application_stylesheet = "application"
   c.application_javascript = "application"
 end
