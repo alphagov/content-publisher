16efeffe57d2ae77378dbe2e9768b0109175e30f
Peter Hartshorn <peter.hartshorn@digital.cabinet-office.gov.uk>
2024-10-14 11:51:32 +0100

    Find current

--- app/controllers/remove_controller.rb
+++ app/controllers/remove_controller.rb
@@ -1,7 +1,7 @@
 class RemoveController < ApplicationController
   def remove
-    @edition = Edition.find_current(document: params[:document])
-    puts(@edition)
+    @edition = Edition.find_current(document: params[:document]) # find current
+    puts(@edition.state)
 
     assert_edition_state(@edition, assertion: "is published") do
       @edition.published? || @edition.published_but_needs_2i?
