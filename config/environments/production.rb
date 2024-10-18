0ad47842a3e56e02dd01efe49bee8ab8d0bf52cb
Chris Ashton <ChrisBAshton@users.noreply.github.com>
2024-10-18 11:20:59 +0100

    Fix hosts regex
    See https://github.com/alphagov/contacts-admin/pull/1483#issuecomment-2421660720

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,7 +94,7 @@ Rails.application.configure do
 
   # Enable DNS rebinding protection and other `Host` header attacks.
   config.hosts = [
-    /content-publisher\..*gov.uk?/,
+    /content-publisher\..*\.gov.uk?/,
   ]
 
   # Skip DNS rebinding protection for the default health check endpoint.
